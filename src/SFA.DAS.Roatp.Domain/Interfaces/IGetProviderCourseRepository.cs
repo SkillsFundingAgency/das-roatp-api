a1a736b593ecbbc5204e14a6c8d3d227bdef99c2
Deven Shah <devenshah1974@gmail.com>
2022-03-01 21:34:50 +0000

    COL-3 Added repository interfaces to the domain

---
+++ src/SFA.DAS.Roatp.Domain/Interfaces/IGetProviderCourseRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.Roatp.Domain.Entities;
+
+namespace SFA.DAS.Roatp.Domain.Interfaces
+{
+    public interface IGetProviderCourseRepository
+    {
+        Task<ProviderCourse> GetProviderCourseDeliveryModels(int ukprn, int larsCode);
+    }
+}
