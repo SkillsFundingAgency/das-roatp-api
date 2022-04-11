686e48950be8ac4b52cc683a16aa8af62645080f
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-04-11 14:59:34 +0100

    CSP-40 added two end points Get and update for provider

---
+++ src/SFA.DAS.Roatp.Data/Repositories/ProviderRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SFA.DAS.Roatp.Domain.Entities;
+using SFA.DAS.Roatp.Domain.Interfaces;
+
+namespace SFA.DAS.Roatp.Data.Repositories
+{
+    internal class ProviderRepository : IProviderRepository
+    {
+        private readonly RoatpDataContext _roatpDataContext;
+
+        public ProviderRepository(RoatpDataContext roatpDataContext)
+        {
+            _roatpDataContext = roatpDataContext;
+        }
+
+        public async Task<Provider> GetProvider(int ukprn)
+        {
+            return await _roatpDataContext
+                .Providers
+                .AsNoTracking()
+                .Where(c => c.Ukprn == ukprn)
+                .SingleOrDefaultAsync();
+        }
+
+        public async Task<Provider> UpdateProvider(int ukprn, bool hasConfirmedDetails)
+        {
+            var providertoUpdate = _roatpDataContext
+                .Providers
+                .AsNoTracking()
+                .Where(c => c.Ukprn == ukprn)
+                .SingleOrDefaultAsync().Result;
+
+            if (providertoUpdate != null)
+            {
+                providertoUpdate.HasConfirmedDetails = hasConfirmedDetails;
+                providertoUpdate.ConfirmedDetailsOn = System.DateTime.Now;
+
+                await _roatpDataContext.SaveChangesAsync();
+            }
+
+            return providertoUpdate;
+        }
+    }
+}
