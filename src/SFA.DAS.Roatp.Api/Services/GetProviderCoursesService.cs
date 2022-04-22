4fc9b7fc0e5aa7634e96da04cc9c942a8e619d6e
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-04-22 11:29:56 +0100

    CSP-75 removed unused packages

--- src/SFA.DAS.Roatp.Api/Services/GetProviderCoursesService.cs
+++ src/SFA.DAS.Roatp.Api/Services/GetProviderCoursesService.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
-using System.Web.Http;
 using Microsoft.Extensions.Logging;
 using SFA.DAS.Roatp.Api.Models;
 using SFA.DAS.Roatp.Domain.Interfaces;
