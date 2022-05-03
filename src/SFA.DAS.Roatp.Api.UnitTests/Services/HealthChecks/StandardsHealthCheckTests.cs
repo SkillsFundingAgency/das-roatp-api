46e4f63ba4acf22ae6f72b4345eae74a6480324c
MarkFCain <mark.cain@digital.education.gov.uk>
2022-05-03 09:19:02 +0100

    CSP-74 standards count moved to application layer + coverage

--- src/SFA.DAS.Roatp.Api.UnitTests/Services/HealthChecks/StandardsHealthCheckTests.cs
+++ src/SFA.DAS.Roatp.Api.UnitTests/Services/HealthChecks/StandardsHealthCheckTests.cs
@@ -1,44 +1,45 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.Roatp.Api.HealthCheck;
-using SFA.DAS.Roatp.Api.Services;
+using SFA.DAS.Roatp.Application.StandardsCount;
 
 namespace SFA.DAS.Roatp.Api.UnitTests.Services.HealthChecks
 {
-    [TestFixture]
-    public class StandardsHealthCheckTests
-    {
-        private Mock<IGetStandardsCountService> _getStandardsService;
-        private StandardsHealthCheck _standardsHealthCheck;
-
-        [SetUp]
-        public void Before_each_test()
-        {
-            _getStandardsService = new Mock<IGetStandardsCountService>();
-            _standardsHealthCheck = new StandardsHealthCheck(_getStandardsService.Object);
-        }
-
-        [Test]
-        public async Task StandardsHealthCheck_StandardsPresent_ReturnsHealthy()
-        {
-
-            _getStandardsService.Setup(x => x.GetStandardsCount()).ReturnsAsync(1);
-
-            var result = await _standardsHealthCheck.CheckHealthAsync(new HealthCheckContext());
-            Assert.AreEqual(HealthStatus.Healthy, result.Status);
-            _getStandardsService.Verify(x => x.GetStandardsCount(), Times.Once);
-        }
-
-        [Test]
-        public async Task StandardsHealthCheck_StandardsAbsent_ReturnsUnhealthy()
-        {
-            _getStandardsService.Setup(x => x.GetStandardsCount()).ReturnsAsync(0);
-
-            var result = await _standardsHealthCheck.CheckHealthAsync(new HealthCheckContext());
-            Assert.AreEqual(HealthStatus.Unhealthy, result.Status);
-            _getStandardsService.Verify(x => x.GetStandardsCount(), Times.Once);
-        }
+     [TestFixture]
+     public class StandardsHealthCheckTests
+     {
+         private Mock<IMediator> _mediatr;
+         private StandardsHealthCheck _standardsHealthCheck;
+    
+         [SetUp]
+         public void Before_each_test()
+         {
+             _mediatr = new Mock<IMediator>();
+             _standardsHealthCheck = new StandardsHealthCheck(_mediatr.Object);
+         }
+    
+         [Test]
+         public async Task StandardsHealthCheck_StandardsPresent_ReturnsHealthy()
+         {
+             var standardsCount = 1;
+             _mediatr.Setup(x => x.Send(It.IsAny<StandardsCountRequest>(), new CancellationToken() )).ReturnsAsync(standardsCount);
+             var result = await _standardsHealthCheck.CheckHealthAsync(new HealthCheckContext());
+             Assert.AreEqual(HealthStatus.Healthy, result.Status);
+             _mediatr.Verify(x => x.Send(It.IsAny<StandardsCountRequest>(), It.IsAny<CancellationToken>()), Times.Once);
+         }
+    
+         [Test]
+         public async Task StandardsHealthCheck_StandardsAbsent_ReturnsUnhealthy()
+         {
+             var standardsCount = 0;
+            _mediatr.Setup(x => x.Send(It.IsAny<StandardsCountRequest>(), new CancellationToken())).ReturnsAsync(standardsCount);
+             var result = await _standardsHealthCheck.CheckHealthAsync(new HealthCheckContext());
+             Assert.AreEqual(HealthStatus.Unhealthy, result.Status);
+             _mediatr.Verify(x => x.Send(It.IsAny<StandardsCountRequest>(), It.IsAny<CancellationToken>()), Times.Once);
+         }
     }
 }
---
+++ src/SFA.DAS.Roatp.Application.UnitTests/StandardsCount/StandardsCountHandlerTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Roatp.Application.ReloadStandards;
+using SFA.DAS.Roatp.Application.StandardsCount;
+using SFA.DAS.Roatp.Domain.Interfaces;
+
+namespace SFA.DAS.Roatp.Application.UnitTests.StandardsCount
+{
+    [TestFixture]
+    public class Tests
+    {
+        private Mock<IGetStandardsCountRepository> _repository;
+        private StandardsCountHandler _handler;
+
+        [SetUp]
+        public void Before_each_test()
+        {
+            _repository = new Mock<IGetStandardsCountRepository>();
+            _handler = new StandardsCountHandler(_repository.Object,
+                Mock.Of<ILogger<StandardsCountHandler>>());
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(10)]
+        [TestCase(100)]
+        [TestCase(1000)]
+        [TestCase(10000)]
+        public async Task StandardsCount_ReturnsExpectedCount(int standardsCount)
+        {
+            _repository.Setup(x => x.GetStandardsCount()).ReturnsAsync(standardsCount);
+            var result = await _handler.Handle(new StandardsCountRequest(), new CancellationToken());
+            Assert.AreEqual(standardsCount,result);
+            _repository.Verify(x => x.GetStandardsCount(), Times.Once);
+        }
+    }
+}
