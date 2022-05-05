83b6b6fd5e6021738fd89f2bb968e1c1bb3b4142
Deven Shah <devenshah1974@gmail.com>
2022-05-05 20:46:35 +0100

    CSP-10 Added fluent validation exception handler

---
+++ src/SFA.DAS.Roatp.Application/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using FluentValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+
+namespace SFA.DAS.Roatp.Application.Extensions
+{
+    [ExcludeFromCodeCoverage]
+    public static class ApplicationBuilderExtensions
+    {
+        public static void UseFluentValidationExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(x =>
+            {
+                x.Run(async context =>
+                {
+                    var errorFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    var exception = errorFeature.Error;
+
+                    if (!(exception is ValidationException validationException))
+                    {
+                        throw exception;
+                    }
+
+                    var errors = validationException.Errors.Select(err => new
+                    {
+                        err.PropertyName,
+                        err.ErrorMessage
+                    });
+
+                    var errorText = JsonSerializer.Serialize(errors);
+                    context.Response.StatusCode = 400;
+                    context.Response.ContentType = "application/json";
+                    await context.Response.WriteAsync(errorText, Encoding.UTF8);
+                });
+            });
+        }
+    }
+}
