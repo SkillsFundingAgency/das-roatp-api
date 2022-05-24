37f21e3eee8db260b1554bf0f10aa562a307a2cd
Deven Shah <devenshah1974@gmail.com>
2022-05-24 04:24:53 +0100

    CSP-19 Implements provider course update endpoint
    CSP-19 Added assertion on result code
    
    CSP-19 Fixed parameter case in the route

---
+++ src/SFA.DAS.Roatp.Data/Repositories/ProviderCourseEditRepository.cs
@@ -0,0 +1,32 @@
+﻿using SFA.DAS.Roatp.Domain.Entities;
+using SFA.DAS.Roatp.Domain.Interfaces;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Roatp.Data.Repositories
+{
+    [ExcludeFromCodeCoverage]
+    internal class ProviderCourseEditRepository : IProviderCourseEditRepository
+    {
+        private readonly RoatpDataContext _roatpDataContext;
+
+        public ProviderCourseEditRepository(RoatpDataContext context)
+        {
+            _roatpDataContext = context;
+        }
+
+        public async Task UpdateContactDetails(ProviderCourse updatedProviderCourseEntity)
+        {
+            var providerCourse = await _roatpDataContext
+                .ProviderCourses
+                .FindAsync(updatedProviderCourseEntity.Id);
+
+            providerCourse.ContactUsEmail = updatedProviderCourseEntity.ContactUsEmail;
+            providerCourse.ContactUsPageUrl = updatedProviderCourseEntity.ContactUsPageUrl;
+            providerCourse.ContactUsPhoneNumber = updatedProviderCourseEntity.ContactUsPhoneNumber;
+            providerCourse.StandardInfoUrl = updatedProviderCourseEntity.StandardInfoUrl;
+
+            await _roatpDataContext.SaveChangesAsync();
+        }
+    }
+}
