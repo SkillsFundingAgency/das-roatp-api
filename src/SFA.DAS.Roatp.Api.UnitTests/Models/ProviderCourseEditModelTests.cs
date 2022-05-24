113892a299cbf798558c3b0490b4516e5de19539
Deven Shah <devenshah1974@gmail.com>
2022-05-24 04:24:53 +0100

    CSP-19 Implements provider course update endpoint
    CSP-19 Added assertion on result code
    
    CSP-19 Fixed parameter case in the route

---
+++ src/SFA.DAS.Roatp.Api.UnitTests/Models/ProviderCourseEditModelTests.cs
@@ -0,0 +1,18 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Roatp.Api.Models;
+using SFA.DAS.Roatp.Application.ProviderCourse;
+
+namespace SFA.DAS.Roatp.Api.UnitTests.Models
+{
+    [TestFixture]
+    public class ProviderCourseEditModelTests
+    {
+        [Test, AutoData]
+        public void ImplicitOperator_ReturnsCommant(ProviderCourseEditModel model)
+        {
+            ((UpdateProviderCourseCommand)model).Should().BeEquivalentTo(model);
+        }
+    }
+}
