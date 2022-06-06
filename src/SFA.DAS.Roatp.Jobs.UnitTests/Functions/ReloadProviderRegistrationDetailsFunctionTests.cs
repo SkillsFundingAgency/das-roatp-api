5a9f499299fc6af0937ea979d24b47a6af607167
Deven Shah <devenshah1974@gmail.com>
2022-06-06 12:09:57 +0100

    CSP-115 Added function to reload provider registration details

---
+++ src/SFA.DAS.Roatp.Jobs.UnitTests/Functions/ReloadProviderRegistrationDetailsFunctionTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Roatp.Jobs.Functions;
+using SFA.DAS.Roatp.Jobs.Services;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Roatp.Jobs.UnitTests.Functions
+{
+    [TestFixture]
+    public class ReloadProviderRegistrationDetailsFunctionTests
+    {
+        [Test]
+        public async Task Run_InvokesService()
+        {
+            var serviceMock = new Mock<IReloadProviderRegistrationDetailService>();
+            var sut = new ReloadProviderRegistrationDetailsFunction(serviceMock.Object);
+
+            await sut.Run(default(TimerInfo), Mock.Of<ILogger>());
+
+            serviceMock.Verify(s => s.ReloadProviderRegistrationDetails());
+        }
+    }
+}
