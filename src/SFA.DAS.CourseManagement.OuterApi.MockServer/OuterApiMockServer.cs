9a1e6010bc06a31f43475c883a738b887502dcff
Deven Shah <devenshah1974@gmail.com>
2022-06-08 18:50:57 +0100

    CSP-140 Added mock server for outer api

---
+++ src/SFA.DAS.CourseManagement.OuterApi.MockServer/OuterApiMockServer.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.RegularExpressions;
+using WireMock.Logging;
+using WireMock.Net.StandAlone;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Settings;
+
+namespace SFA.DAS.CourseManagement.OuterApi.MockServer
+{
+    public static class OuterApiMockServer
+    {
+        public static void Run()
+        {
+            var settings = new WireMockServerSettings
+            {
+                Port = 5334,
+                UseSSL = true,
+                Logger = new WireMockConsoleLogger()
+            };
+
+            var server = StandAloneApp.Start(settings);
+
+            server
+                .Given(Request.Create().WithPath("/*").UsingGet())
+                .RespondWith(Response.Create()
+                    .WithStatusCode(404)
+                    .WithHeader("Content-Type", "application/json")
+                    .WithBody(@"{ 'msg': 'No matching endpoint found. Are you missing a setup?'}"));
+
+            server
+                .Given(Request.Create().WithPath(u => u.Contains("registered-providers"))
+                .UsingGet())
+                .RespondWith(Response.Create()
+                    .WithStatusCode(200)
+                    .WithHeader("Content-Type", "application/json")
+                    .WithBodyFromFile("registered-providers.json"));
+
+            server
+                .Given(Request.Create().WithPath(u => u.Contains("standards"))
+                .UsingGet())
+                .RespondWith(Response.Create()
+                    .WithStatusCode(200)
+                    .WithHeader("Content-Type", "application/json")
+                    .WithBodyFromFile("standards-lookup.json"));
+        }
+    }
+}
---
+++ src/SFA.DAS.CourseManagement.OuterApi.MockServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SFA.DAS.CourseManagement.OuterApi.MockServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running Outer API Server on http://localhost:5021");
+            OuterApiMockServer.Run();
+            Console.WriteLine(("Press any key to stop the server"));
+            Console.ReadKey();
+        }
+    }
+}
