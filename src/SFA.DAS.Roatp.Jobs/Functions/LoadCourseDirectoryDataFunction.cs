77a8f29577da284a9036b5a049947701b4f82999
MarkFCain <mark.cain@digital.education.gov.uk>
2022-06-13 07:04:03 +0100

    CSP-89 import course directory data preliminary work

---
+++ src/SFA.DAS.Roatp.Jobs/Functions/LoadCourseDirectoryDataFunction.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Roatp.Jobs.Services;
+
+namespace SFA.DAS.Roatp.Jobs.Functions
+{
+    public class LoadCourseDirectoryDataFunction
+    {
+        private readonly ILoadCourseDirectoryDataService _loadCourseDirectoryDataService;
+
+        public LoadCourseDirectoryDataFunction(ILoadCourseDirectoryDataService loadCourseDirectoryDataService)
+        {
+            _loadCourseDirectoryDataService = loadCourseDirectoryDataService;
+        }
+
+
+        [FunctionName(nameof(LoadCourseDirectoryDataFunction))]
+        public async Task Run([TimerTrigger("%ReloadStandardsCacheSchedule%"
+#if DEBUG
+            , RunOnStartup=true
+#endif
+        
+        )] TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation("LoadCourseDirectoryData function started");
+
+            await _loadCourseDirectoryDataService.LoadCourseDirectoryData();
+
+            log.LogInformation("Course directory data complete");
+        }
+    }
+}
