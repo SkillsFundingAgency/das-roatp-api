8e7bffa01daef915bb70c274cd65661cdca0b5fb
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-06-17 17:09:35 +0100

    CSP-114 Get all regions

---
+++ src/SFA.DAS.Roatp.Application/Region/Queries/RegionModel.cs
@@ -0,0 +1,22 @@
+﻿
+namespace SFA.DAS.Roatp.Application.Region.Queries
+{
+    public class RegionModel
+    {
+        public int Id { get; set; }
+        public string SubregionName { get; set; }
+        public string RegionName { get; set; }
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+
+        public static implicit operator RegionModel(Domain.Entities.Region source) =>
+            new RegionModel
+            {
+                Id = source.Id,
+                SubregionName = source.SubregionName,
+                RegionName = source.RegionName,
+                Latitude = source.Latitude,
+                Longitude = source.Longitude,
+            };
+    }
+}
