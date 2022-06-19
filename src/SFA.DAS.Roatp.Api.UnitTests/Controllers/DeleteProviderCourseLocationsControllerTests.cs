c0dc0b7f884d55db206c4f7c957fa28a0c02d24b
Deven Shah <devenshah1974@gmail.com>
2022-06-19 13:04:25 +0100

    CSP-81 Added DeleteProviderCourseLocationsController

---
+++ src/SFA.DAS.Roatp.Api.UnitTests/Controllers/DeleteProviderCourseLocationsControllerTests.cs
@@ -0,0 +1,65 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Roatp.Api.Controllers;
+using SFA.DAS.Roatp.Application.ProviderCourseLocations.Commands.BulkDelete;
+using SFA.DAS.Testing.AutoFixture;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Roatp.Api.UnitTests.Controllers
+{
+    [TestFixture]
+    public class DeleteProviderCourseLocationsControllerTests
+    {
+        [Test, MoqAutoData]
+        public async Task BulkDeleteProviderCourseLocations_CallsHandler(
+            [Frozen] Mock<IMediator> _mediatorMock,
+            [Greedy] DeleteProviderCourseLocationsController sut,
+            int ukprn, int larsCode, DeleteOptions options)
+        {
+            await sut.BulkDeleteProviderCourseLocations(ukprn, larsCode, options);
+
+            _mediatorMock.Verify(m => m.Send(It.Is<BulkDeleteProviderCourseLocationsCommand>(c => c.Ukprn == ukprn && c.LarsCode == larsCode && c.DeleteOptions == options), It.IsAny<CancellationToken>()));
+        }
+
+        [Test, MoqAutoData]
+        public async Task BulkDeleteProviderCourseLocations_ZeroResults_ReturnsNotFoundResponse(
+            [Frozen] Mock<IMediator> _mediatorMock,
+            [Greedy] DeleteProviderCourseLocationsController sut,
+            int ukprn, int larsCode, DeleteOptions options)
+        {
+            _mediatorMock.Setup(m => m.Send(It.Is<BulkDeleteProviderCourseLocationsCommand>(c => c.Ukprn == ukprn && c.LarsCode == larsCode && c.DeleteOptions == options), It.IsAny<CancellationToken>())).ReturnsAsync(0);
+
+            var result = await sut.BulkDeleteProviderCourseLocations(ukprn, larsCode, options);
+
+            _mediatorMock.Verify(m => m.Send(It.Is<BulkDeleteProviderCourseLocationsCommand>(c => c.Ukprn == ukprn && c.LarsCode == larsCode && c.DeleteOptions == options), It.IsAny<CancellationToken>()));
+
+            var statusCodeResult = (StatusCodeResult)result;
+
+            statusCodeResult.Should().NotBeNull();
+            statusCodeResult.StatusCode.Should().Be(404);
+        }
+
+        [Test, MoqAutoData]
+        public async Task BulkDeleteProviderCourseLocations_MoreThanZeroResults_ReturnsNoContentResponse(
+            [Frozen] Mock<IMediator> _mediatorMock,
+            [Greedy] DeleteProviderCourseLocationsController sut,
+            int ukprn, int larsCode, DeleteOptions options)
+        {
+            _mediatorMock.Setup(m => m.Send(It.Is<BulkDeleteProviderCourseLocationsCommand>(c => c.Ukprn == ukprn && c.LarsCode == larsCode && c.DeleteOptions == options), It.IsAny<CancellationToken>())).ReturnsAsync(1);
+
+            var result = await sut.BulkDeleteProviderCourseLocations(ukprn, larsCode, options);
+
+            _mediatorMock.Verify(m => m.Send(It.Is<BulkDeleteProviderCourseLocationsCommand>(c => c.Ukprn == ukprn && c.LarsCode == larsCode && c.DeleteOptions == options), It.IsAny<CancellationToken>()));
+
+            var statusCodeResult = (StatusCodeResult)result;
+
+            statusCodeResult.Should().NotBeNull();
+            statusCodeResult.StatusCode.Should().Be(204);
+        }
+    }
+}
---
+++ src/SFA.DAS.Roatp.Api/Controllers/DeleteProviderCourseLocationsController.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Roatp.Application.ProviderCourseLocations.Commands.BulkDelete;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Roatp.Api.Controllers
+{
+    [ApiController]
+    public class DeleteProviderCourseLocationsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<DeleteProviderCourseLocationsController> _logger;
+
+        public DeleteProviderCourseLocationsController(IMediator mediator, ILogger<DeleteProviderCourseLocationsController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        [HttpDelete]
+        [Route("/providers/{ukprn}/courses/{larsCode}/locations")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<ActionResult> BulkDeleteProviderCourseLocations([FromRoute] int ukprn, [FromRoute] int larsCode, [FromQuery] DeleteOptions options)
+        {
+            _logger.LogInformation("Request received for bulk delete provider course locations for Ukprn:{ukprn} LarsCode:{larscode} DeleteOptions:{deleteOptions}", ukprn, larsCode, options);
+
+            var command = new BulkDeleteProviderCourseLocationsCommand(ukprn, larsCode, options);
+            var result = await _mediator.Send(command);
+
+            _logger.LogInformation("Deleted {numberOfRecordsDeleted} provider course locations for Ukprn:{ukprn} LarsCode:{larscode}", result, ukprn, larsCode);
+
+            return result > 0 ? StatusCode((int)HttpStatusCode.NoContent) : StatusCode((int)HttpStatusCode.NotFound);
+        }
+    }
+}
