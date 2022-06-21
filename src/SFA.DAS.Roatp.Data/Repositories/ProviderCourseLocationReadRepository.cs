d178f3f9ee2303a3838adb23505cf7c138d66fd3
Deven Shah <devenshah1974@gmail.com>
2022-06-21 16:17:56 +0100

    CSP-81 Excluded repositories from code coverage

--- src/SFA.DAS.Roatp.Data/Repositories/ProviderCourseLocationReadRepository.cs
+++ src/SFA.DAS.Roatp.Data/Repositories/ProviderCourseLocationReadRepository.cs
@@ -2,11 +2,13 @@
 using SFA.DAS.Roatp.Domain.Entities;
 using SFA.DAS.Roatp.Domain.Interfaces;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SFA.DAS.Roatp.Data.Repositories
 {
+    [ExcludeFromCodeCoverage]
    internal class ProviderCourseLocationReadRepository : IProviderCourseLocationReadRepository
     {
         private readonly RoatpDataContext _roatpDataContext;
