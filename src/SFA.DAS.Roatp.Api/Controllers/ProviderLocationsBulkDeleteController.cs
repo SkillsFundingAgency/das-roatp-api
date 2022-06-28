71b7d8cc6efb16dbb65deae7d79b3018ba80850b
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-06-28 17:41:43 +0100

    CSP-114 provide location bulk delete seperate command

--- src/SFA.DAS.Roatp.Api/Controllers/ProviderLocationsBulkDeleteController.cs
+++ src/SFA.DAS.Roatp.Api/Controllers/ProviderLocationsBulkDeleteController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SFA.DAS.Roatp.Api.Models;
-using SFA.DAS.Roatp.Application.Locations.Commands.BulkInsert;
+using SFA.DAS.Roatp.Application.Locations.Commands.BulkDelete;
 using SFA.DAS.Roatp.Application.Locations.Queries;
 
 namespace SFA.DAS.Roatp.Api.Controllers
@@ -27,10 +27,10 @@ namespace SFA.DAS.Roatp.Api.Controllers
         [Route("/providers/{ukprn}/locations/cleanup")]
         [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(typeof(string), StatusCodes.Status204NoContent)]
-        public async Task<ActionResult<List<ProviderLocationModel>>> BulkDeleteProviderLocations([FromRoute] int ukprn,  ProviderLocationsInsertModel providerLocationsInsertModel)
+        public async Task<ActionResult<List<ProviderLocationModel>>> BulkDeleteProviderLocations([FromRoute] int ukprn,  ProviderLocationsDeleteModel providerLocationsDeleteModel)
         {
-            _logger.LogInformation("Inner API: Request received to bulk insert locations ukprn: {ukprn} larscode: {larscode} userid:{userid}", ukprn, providerLocationsInsertModel.LarsCode, providerLocationsInsertModel.UserId);
-            var command = (BulkInsertProviderLocationsCommand)providerLocationsInsertModel;
+            _logger.LogInformation("Inner API: Request received to bulk insert locations ukprn: {ukprn} larscode: {larscode} userid:{userid}", ukprn, providerLocationsDeleteModel.LarsCode, providerLocationsDeleteModel.UserId);
+            var command = (BulkDeleteProviderLocationsCommand)providerLocationsDeleteModel;
             command.Ukprn = ukprn;
             await _mediator.Send(command);
             return NoContent();
