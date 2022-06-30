2eeb66dcb1fb6aed3efe158008fe78b9e86f465a
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2022-06-30 17:19:21 +0100

    CSP-Shared_89_114 RegionReadRepository added and script update for regions to include Enfield (#47)

--- src/SFA.DAS.Roatp.Data/Extensions/ServiceCollectionExtensions.cs
+++ src/SFA.DAS.Roatp.Data/Extensions/ServiceCollectionExtensions.cs
@@ -39,6 +39,7 @@ namespace SFA.DAS.Roatp.Data.Extensions
             services.AddTransient<IStandardReadRepository, StandardReadRepository>();
             services.AddTransient<IProviderReadRepository, ProviderReadRepository>();
             services.AddTransient<IProviderCourseReadRepository, ProviderCourseReadRepository>();
+            services.AddTransient<IRegionReadRepository, RegionReadRepository>();
             services.AddTransient<ICreateProviderRepository, CreateProviderRepository>();
             services.AddTransient<IReloadStandardsRepository, ReloadStandardsRepository>();
             services.AddTransient<IGetStandardsCountRepository, GetStandardsCountRepository>();
