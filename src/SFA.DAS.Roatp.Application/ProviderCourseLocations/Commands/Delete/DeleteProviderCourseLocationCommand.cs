df2c420207ec0cdbe8b2c9fd6fac3e5f44785ba5
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-07-07 18:40:14 +0100

    CSP-135 provider course location delete endpoint added

---
+++ src/SFA.DAS.Roatp.Application/ProviderCourseLocations/Commands/Delete/DeleteProviderCourseLocationCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using SFA.DAS.Roatp.Application.Common;
+
+namespace SFA.DAS.Roatp.Application.ProviderCourseLocations.Commands.Delete
+{
+    public class DeleteProviderCourseLocationCommand : IRequest<Unit>, ILarsCode, IUkprn
+    {
+        public int Ukprn { get; }
+        public int LarsCode { get; }
+        public int ProviderCourseLocationId { get; set; }
+        public string UserId { get; }
+        public DeleteProviderCourseLocationCommand(int ukprn, int larsCode, int providerCourseLocationId, string userId)
+        {
+            Ukprn = ukprn;
+            LarsCode = larsCode;
+            ProviderCourseLocationId = providerCourseLocationId;
+            UserId = userId;
+        }
+    }
+}
