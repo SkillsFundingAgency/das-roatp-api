0d78b7f0d1383a12d02f035bf8bd06745dcff563
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-07-18 23:35:08 +0100

    CSP-135 unit tests added

---
+++ src/SFA.DAS.Roatp.Application.UnitTests/ProviderCourseLocations/Commands/Delete/BulkDeleteProviderCourseLocationsCommandHandlerTests.cs
@@ -0,0 +1,47 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Roatp.Application.ProviderCourseLocations.Commands.BulkDelete;
+using SFA.DAS.Roatp.Application.ProviderCourseLocations.Commands.Delete;
+using SFA.DAS.Roatp.Domain.Entities;
+using SFA.DAS.Roatp.Domain.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Roatp.Application.UnitTests.ProviderCourseLocations.Commands.Delete
+{
+    [TestFixture]
+    public class DeleteProviderCourseLocationCommandHandlerTests
+    {
+        private Mock<IProviderCourseLocationsDeleteRepository> _providerCourseLocationDeleteRepositoryMock;
+        private Mock<IProviderCourseLocationReadRepository> _providerCourseLocationReadRepositoryMock;
+        private DeleteProviderCourseLocationCommandHandler _sut;
+
+        private readonly ProviderCourseLocation _regionalLocation = new ProviderCourseLocation { Id = 1, Location = new ProviderLocation { LocationType = LocationType.Regional } };
+        private readonly ProviderCourseLocation _nationalLocation = new ProviderCourseLocation { Id = 2, Location = new ProviderLocation { LocationType = LocationType.National } };
+        private readonly ProviderCourseLocation _providerLocation = new ProviderCourseLocation { Id = 3, Location = new ProviderLocation { LocationType = LocationType.Provider } };
+
+
+        [SetUp]
+        public void Before_Each_Test()
+        {
+            _providerCourseLocationDeleteRepositoryMock = new Mock<IProviderCourseLocationsDeleteRepository>();
+            _providerCourseLocationReadRepositoryMock = new Mock<IProviderCourseLocationReadRepository>();
+            _sut = new DeleteProviderCourseLocationCommandHandler(_providerCourseLocationDeleteRepositoryMock.Object, Mock.Of<ILogger<DeleteProviderCourseLocationCommandHandler>>());
+        }
+
+        [Test, AutoData]
+        public async Task Handler_NoCourseLocations_ReturnsZero(DeleteProviderCourseLocationCommand request)
+        {
+            var result = await _sut.Handle(request, new CancellationToken());
+
+            result.Should().Be(Unit.Value);
+            _providerCourseLocationDeleteRepositoryMock.Verify(d => d.Delete(It.IsAny<int>()), Times.Once);
+        }
+    }
+}
---
+++ src/SFA.DAS.Roatp.Application.UnitTests/ProviderCourseLocations/Commands/Delete/BulkDeleteProviderCourseLocationsCommandValidatorTests.cs
@@ -0,0 +1,80 @@
+﻿using FluentValidation.TestHelper;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Roatp.Application.ProviderCourseLocations.Commands.Delete;
+using SFA.DAS.Roatp.Domain.Interfaces;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Roatp.Application.UnitTests.ProviderCourseLocations.Commands.Delete
+{
+    [TestFixture]
+    public class DeleteProviderCourseLocationCommandValidatorTests
+    {
+        private readonly string _userId = "userid";
+        [Test]
+        public async Task ValidateId_LessThanZero_ReturnsError()
+        {
+            var command = new DeleteProviderCourseLocationCommand(10012002, 123, 0, _userId);
+
+            var sut = new DeleteProviderCourseLocationCommandValidator(Mock.Of<IProviderReadRepository>(), Mock.Of<IProviderCourseReadRepository>(), Mock.Of<IProviderCourseLocationReadRepository>());
+
+            var result = await sut.TestValidateAsync(command);
+
+            result.ShouldHaveValidationErrorFor(c => c.Id);
+        }
+
+       
+        [Test]
+        public async Task ValidateUkprn_InValid_ReturnsError()
+        {
+            var command = new DeleteProviderCourseLocationCommand(10012002, 123, 1, _userId);
+
+            var _providerCourseLocationReadRepositoryMock = new Mock<IProviderCourseLocationReadRepository>();
+            _providerCourseLocationReadRepositoryMock
+                .Setup(x => x.GetAllProviderCourseLocations(It.IsAny<int>(), It.IsAny<int>()))
+                .ReturnsAsync(new System.Collections.Generic.List<Domain.Entities.ProviderCourseLocation> { new Domain.Entities.ProviderCourseLocation { Id = 1 } });
+
+            var sut = new DeleteProviderCourseLocationCommandValidator(Mock.Of<IProviderReadRepository>(), Mock.Of<IProviderCourseReadRepository>(), _providerCourseLocationReadRepositoryMock.Object);
+
+            var result = await sut.TestValidateAsync(command);
+
+            result.ShouldHaveValidationErrorFor(c => c.Ukprn);
+        }
+
+        [Test]
+        public async Task ValidateLarsCode_InValid_ReturnsError()
+        {
+            var command = new DeleteProviderCourseLocationCommand(10012002, 123, 1, _userId);
+
+            var _providerCourseLocationReadRepositoryMock = new Mock<IProviderCourseLocationReadRepository>();
+            _providerCourseLocationReadRepositoryMock
+                .Setup(x => x.GetAllProviderCourseLocations(It.IsAny<int>(), It.IsAny<int>()))
+                .ReturnsAsync(new System.Collections.Generic.List<Domain.Entities.ProviderCourseLocation> { new Domain.Entities.ProviderCourseLocation { Id = 1 } });
+
+            var sut = new DeleteProviderCourseLocationCommandValidator(Mock.Of<IProviderReadRepository>(), Mock.Of<IProviderCourseReadRepository>(), _providerCourseLocationReadRepositoryMock.Object);
+
+            var result = await sut.TestValidateAsync(command);
+
+            result.ShouldHaveValidationErrorFor(c => c.LarsCode);
+        }
+
+        [TestCase("")]
+        [TestCase(null)]
+        [TestCase(" ")]
+        public async Task ValidateUserId_Empty_ReturnsError(string userId)
+        {
+            var command = new DeleteProviderCourseLocationCommand(10012002, 123, 1, userId);
+
+            var _providerCourseLocationReadRepositoryMock = new Mock<IProviderCourseLocationReadRepository>();
+            _providerCourseLocationReadRepositoryMock
+                .Setup(x => x.GetAllProviderCourseLocations(It.IsAny<int>(), It.IsAny<int>()))
+                .ReturnsAsync(new System.Collections.Generic.List<Domain.Entities.ProviderCourseLocation> { new Domain.Entities.ProviderCourseLocation { Id = 1 } });
+
+            var sut = new DeleteProviderCourseLocationCommandValidator(Mock.Of<IProviderReadRepository>(), Mock.Of<IProviderCourseReadRepository>(), _providerCourseLocationReadRepositoryMock.Object);
+
+            var result = await sut.TestValidateAsync(command);
+
+            result.ShouldHaveValidationErrorFor(c => c.UserId);
+        }
+    }
+}
