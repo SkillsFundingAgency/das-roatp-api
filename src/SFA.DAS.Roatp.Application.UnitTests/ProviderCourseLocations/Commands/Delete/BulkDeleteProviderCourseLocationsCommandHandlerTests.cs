12f740a65db4560ca28bfb7d4d81dac09192971d
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-07-20 09:14:09 +0100

    CSP-135 unsed code removed

--- src/SFA.DAS.Roatp.Application.UnitTests/ProviderCourseLocations/Commands/Delete/BulkDeleteProviderCourseLocationsCommandHandlerTests.cs
+++ src/SFA.DAS.Roatp.Application.UnitTests/ProviderCourseLocations/Commands/Delete/BulkDeleteProviderCourseLocationsCommandHandlerTests.cs
@@ -4,12 +4,9 @@ using MediatR;
 using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
-using SFA.DAS.Roatp.Application.ProviderCourseLocations.Commands.BulkDelete;
 using SFA.DAS.Roatp.Application.ProviderCourseLocations.Commands.Delete;
 using SFA.DAS.Roatp.Domain.Entities;
 using SFA.DAS.Roatp.Domain.Interfaces;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -19,19 +16,12 @@ namespace SFA.DAS.Roatp.Application.UnitTests.ProviderCourseLocations.Commands.D
     public class DeleteProviderCourseLocationCommandHandlerTests
     {
         private Mock<IProviderCourseLocationsDeleteRepository> _providerCourseLocationDeleteRepositoryMock;
-        private Mock<IProviderCourseLocationReadRepository> _providerCourseLocationReadRepositoryMock;
         private DeleteProviderCourseLocationCommandHandler _sut;
-
-        private readonly ProviderCourseLocation _regionalLocation = new ProviderCourseLocation { Id = 1, Location = new ProviderLocation { LocationType = LocationType.Regional } };
-        private readonly ProviderCourseLocation _nationalLocation = new ProviderCourseLocation { Id = 2, Location = new ProviderLocation { LocationType = LocationType.National } };
-        private readonly ProviderCourseLocation _providerLocation = new ProviderCourseLocation { Id = 3, Location = new ProviderLocation { LocationType = LocationType.Provider } };
-
 
         [SetUp]
         public void Before_Each_Test()
         {
             _providerCourseLocationDeleteRepositoryMock = new Mock<IProviderCourseLocationsDeleteRepository>();
-            _providerCourseLocationReadRepositoryMock = new Mock<IProviderCourseLocationReadRepository>();
             _sut = new DeleteProviderCourseLocationCommandHandler(_providerCourseLocationDeleteRepositoryMock.Object, Mock.Of<ILogger<DeleteProviderCourseLocationCommandHandler>>());
         }
 
