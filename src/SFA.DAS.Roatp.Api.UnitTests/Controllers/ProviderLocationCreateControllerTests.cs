9c7f40fea97bd349815cdd3abcfddd2d28c3466c
Deven Shah <devenshah1974@gmail.com>
2022-07-24 00:16:40 +0100

    CSP-48 Added controller action to create provider location

---
+++ src/SFA.DAS.Roatp.Api.UnitTests/Controllers/ProviderLocationCreateControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Roatp.Api.Controllers;
+using SFA.DAS.Roatp.Api.Models;
+using SFA.DAS.Roatp.Application.Locations.Commands.CreateLocation;
+using SFA.DAS.Testing.AutoFixture;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Roatp.Api.UnitTests.Controllers
+{
+    [TestFixture]
+    public class ProviderLocationCreateControllerTests
+    {
+        [Test, MoqAutoData]
+        public async Task CreateLocaiton_InvokesCommand(
+            [Frozen] Mock<IMediator> mediatorMock,
+            [Greedy] ProviderLocationCreateController sut,
+            int ukprn,
+            ProviderLocationCreateModel model)
+        {
+            var response = await sut.CreateLocation(ukprn, model);
+
+            mediatorMock.Verify(m => m.Send(It.Is<CreateProviderLocationCommand>(c => c.Ukprn == ukprn), It.IsAny<CancellationToken>()));
+            var result = (StatusCodeResult)response;
+            result.Should().NotBeNull();
+            result.StatusCode.Should().Be(StatusCodes.Status201Created);
+        }
+    }
+}
