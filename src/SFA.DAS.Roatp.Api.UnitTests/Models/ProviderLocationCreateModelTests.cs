e2a5a339b1974c3360da66aea58886eef52f14d2
Deven Shah <devenshah1974@gmail.com>
2022-07-24 00:07:48 +0100

    CSP-48 Implemented create provider location model and command

---
+++ src/SFA.DAS.Roatp.Api.UnitTests/Models/ProviderLocationCreateModelTests.cs
@@ -0,0 +1,20 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Roatp.Api.Models;
+using SFA.DAS.Roatp.Application.Locations.Commands.CreateLocation;
+
+namespace SFA.DAS.Roatp.Api.UnitTests.Models
+{
+    [TestFixture]
+    public class ProviderLocationCreateModelTests
+    {
+        [Test, AutoData]
+        public void Operator_ReturnsCommand(ProviderLocationCreateModel sut)
+        {
+            var command = (CreateProviderLocationCommand)sut;
+            Assert.That(command, Is.Not.Null);
+            command.Should().BeEquivalentTo(sut);
+        }
+    }
+}
