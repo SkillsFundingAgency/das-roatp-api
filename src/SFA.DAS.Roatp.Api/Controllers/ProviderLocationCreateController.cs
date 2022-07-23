835e7fb9bafe6b8e63f54adaa6c23d4822d49283
Deven Shah <devenshah1974@gmail.com>
2022-07-24 00:16:40 +0100

    CSP-48 Added controller action to create provider location

---
+++ src/SFA.DAS.Roatp.Api/Controllers/ProviderLocationCreateController.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Roatp.Api.Models;
+using SFA.DAS.Roatp.Application.Locations.Commands.CreateLocation;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Roatp.Api.Controllers
+{
+    [ApiController]
+    public class ProviderLocationCreateController : ControllerBase
+    {
+        private readonly ILogger<ProviderLocationCreateController> _logger;
+        private readonly IMediator _mediator;
+
+        public ProviderLocationCreateController(IMediator mediator, ILogger<ProviderLocationCreateController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("/providers/{ukprn}/locations")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status201Created)]
+        public async Task<IActionResult> CreateLocation([FromRoute] int ukprn, ProviderLocationCreateModel model)
+        {
+            _logger.LogInformation("Request to save Provider Location {locationName} for Ukprn {ukprn}", model.LocationName, ukprn);
+
+            var command = (CreateProviderLocationCommand)model;
+            command.Ukprn = ukprn;
+
+            await _mediator.Send(command);
+
+            return new StatusCodeResult(StatusCodes.Status201Created);
+        }
+    }
+}
