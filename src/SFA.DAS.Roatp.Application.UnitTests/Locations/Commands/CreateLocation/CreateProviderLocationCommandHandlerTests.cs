0f50cfe3e24fb52418c01f9aba2eff987249b4cd
Deven Shah <devenshah1974@gmail.com>
2022-07-24 00:49:09 +0100

    CSP-48 Created handler for create location requests

---
+++ src/SFA.DAS.Roatp.Application.UnitTests/Locations/Commands/CreateLocation/CreateProviderLocationCommandHandlerTests.cs
@@ -0,0 +1,54 @@
+﻿using AutoFixture;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Roatp.Application.Locations.Commands.CreateLocation;
+using SFA.DAS.Roatp.Domain.Entities;
+using SFA.DAS.Roatp.Domain.Interfaces;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Roatp.Application.UnitTests.Locations.Commands.CreateLocation
+{
+    [TestFixture]
+    public class CreateProviderLocationCommandHandlerTests
+    {
+        private Mock<IProviderReadRepository> _providerReadRepositoryMock;
+        private Mock<IProviderLocationWriteRepository> _providerLocationsWriteRepositoryMock;
+        private CreateProviderLocationCommandHandler _sut;
+        private CreateProviderLocationCommand _command;
+        private Provider _provider;
+
+        [SetUp]
+        public async Task Before_Each_Test()
+        {
+            var fixture = new Fixture();
+            fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList().ForEach(b => fixture.Behaviors.Remove(b));
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+            _command = fixture.Create<CreateProviderLocationCommand>();
+            _provider = fixture.Create<Provider>();
+
+            _providerReadRepositoryMock = new Mock<IProviderReadRepository>();
+            _providerReadRepositoryMock.Setup(r => r.GetByUkprn(_command.Ukprn)).ReturnsAsync(_provider);
+
+            _providerLocationsWriteRepositoryMock = new Mock<IProviderLocationWriteRepository>();
+            _sut = new CreateProviderLocationCommandHandler(_providerReadRepositoryMock.Object, _providerLocationsWriteRepositoryMock.Object, Mock.Of<ILogger<CreateProviderLocationCommandHandler>>());
+
+            await _sut.Handle(_command, new CancellationToken());
+        }
+
+        [Test]
+        public void ThenGetsProviderUringUkprn()
+        {
+            _providerReadRepositoryMock.Verify(r => r.GetByUkprn(_command.Ukprn));
+        }
+
+        [Test]
+        public void ThenConvertsCommandToEntityAndCallsCreateRepository()
+        {
+            _providerLocationsWriteRepositoryMock.Verify(r => r.Create(It.Is<ProviderLocation>(p => p.ProviderId == _provider.Id && p.LocationName == _command.LocationName)));
+        }
+    }
+}
