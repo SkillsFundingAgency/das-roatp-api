a4e892235e173f1f4094bfb0100f4ba81308ee5a
Deven Shah <devenshah1974@gmail.com>
2022-07-29 11:17:48 +0100

    CSP-29 Implements get all standards endpoint

---
+++ src/SFA.DAS.Roatp.Api.UnitTests/Controllers/StandardsControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Roatp.Api.Controllers;
+using SFA.DAS.Roatp.Application.Standards.Queries;
+using SFA.DAS.Roatp.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Roatp.Api.UnitTests.Controllers
+{
+    [TestFixture]
+    public class StandardsControllerTests
+    {
+        [Test, AutoData]
+        public async Task GetAllStandards_ReturnsListOfStandards(List<Standard> standards)
+        {
+            var mediatorMock = new Mock<IMediator>();
+            mediatorMock.Setup(r => r.Send(It.IsAny<GetAllStandardsQuery>(), It.IsAny<CancellationToken>())).ReturnsAsync(new GetAllStandardsQueryResult(standards));
+            var sut = new StandardsController(Mock.Of<ILogger<StandardsController>>(), mediatorMock.Object);
+
+            var response = await sut.GetAllStandards();
+
+            var result = response.Result as OkObjectResult;
+            result.Should().NotBeNull();
+            var queryResult = result.Value as GetAllStandardsQueryResult;
+            queryResult.Standards.Should().BeEquivalentTo(standards);
+        }
+    }
+}
