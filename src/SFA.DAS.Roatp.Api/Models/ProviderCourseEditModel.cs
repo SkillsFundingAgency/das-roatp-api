1082ce1a15ec28cc2bbd1a56923e1a20d5c87cd4
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-08-04 10:45:27 +0100

    CSP-165 models names changed

--- src/SFA.DAS.Roatp.Api/Models/ProviderCourseEditModel.cs
+++ src/SFA.DAS.Roatp.Api/Models/ProviderCourseEditModel.cs
@@ -1,22 +1,24 @@
-﻿using SFA.DAS.Roatp.Application.Locations.Commands.UpdateProviderLocationDetails;
+﻿using SFA.DAS.Roatp.Application.ProviderCourse.Commands.UpdateProviderCourse;
 
 namespace SFA.DAS.Roatp.Api.Models
 {
-    public class ProviderLocationEditModel
+    public class ProviderCourseEditModel
     {
         public string UserId { get; set; }
-        public string LocationName { get; set; }
-        public string Website { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
-        public static implicit operator UpdateProviderLocationDetailsCommand(ProviderLocationEditModel model) =>
-            new UpdateProviderLocationDetailsCommand
+        public string ContactUsEmail { get; set; }
+        public string ContactUsPhoneNumber { get; set; }
+        public string ContactUsPageUrl { get; set; }
+        public string StandardInfoUrl { get; set; }
+        public bool? IsApprovedByRegulator { get; set; }
+        public static implicit operator UpdateProviderCourseCommand(ProviderCourseEditModel model) =>
+            new UpdateProviderCourseCommand
             {
                 UserId = model.UserId,
-                LocationName = model.LocationName,
-                Website = model.Website,
-                Email = model.Email,
-                Phone = model.Phone,
+                ContactUsEmail = model.ContactUsEmail,
+                ContactUsPhoneNumber = model.ContactUsPhoneNumber,
+                StandardInfoUrl = model.StandardInfoUrl,
+                ContactUsPageUrl = model.ContactUsPageUrl,
+                IsApprovedByRegulator = model.IsApprovedByRegulator
             };
     }
 }
