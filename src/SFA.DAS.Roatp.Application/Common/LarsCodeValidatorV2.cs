d86f757bd3bfe7a88544cca402a6671585d90d8b
Deven Shah <devenshah1974@gmail.com>
2022-08-17 17:58:30 +0100

    CSP-215 Implements new lars code v2 validator with additional checks

---
+++ src/SFA.DAS.Roatp.Application/Common/LarsCodeValidatorV2.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using SFA.DAS.Roatp.Domain.Interfaces;
+
+namespace SFA.DAS.Roatp.Application.Common
+{
+    public class LarsCodeValidatorV2 : AbstractValidator<ILarsCode>
+    {
+        public const string InvalidMessage = "Larscode must be greater than zero";
+        public const string NotFoundMessage = "Larscode not found";
+        public const string CombinationAlreadyExistsMessage = "Ukprn and LarsCode combination already exists";
+        public const string CombinationNotFoundErrorMessage = "Invalid Ukprn and LarsCode, combination not found";
+        public LarsCodeValidatorV2(IStandardReadRepository standardReadRepository, IProviderCourseReadRepository providerCourseReadRepository, bool expectProviderCourseToExist)
+        {
+            RuleFor(x => x.LarsCode)
+                .Cascade(CascadeMode.Stop)
+                .GreaterThan(0)
+                .WithMessage(InvalidMessage)
+                .MustAsync(async (larsCode, cancellation) =>
+                {
+                    var standard = await standardReadRepository.GetStandard(larsCode);
+                    return standard != null;
+                })
+                .WithMessage(NotFoundMessage)
+               .MustAsync(async (model, larsCode, cancellation) =>
+               {
+                   var providerCourse = await providerCourseReadRepository.GetProviderCourseByUkprn(model.Ukprn, larsCode);
+                   return providerCourse != null;
+               })
+               .WithMessage(CombinationNotFoundErrorMessage)
+               .When(_ => expectProviderCourseToExist, ApplyConditionTo.CurrentValidator)
+               .MustAsync(async (model, larsCode, cancellation) =>
+               {
+                   var providerCourse = await providerCourseReadRepository.GetProviderCourseByUkprn(model.Ukprn, larsCode);
+                   return providerCourse == null;
+               })
+               .WithMessage(CombinationAlreadyExistsMessage)
+               .When(_ => !expectProviderCourseToExist, ApplyConditionTo.CurrentValidator);
+        }
+    }
+}
