82abeca2d09228ecedad8a09d294c688eebf7c58
Deven Shah <devenshah1974@gmail.com>
2022-08-24 10:29:42 +0100

    CSP-186 Updated create course handler to add regions

---
+++ src/SFA.DAS.Roatp.Application.UnitTests/ProviderCourse/Commands/CreateProviderCourse/CreateProviderCourseHandlerTests/CreateProviderCourseHandlerWhenEmployerLocationOptionWithRegionsTests.cs
@@ -0,0 +1,64 @@
+﻿using AutoFixture.NUnit3;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Roatp.Application.ProviderCourse.Commands.CreateProviderCourse;
+using SFA.DAS.Roatp.Domain.Entities;
+using SFA.DAS.Roatp.Domain.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Roatp.Application.UnitTests.ProviderCourse.Commands.CreateProviderCourse.CreateProviderCourseHandlerTests
+{
+    public class CreateProviderCourseHandlerWhenEmployerLocationOptionWithRegionsTests
+    {
+        [Test, RecursiveMoqAutoData]
+        public async Task Handle_HasSelectedExistingSubregions_UsesExistingProviderLocationId(
+        [Frozen] Mock<IProviderReadRepository> providerReadRepositoryMock,
+        [Frozen] Mock<IProviderLocationsReadRepository> providerLocationsReadRepositoryMock,
+        [Frozen] Mock<IProviderCourseEditRepository> providerCourseEditRepositoryMock,
+        CreateProviderCourseCommandHandler sut,
+        CreateProviderCourseCommand command,
+        Provider provider,
+        ProviderLocation providerLocation)
+        {
+            providerLocation.LocationType = LocationType.Regional;
+            command.HasNationalDeliveryOption = false;
+            command.ProviderLocations = null;
+            command.SubregionIds = new List<int> { providerLocation.RegionId.GetValueOrDefault() };
+            providerReadRepositoryMock.Setup(p => p.GetByUkprn(command.Ukprn)).ReturnsAsync(provider);
+            providerLocationsReadRepositoryMock.Setup(p => p.GetAllProviderLocations(command.Ukprn)).ReturnsAsync(new List<ProviderLocation>() { providerLocation });
+
+            await sut.Handle(command, new CancellationToken());
+
+            providerCourseEditRepositoryMock.Verify(p => p.CreateProviderCourse(It.Is<Domain.Entities.ProviderCourse>(c => c.ProviderId == provider.Id && c.Locations.Count == 1 && c.Locations.First().ProviderLocationId == providerLocation.Id && c.Locations.First().Location == null)));
+        }
+
+        [Test, RecursiveMoqAutoData]
+        public async Task Handle_HasSelectedNonExistingSubregions_CreatesNewProviderLocation(
+            [Frozen] Mock<IProviderReadRepository> providerReadRepositoryMock,
+            [Frozen] Mock<IProviderLocationsReadRepository> providerLocationsReadRepositoryMock,
+            [Frozen] Mock<IProviderCourseEditRepository> providerCourseEditRepositoryMock,
+            [Frozen] Mock<IRegionReadRepository> regionReadRepositoryMock,
+            CreateProviderCourseCommandHandler sut,
+            CreateProviderCourseCommand command,
+            Provider provider,
+            ProviderLocation providerLocation,
+            Region region)
+        {
+            providerLocation.LocationType = LocationType.Regional;
+            command.HasNationalDeliveryOption = false;
+            command.ProviderLocations = null;
+            command.SubregionIds = new List<int> { region.Id };
+            regionReadRepositoryMock.Setup(r => r.GetAllRegions()).ReturnsAsync(new List<Region> { region });
+            providerReadRepositoryMock.Setup(p => p.GetByUkprn(command.Ukprn)).ReturnsAsync(provider);
+            providerLocationsReadRepositoryMock.Setup(p => p.GetAllProviderLocations(command.Ukprn)).ReturnsAsync(new List<ProviderLocation>() { providerLocation });
+
+            await sut.Handle(command, new CancellationToken());
+
+            providerCourseEditRepositoryMock.Verify(p => p.CreateProviderCourse(It.Is<Domain.Entities.ProviderCourse>(c => c.ProviderId == provider.Id && c.Locations.Count == 1 && c.Locations.First().Location != null)));
+        }
+    }
+}
--- src/SFA.DAS.Roatp.Application/ProviderCourse/Commands/CreateProviderCourse/CreateProviderCourseCommandHandler.cs
+++ src/SFA.DAS.Roatp.Application/ProviderCourse/Commands/CreateProviderCourse/CreateProviderCourseCommandHandler.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using SFA.DAS.Roatp.Domain.Entities;
 using SFA.DAS.Roatp.Domain.Interfaces;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,14 +15,21 @@ namespace SFA.DAS.Roatp.Application.ProviderCourse.Commands.CreateProviderCourse
         private readonly IProviderReadRepository _providerReadRepository;
         private readonly IProviderLocationsReadRepository _providerLocationsReadRepository;
         private readonly IProviderCourseEditRepository _providerCourseEditRepository;
+        private readonly IRegionReadRepository _regionReadRepository;
         private readonly ILogger<CreateProviderCourseCommandHandler> _logger;
 
-        public CreateProviderCourseCommandHandler(IProviderLocationsReadRepository providerLocationsReadRepository, IProviderReadRepository providerReadRepository, IProviderCourseEditRepository providerCourseEditRepository, ILogger<CreateProviderCourseCommandHandler> logger)
+        public CreateProviderCourseCommandHandler(
+            IProviderLocationsReadRepository providerLocationsReadRepository,
+            IProviderReadRepository providerReadRepository,
+            IProviderCourseEditRepository providerCourseEditRepository,
+            ILogger<CreateProviderCourseCommandHandler> logger,
+            IRegionReadRepository regionReadRepository)
         {
             _providerLocationsReadRepository = providerLocationsReadRepository;
             _providerReadRepository = providerReadRepository;
             _providerCourseEditRepository = providerCourseEditRepository;
             _logger = logger;
+            _regionReadRepository = regionReadRepository;
         }
 
         public async Task<int> Handle(CreateProviderCourseCommand request, CancellationToken cancellationToken)
@@ -32,31 +40,63 @@ namespace SFA.DAS.Roatp.Application.ProviderCourse.Commands.CreateProviderCourse
 
             Domain.Entities.ProviderCourse providerCourse = request;
             providerCourse.ProviderId = provider.Id;
+            var allProviderLocations = await _providerLocationsReadRepository.GetAllProviderLocations(request.Ukprn);
 
             if (request.HasNationalDeliveryOption)
             {
-                var allProviderLocations = await _providerLocationsReadRepository.GetAllProviderLocations(request.Ukprn);
-                var nationalLocation = allProviderLocations.FirstOrDefault(l => l.LocationType == LocationType.National);
-                if (nationalLocation == null)
-                {
-                    _logger.LogInformation("Provider:{ukprn} delivers course:{larscode} nationally, there is no national location, creating a new one", request.Ukprn, request.LarsCode);
-                    nationalLocation = ProviderLocation.CreateNationalLocation(provider.Id);
-                    providerCourse.Locations.Add(new ProviderCourseLocation { NavigationId = Guid.NewGuid(), Location = nationalLocation });
-                }
-                else
-                {
-                    _logger.LogInformation("Provider:{ukprn} delivers course:{larscode} nationally, existing national location id:{providerlocationid} will be used", request.Ukprn, request.LarsCode, nationalLocation.Id);
-                    providerCourse.Locations.Add(new ProviderCourseLocation { NavigationId = Guid.NewGuid(), ProviderLocationId = nationalLocation.Id });
-                }
+                AddNationaLocationToProviderCourse(request, provider, allProviderLocations, providerCourse);
+            }
+
+            //It is assumed here that HasNationalDeliveryOption is false otherwise the validator will throw exception
+            if(request.SubregionIds != null && request.SubregionIds.Count > 0)
+            {
+                await AddRegionalLocationsToProviderCourse(request, provider.Id, allProviderLocations, providerCourse);
             }
 
             await ProcessProviderLocations(request, providerCourse);
 
             await _providerCourseEditRepository.CreateProviderCourse(providerCourse);
-            
+
             _logger.LogInformation("Added course:{larscode} with id:{providercourseid} for provider: {ukprn}", request.LarsCode, providerCourse.Id, request.Ukprn);
             return providerCourse.Id;
         }
+
+        private async Task AddRegionalLocationsToProviderCourse(CreateProviderCourseCommand request, int providerId, List<ProviderLocation> allProviderLocations, Domain.Entities.ProviderCourse providerCourse)
+        {
+            var allRegions = await _regionReadRepository.GetAllRegions();
+
+            request.SubregionIds.ForEach(async regionId => 
+            {
+                var providerLocation = allProviderLocations.FirstOrDefault(l => l.RegionId == regionId);
+                if (providerLocation == null)
+                {
+                    var region = allRegions.FirstOrDefault(r => r.Id == regionId);
+                    providerLocation = ProviderLocation.CreateRegionalLocation(providerId, region);
+                    providerCourse.Locations.Add(new ProviderCourseLocation { NavigationId = Guid.NewGuid(), IsImported = false, Location = providerLocation });
+                }
+                else
+                {
+                    providerCourse.Locations.Add(new ProviderCourseLocation { NavigationId = Guid.NewGuid(), IsImported = false, ProviderLocationId = providerLocation.Id });
+                }
+            });
+        }
+
+        private void AddNationaLocationToProviderCourse(CreateProviderCourseCommand request, Provider provider, List<ProviderLocation> allProviderLocations, Domain.Entities.ProviderCourse providerCourse)
+        {
+            var nationalLocation = allProviderLocations.FirstOrDefault(l => l.LocationType == LocationType.National);
+            if (nationalLocation == null)
+            {
+                _logger.LogInformation("Provider:{ukprn} delivers course:{larscode} nationally, there is no national location, creating a new one", request.Ukprn, request.LarsCode);
+                nationalLocation = ProviderLocation.CreateNationalLocation(provider.Id);
+                providerCourse.Locations.Add(new ProviderCourseLocation { NavigationId = Guid.NewGuid(), Location = nationalLocation });
+            }
+            else
+            {
+                _logger.LogInformation("Provider:{ukprn} delivers course:{larscode} nationally, existing national location id:{providerlocationid} will be used", request.Ukprn, request.LarsCode, nationalLocation.Id);
+                providerCourse.Locations.Add(new ProviderCourseLocation { NavigationId = Guid.NewGuid(), ProviderLocationId = nationalLocation.Id });
+            }
+        }
+
 
         private async Task ProcessProviderLocations(CreateProviderCourseCommand request, Domain.Entities.ProviderCourse providerCourse)
         {
