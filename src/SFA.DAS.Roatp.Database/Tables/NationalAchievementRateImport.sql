73fe37b87685b4ae107b60f84316c7616a103d3b
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-09-28 18:00:07 +0100

    CSP-256 Import table and classes are renamed

--- src/SFA.DAS.Roatp.Database/Tables/NationalAchievementRateImport.sql
+++ src/SFA.DAS.Roatp.Database/Tables/NationalAchievementRateImport.sql
@@ -1,11 +1,12 @@
-﻿CREATE TABLE [dbo].[NationalAchievementRateOverall_Import]
+﻿CREATE TABLE [dbo].[NationalAchievementRateImport]
 (
 	[Id] BIGINT IDENTITY (1,1),
+	[Ukprn] INT NOT NULL,
 	[Age] SMALLINT NOT NULL DEFAULT 0,
 	[SectorSubjectArea] VARCHAR(1000) NOT NULL,
 	[ApprenticeshipLevel] SMALLINT NOT NULL DEFAULT 0,
 	[OverallCohort] INT NULL,
 	[OverallAchievementRate] decimal(10,4) NULL,
-	CONSTRAINT PK_NationalAchievementRateOverallImport PRIMARY KEY (Id)
+	CONSTRAINT PK_NationalAchievementRateImport PRIMARY KEY (Id)
 )
 GO
--- src/SFA.DAS.Roatp.Database/Tables/NationalAchievementRateOverallImport.sql
+++ src/SFA.DAS.Roatp.Database/Tables/NationalAchievementRateOverallImport.sql
@@ -1,12 +1,11 @@
-﻿CREATE TABLE [dbo].[NationalAchievementRate_Import]
+﻿CREATE TABLE [dbo].[NationalAchievementRateOverallImport]
 (
 	[Id] BIGINT IDENTITY (1,1),
-	[Ukprn] INT NOT NULL,
 	[Age] SMALLINT NOT NULL DEFAULT 0,
 	[SectorSubjectArea] VARCHAR(1000) NOT NULL,
 	[ApprenticeshipLevel] SMALLINT NOT NULL DEFAULT 0,
 	[OverallCohort] INT NULL,
 	[OverallAchievementRate] decimal(10,4) NULL,
-	CONSTRAINT PK_NationalAchievementRateImport PRIMARY KEY (Id)
+	CONSTRAINT PK_NationalAchievementRateOverallImport PRIMARY KEY (Id)
 )
 GO
