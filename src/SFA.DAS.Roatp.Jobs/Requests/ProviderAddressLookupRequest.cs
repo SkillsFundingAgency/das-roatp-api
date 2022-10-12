ac46b5a72523c3b875f14914bc7cabb365846c79
MarkFCain <mark.cain@digital.education.gov.uk>
2022-10-12 12:05:16 +0100

    CSP-258 added coverage

--- src/SFA.DAS.Roatp.Jobs/Requests/ProviderAddressLookupRequest.cs
+++ src/SFA.DAS.Roatp.Jobs/Requests/ProviderAddressLookupRequest.cs
@@ -6,6 +6,5 @@ namespace SFA.DAS.Roatp.Jobs.Requests
     public class ProviderAddressLookupRequest
     {
         public List<int> Ukprns { get; set; }
-        public DateTime? ProvidersUpdatedSince { get; set; }
     }
 }
