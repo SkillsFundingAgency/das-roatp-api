6503495e02efea0f05d1ae784141341b4ad5d05b
MarkFCain <mark.cain@digital.education.gov.uk>
2022-10-17 09:08:21 +0100

    CSP-258 caching partial code completion

--- src/SFA.DAS.Roatp.Jobs/Requests/ProviderAddressLookupRequest.cs
+++ src/SFA.DAS.Roatp.Jobs/Requests/ProviderAddressLookupRequest.cs
@@ -6,5 +6,6 @@ namespace SFA.DAS.Roatp.Jobs.Requests
     public class ProviderAddressLookupRequest
     {
         public List<int> Ukprns { get; set; }
+        public DateTime? ProvidersUpdatedSince { get; set; }
     }
 }
--- src/SFA.DAS.Roatp.Jobs/Services/ILoadUkrlpAddressesService.cs
+++ src/SFA.DAS.Roatp.Jobs/Services/ILoadUkrlpAddressesService.cs
@@ -1,4 +1,5 @@
-﻿using SFA.DAS.Roatp.Jobs.ApiModels.CourseDirectory;
+﻿using System;
+using SFA.DAS.Roatp.Jobs.ApiModels.CourseDirectory;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using SFA.DAS.Roatp.Domain.Entities;
@@ -8,5 +9,6 @@ namespace SFA.DAS.Roatp.Jobs.Services
     public interface ILoadUkrlpAddressesService
     { 
         Task<bool> LoadUkrlpAddresses();
+        public Task<bool> LoadUkrlpAddressesSinceLastUpdated();
     }
 }
