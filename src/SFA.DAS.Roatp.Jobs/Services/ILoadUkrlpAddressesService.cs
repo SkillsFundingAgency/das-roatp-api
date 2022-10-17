a62330e6146270859f2c50227b36ce2fcd433538
MarkFCain <mark.cain@digital.education.gov.uk>
2022-10-17 09:21:22 +0100

    CSP-258 reversing accidental inclusion of code from CSP-292

--- src/SFA.DAS.Roatp.Jobs/Services/ILoadUkrlpAddressesService.cs
+++ src/SFA.DAS.Roatp.Jobs/Services/ILoadUkrlpAddressesService.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using SFA.DAS.Roatp.Jobs.ApiModels.CourseDirectory;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using SFA.DAS.Roatp.Domain.Entities;
+﻿using System.Threading.Tasks;
 
 namespace SFA.DAS.Roatp.Jobs.Services
 {
     public interface ILoadUkrlpAddressesService
     { 
         Task<bool> LoadUkrlpAddresses();
-        public Task<bool> LoadUkrlpAddressesSinceLastUpdated();
     }
 }
