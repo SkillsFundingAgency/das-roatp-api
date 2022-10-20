4b25ca9c50a45deae7dc0ca5e529064b56b0c45c
MarkFCain <mark.cain@digital.education.gov.uk>
2022-10-20 09:57:19 +0100

    CSP-291 initial framework added

---
+++ src/SFA.DAS.Roatp.Jobs/Functions/UpdateProviderAddressCoordinatesFunction.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Azure.WebJobs;
+using SFA.DAS.Roatp.Jobs.Services;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace SFA.DAS.Roatp.Jobs.Functions
+{
+    public class UpdateProviderAddressCoordinatesFunction
+    {
+        private readonly IReloadStandardsCacheService _reloadStandardsCacheService;
+
+        public UpdateProviderAddressCoordinatesFunction(IReloadStandardsCacheService reloadStandardsCacheService)
+        {
+            _reloadStandardsCacheService = reloadStandardsCacheService;
+        }
+
+
+        [FunctionName(nameof(UpdateProviderAddressCoordinatesFunction))]
+        public async Task Run([TimerTrigger("%UpdateProviderAddressCoordinatesSchedule%",RunOnStartup = true)] TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation("UpdateProviderAddressCoordinatesFunction function started");
+
+            await _reloadStandardsCacheService.ReloadStandardsCache();
+
+            log.LogInformation("UpdateProviderAddressCoordinatesFunction complete");
+        }
+    }
+}
---
+++ src/SFA.DAS.Roatp.Jobs/Services/UpdateProviderAddressCoordinatesService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Roatp.Domain.Entities;
+using SFA.DAS.Roatp.Domain.Interfaces;
+using SFA.DAS.Roatp.Jobs.ApiClients;
+using SFA.DAS.Roatp.Jobs.ApiModels.Lookup;
+
+namespace SFA.DAS.Roatp.Jobs.Services;
+
+public class UpdateProviderAddressCoordinatesService : IUpdateProviderAddressCoordinatesService
+{
+    private readonly ICourseManagementOuterApiClient _courseManagementOuterApiClient;
+    private readonly IReloadStandardsRepository _reloadStandardsRepository;
+    private readonly IImportAuditWriteRepository _importAuditWriteRepository;
+    private readonly ILogger<ReloadStandardsCacheService> _logger;
+
+    public UpdateProviderAddressCoordinatesService(ILogger<ReloadStandardsCacheService> logger, ICourseManagementOuterApiClient courseManagementOuterApiClient, IReloadStandardsRepository reloadStandardsRepository, IImportAuditWriteRepository importAuditWriteRepository)
+    {
+        _logger = logger;
+        _courseManagementOuterApiClient = courseManagementOuterApiClient;
+        _reloadStandardsRepository = reloadStandardsRepository;
+        _importAuditWriteRepository = importAuditWriteRepository;
+    }
+
+    public async Task UpdateProviderAddressCoordinates()
+    {
+        var timeStarted = DateTime.UtcNow;
+        var (success, standardList) = await _courseManagementOuterApiClient.Get<StandardList>("lookup/standards");
+        if (!success || !standardList.Standards.Any())
+        {
+            _logger.LogError($"ReloadStandardsCacheFunction function failed to get active standards");
+            throw new InvalidOperationException("No standards were retrieved from courses api");
+        }
+
+        var standardsToReload = standardList.Standards.Select(standard => (Domain.Entities.Standard)standard).ToList();
+
+        await _reloadStandardsRepository.ReloadStandards(standardsToReload);
+
+        _logger.LogInformation("Standards reload complete");
+        await _importAuditWriteRepository.Insert(new ImportAudit(timeStarted, standardsToReload.Count, ImportType.Standards));
+    }
+}
