b298859d65558bed1bd2fbdcac0bb57380de3fa1
Deven Shah <devenshah1974@gmail.com>
2022-10-21 11:22:01 +0100

    CSP-262 Added external endpoint to get all provider courses

--- src/SFA.DAS.Roatp.Api/Controllers/ExternalReadControllers/ProvidersController.cs
+++ src/SFA.DAS.Roatp.Api/Controllers/ExternalReadControllers/ProvidersController.cs
@@ -1,8 +1,11 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SFA.DAS.Roatp.Application.ProviderCourse.Queries.GetProviderAllCourses;
 using SFA.DAS.Roatp.Application.Providers.Queries.GetProviders;
 using SFA.DAS.Roatp.Application.Providers.Queries.GetProviderSummary;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SFA.DAS.Roatp.Api.Controllers.ExternalReadControllers
@@ -36,6 +39,25 @@ namespace SFA.DAS.Roatp.Api.Controllers.ExternalReadControllers
             var providerResult = await _mediator.Send(new GetProviderSummaryQuery(ukprn));
             _logger.LogInformation("Provider summary data found for {ukprn}:", ukprn);
             return new OkObjectResult(providerResult);
+        }
+
+        /// <summary>
+        /// Gets all the courses for the given provider
+        /// </summary>
+        /// <param name="ukprn"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{ukprn}/courses")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(List<ProviderCourseModel>), 200)]
+        public async Task<ActionResult<List<ProviderCourseModel>>> GetAllProviderCourses(int ukprn)
+        {
+            var allCoursesResult = await _mediator.Send(new GetProviderAllCoursesQuery(ukprn));
+            var result = allCoursesResult.Courses;
+            _logger.LogInformation("Courses data found for {ukprn}", ukprn);
+            return new OkObjectResult(result);
         }
     }
 }
