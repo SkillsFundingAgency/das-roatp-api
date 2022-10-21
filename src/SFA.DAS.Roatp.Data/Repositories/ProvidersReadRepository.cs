a5e5ee2cb4bd9dc33296a7c3189162c13e2b2934
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-10-21 15:27:59 +0100

    Revert "CSP-329 added logging at repository level"
    This reverts commit 7102198b0ab47896d4860868fcc05f0edd1e13b4.

--- src/SFA.DAS.Roatp.Data/Repositories/ProvidersReadRepository.cs
+++ src/SFA.DAS.Roatp.Data/Repositories/ProvidersReadRepository.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using SFA.DAS.Roatp.Domain.Entities;
 using SFA.DAS.Roatp.Domain.Interfaces;
 
@@ -13,41 +11,23 @@ namespace SFA.DAS.Roatp.Data.Repositories
     internal class ProvidersReadRepository : IProvidersReadRepository
     {
         private readonly RoatpDataContext _roatpDataContext;
-        private readonly ILogger<ProvidersReadRepository> _logger;
 
-        public ProvidersReadRepository(RoatpDataContext roatpDataContext, ILogger<ProvidersReadRepository> logger)
+        public ProvidersReadRepository(RoatpDataContext roatpDataContext)
         {
             _roatpDataContext = roatpDataContext;
-            _logger = logger;
         }
         public async Task<Provider> GetByUkprn(int ukprn)
         {
-            try
-            {
-                return await _roatpDataContext.Providers
-                            .Include(p => p.ProviderAddress)
-                            .AsNoTracking().SingleOrDefaultAsync(p => p.Ukprn == ukprn);
-            }
-            catch(Exception e)
-            {
-                _logger.LogError("Error occured while getting provider {ukprn} error message : {error}", ukprn, e.Message);
-                return null;
-            }
+            return await _roatpDataContext.Providers
+                        .Include(p => p.ProviderAddress)
+                        .AsNoTracking().SingleOrDefaultAsync(p => p.Ukprn == ukprn);
         }
 
         public async Task<List<Provider>> GetAllProviders()
         {
-            try
-            {
-                return await _roatpDataContext.Providers
+            return await _roatpDataContext.Providers
                         .Include(p => p.ProviderAddress)
                         .AsNoTracking().ToListAsync();
-            }
-            catch (Exception e)
-            {
-                _logger.LogError("Error occured while getting all providers error message : {error}", e.Message);
-               return null;
-            }
         }
     }
 }
