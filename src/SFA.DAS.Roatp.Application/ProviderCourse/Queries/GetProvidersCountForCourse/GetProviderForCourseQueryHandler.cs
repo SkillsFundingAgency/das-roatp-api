5f3fca6a4106377a9e0739fc679a00752dc4f672
MarkFCain <mark.cain@digital.education.gov.uk>
2022-10-26 12:22:19 +0100

    CSP-268 first pass structural work

---
+++ src/SFA.DAS.Roatp.Application/ProviderCourse/Queries/GetProvidersCountForCourse/GetProviderForCourseQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.Roatp.Domain.Interfaces;
+
+namespace SFA.DAS.Roatp.Application.ProviderCourse.Queries.GetProvidersCountForCourse;
+
+public class GetProviderForCourseQueryHandler : IRequestHandler<GetProviderForCourseQuery, GetProviderForCourseQueryResult>
+{
+    private readonly IProviderCoursesReadRepository _providerCoursesReadRepository;
+
+    public GetProviderForCourseQueryHandler(IProviderCoursesReadRepository providerCoursesReadRepository)
+    {
+        _providerCoursesReadRepository = providerCoursesReadRepository;
+    }
+
+    public async Task<GetProviderForCourseQueryResult> Handle(GetProviderForCourseQuery request, CancellationToken cancellationToken)
+    {
+        // var result = await _providerCoursesReadRepository.GetProvidersCount(request.LarsCode);
+        return new GetProviderForCourseQueryResult { Ukprn = request.Ukprn};
+    }
+}
