d33118ccf291c98e78ae6f0ef81ad05b36ef716f
MarkFCain <mark.cain@digital.education.gov.uk>
2022-10-28 14:07:00 +0100

    CSP-268 national achievement rates and location and delivery details added

--- src/SFA.DAS.Roatp.Application/Courses/GetProviderDetailsForCourse/GetProviderDetailsForCourseQueryResult.cs
+++ src/SFA.DAS.Roatp.Application/Courses/GetProviderDetailsForCourse/GetProviderDetailsForCourseQueryResult.cs
@@ -6,6 +6,7 @@ namespace SFA.DAS.Roatp.Application.Courses.GetProviderDetailsForCourse
     public class GetProviderDetailsForCourseQueryResult
     {
         public int Ukprn { get; set; }
+        public int LarsCode { get; set; }
 
         public string Name { get; set; }
         public string TradingName { get; set; }
@@ -20,70 +21,44 @@ namespace SFA.DAS.Roatp.Application.Courses.GetProviderDetailsForCourse
         public string Address4 { get; set; }
         public string Town { get; set; }
         public string Postcode { get; set; }
-        public double DistanceInMiles { get; set; }
+       
 
-        public string StandardInfoUrl { get; set; }    //MFCMFC this is course information
+        public string StandardInfoUrl { get; set; }   
         public string MarketingInfo { get; set; }
         public double? Latitude { get; set; }
-        public double? Longitude { get; set; }     
+        public double? Longitude { get; set; }
+        public double DistanceInMiles { get; set; }
         public List<NationalAchievementRateModel> AchievementRates { get; set; } =
             new List<NationalAchievementRateModel>();
 
-        public List<GetDeliveryTypesResponse> DeliveryTypes { get; set; } = new List<GetDeliveryTypesResponse>();
+        public List<LocationAndDeliveryDetail> LocationAndDeliveryDetails { get; set; } = new List<LocationAndDeliveryDetail>();
 
-        // NOT REQUIRED
-        // public GetEmployerFeedbackResponse EmployerFeedback { get; set; }
+        public static implicit operator GetProviderDetailsForCourseQueryResult(ProviderAndCourseDetailsWithDistance providerAndCourseDetails)
+        {
+            if (providerAndCourseDetails == null)
+                return null;
 
-
-        public static implicit operator GetProviderDetailsForCourseQueryResult(ProviderDetailsWithDistance providerDetails) =>
-            new()
+            return new GetProviderDetailsForCourseQueryResult
             {
-                Ukprn = providerDetails.Ukprn,
-                Email = providerDetails.Email,
-                Phone = providerDetails.Phone,
-                Name = providerDetails.LegalName,
-                TradingName = providerDetails.TradingName,
-                MarketingInfo = providerDetails.MarketingInfo,
-                Address1 = providerDetails.AddressLine1,
-                Address2 = providerDetails.AddressLine2,
-                Address3 = providerDetails.AddressLine3,
-                Address4 = providerDetails.AddressLine4,
-                Town = providerDetails.Town,
-                Postcode = providerDetails.Postcode,
-                DistanceInMiles = providerDetails.Distance,
-                Latitude = providerDetails.Latitude,
-                Longitude = providerDetails.Longitude,
+                Ukprn = providerAndCourseDetails.Ukprn,
+                LarsCode = providerAndCourseDetails.LarsCode,
+                ContactUrl = providerAndCourseDetails.StandardContactUrl,
+                Email = providerAndCourseDetails.Email,
+                Phone = providerAndCourseDetails.Phone,
+                Name = providerAndCourseDetails.LegalName,
+                TradingName = providerAndCourseDetails.TradingName,
+                MarketingInfo = providerAndCourseDetails.MarketingInfo,
+                StandardInfoUrl = providerAndCourseDetails.StandardInfoUrl,
+                Address1 = providerAndCourseDetails.Address1,
+                Address2 = providerAndCourseDetails.Address2,
+                Address3 = providerAndCourseDetails.Address3,
+                Address4 = providerAndCourseDetails.Address4,
+                Town = providerAndCourseDetails.Town,
+                Postcode = providerAndCourseDetails.Postcode,
+                DistanceInMiles = providerAndCourseDetails.Distance,
+                Latitude = providerAndCourseDetails.Latitude,
+                Longitude = providerAndCourseDetails.Longitude
             };
-
-    }
-
-    public class GetDeliveryTypesResponse
-    {
-        public bool National { get; set; }
-
-        // convert to subclass and include lat/long, distance
-        public string Address1 { get; set; }
-        public string Address2 { get; set; }
-        public string Town { get; set; }
-        public string Postcode { get; set; }
-        public string County { get; set; }
-        public double DistanceInMiles { get; set; }
-
-
-        public string DeliveryModes { get; set; } // This is an amalgam?
-        // EXAMPLES: 	100PercentEmployer|DayRelease|BlockRelease
-        //              DayRelease
-        //              100PercentEmployer|DayRelease
-
-      // blockRelease bool
-      // blockrelease distance
-      // dayRelease bool
-      // day release distance
-      // regionname
-      // subregionname
-      // locationname
-      // LocationType
-
-      public int LocationId { get; set; }
+        }
     }
 }
--- src/SFA.DAS.Roatp.Domain/Entities/ProviderAndCourseDetailsWithDistance.cs
+++ src/SFA.DAS.Roatp.Domain/Entities/ProviderAndCourseDetailsWithDistance.cs
@@ -1,19 +1,22 @@
 ﻿namespace SFA.DAS.Roatp.Domain.Entities
 {
 
-    public class ProviderDetailsWithDistance
+    public class ProviderAndCourseDetailsWithDistance
     {
         public int Ukprn { get; set; }
+        public int LarsCode { get; set; }
         public string LegalName { get; set; }
         public string TradingName { get; set; }
         public string MarketingInfo { get; set; }
+        public string StandardInfoUrl { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
-        public string Website { get; set; }
-        public string AddressLine1 { get; set; }
-        public string AddressLine2 { get; set; }
-        public string AddressLine3 { get; set; }
-        public string AddressLine4 { get; set; }
+        public string StandardContactUrl { get; set; }
+        public string ProviderWebsite { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Address3 { get; set; }
+        public string Address4 { get; set; }
         public string Town { get; set; }
         public string Postcode { get; set; }
         public double? Latitude { get; set; }
