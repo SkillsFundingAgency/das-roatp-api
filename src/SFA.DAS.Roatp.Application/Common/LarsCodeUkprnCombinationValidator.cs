08c545741aba6606db15a621c3f5fbd816a3d9a6
MarkFCain <mark.cain@digital.education.gov.uk>
2022-11-16 14:27:57 +0000

    CSP-267 rearranged validators and updated coverage

--- src/SFA.DAS.Roatp.Application/Common/LarsCodeUkprnCombinationValidator.cs
+++ src/SFA.DAS.Roatp.Application/Common/LarsCodeUkprnCombinationValidator.cs
@@ -3,24 +3,14 @@ using SFA.DAS.Roatp.Domain.Interfaces;
 
 namespace SFA.DAS.Roatp.Application.Common
 {
-    public class LarsCodeUkprnAlreadyExistsValidator : AbstractValidator<ILarsCodeUkprn>
+    public class LarsCodeUkprnCombinationValidator : AbstractValidator<ILarsCodeUkprn>
     {
-        public const string InvalidMessage = "Larscode must be greater than zero";
-        public const string NotFoundMessage = "Larscode not found";
         public const string CombinationAlreadyExistsMessage = "Ukprn and LarsCode combination already exists";
         public const string CombinationNotFoundErrorMessage = "Invalid Ukprn and LarsCode, combination not found";
-        public LarsCodeUkprnAlreadyExistsValidator(IStandardsReadRepository standardsReadRepository, IProviderCoursesReadRepository providerCoursesReadRepository, bool expectProviderCourseToExist = false)
+        public LarsCodeUkprnCombinationValidator( IProviderCoursesReadRepository providerCoursesReadRepository, bool expectProviderCourseToExist = false)
         {
             RuleFor(x => x.LarsCode)
                 .Cascade(CascadeMode.Stop)
-                .GreaterThan(0)
-                .WithMessage(InvalidMessage)
-                .MustAsync(async (larsCode, cancellation) =>
-                {
-                    var standard = await standardsReadRepository.GetStandard(larsCode);
-                    return standard != null;
-                })
-                .WithMessage(NotFoundMessage)
                 .MustAsync(async (model, larsCode, cancellation) =>
                 {
                     var providerCourse = await providerCoursesReadRepository.GetProviderCourseByUkprn(model.Ukprn, larsCode);
