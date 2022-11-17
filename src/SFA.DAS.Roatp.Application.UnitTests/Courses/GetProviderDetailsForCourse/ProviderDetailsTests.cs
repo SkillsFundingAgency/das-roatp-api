4e2f633b2cb5a8900b330a4e147a694bf07cac42
MarkFCain <mark.cain@digital.education.gov.uk>
2022-11-17 11:03:55 +0000

    CSP-267 reworked providers endpoint to return less data, and fixed swagger issue

--- src/SFA.DAS.Roatp.Application.UnitTests/Courses/GetProviderDetailsForCourse/ProviderDetailsTests.cs
+++ src/SFA.DAS.Roatp.Application.UnitTests/Courses/GetProviderDetailsForCourse/ProviderDetailsTests.cs
@@ -18,11 +18,9 @@ namespace SFA.DAS.Roatp.Application.UnitTests.Courses.GetProviderDetailsForCours
             
             model.Should().BeEquivalentTo(modelBase, c => c
                 .Excluding(s => s.LegalName)
-                .Excluding(s => s.StandardContactUrl)
                 .Excluding(s => s.Distance)
             );
             Assert.AreEqual(modelBase.LegalName, model.Name);
-            Assert.AreEqual(modelBase.StandardContactUrl, model.ContactUrl); 
             Assert.AreEqual(modelBase.Distance, model.ProviderHeadOfficeDistanceInMiles);
         }
 
@@ -35,20 +33,11 @@ namespace SFA.DAS.Roatp.Application.UnitTests.Courses.GetProviderDetailsForCours
 
             model.Should().BeEquivalentTo(summaryModel, c => c
                 .Excluding(s => s.LegalName)
-                .Excluding(s => s.StandardContactUrl)
                 .Excluding(s => s.Distance)
                 .Excluding(s=>s.ProviderId)
             );
             Assert.AreEqual(summaryModel.LegalName, model.Name);
-            Assert.AreEqual(summaryModel.StandardContactUrl, model.ContactUrl);
             Assert.AreEqual(summaryModel.Distance, model.ProviderHeadOfficeDistanceInMiles);
-        }
-
-        [Test]
-        public void Operator_PopulatesModelFromSummaryModelNull()
-        { 
-            var model = (ProviderDetails)(ProviderCourseDetailsSummaryModel)null;
-            Assert.That(model, Is.Null);
         }
 
         [Test, RecursiveMoqAutoData]
--- src/SFA.DAS.Roatp.Domain/Models/ProviderCourseDetailsModel.cs
+++ src/SFA.DAS.Roatp.Domain/Models/ProviderCourseDetailsModel.cs
@@ -3,6 +3,16 @@
     public class ProviderCourseDetailsModel : ProviderCourseDetailsModelBase
     {
         public string MarketingInfo { get; set; }
+        public string StandardInfoUrl { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string StandardContactUrl { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Address3 { get; set; }
+        public string Address4 { get; set; }
+        public string Town { get; set; }
+        public string Postcode { get; set; }
     }
     public class ProviderCourseDetailsSummaryModel: ProviderCourseDetailsModelBase
     {
@@ -15,16 +25,7 @@
         public string LegalName { get; set; }
         public string TradingName { get; set; }
       
-        public string StandardInfoUrl { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
-        public string StandardContactUrl { get; set; }
-        public string Address1 { get; set; }
-        public string Address2 { get; set; }
-        public string Address3 { get; set; }
-        public string Address4 { get; set; }
-        public string Town { get; set; }
-        public string Postcode { get; set; }
+        
         public double? Distance { get; set; }
     }
 }
