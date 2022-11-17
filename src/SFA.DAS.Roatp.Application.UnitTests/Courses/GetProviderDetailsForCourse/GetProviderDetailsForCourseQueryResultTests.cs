fa24bf60e6511d6e9b32c6de54c934d2d9bb4961
MarkFCain <mark.cain@digital.education.gov.uk>
2022-11-17 09:02:34 +0000

    CSP-267 final finessing before TR

--- src/SFA.DAS.Roatp.Application.UnitTests/Courses/GetProviderDetailsForCourse/GetProviderDetailsForCourseQueryResultTests.cs
+++ src/SFA.DAS.Roatp.Application.UnitTests/Courses/GetProviderDetailsForCourse/GetProviderDetailsForCourseQueryResultTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using FluentAssertions;
+﻿using FluentAssertions;
 using NUnit.Framework;
 using SFA.DAS.Roatp.Application.Courses.Queries.GetProviderDetailsForCourse;
 using SFA.DAS.Roatp.Domain.Models;
