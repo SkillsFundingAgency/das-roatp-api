e3c995a1adfe1c7d38f01f0e62946f174f4918fd
MarkFCain <mark.cain@digital.education.gov.uk>
2022-11-18 11:45:39 +0000

    CSP-358 EF changes to filter count on active providers only

--- src/SFA.DAS.Roatp.Data/Repositories/ProviderCoursesReadRepository.cs
+++ src/SFA.DAS.Roatp.Data/Repositories/ProviderCoursesReadRepository.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SFA.DAS.Roatp.Data.Constants;
 using SFA.DAS.Roatp.Domain.Entities;
 using SFA.DAS.Roatp.Domain.Interfaces;
 
@@ -47,10 +49,23 @@ namespace SFA.DAS.Roatp.Data.Repositories
 
         public async Task<int> GetProvidersCount(int larscode)
         {
+            var activeProviderRegistrationDetails = await _roatpDataContext
+                .ProviderRegistrationDetails
+                .AsNoTracking()
+                .Where(p=>
+                            p.StatusId == OrganisationStatus.Active 
+                            || p.StatusId==OrganisationStatus.ActiveNotTakingOnApprentices)
+                .ToListAsync();
+
+            var activeProviders = await _roatpDataContext
+                .Providers.Where(p=> activeProviderRegistrationDetails
+                    .Select(prd => prd.Ukprn).Contains(p.Ukprn))
+                    .ToListAsync();
+
             return await _roatpDataContext
                 .ProviderCourses
                 .AsNoTracking()
-                .Where(c => c.LarsCode == larscode)
+                .Where(c => c.LarsCode == larscode && activeProviders.Select(x=>x.Id).Contains(c.ProviderId))
                 .CountAsync();
         }
     }
