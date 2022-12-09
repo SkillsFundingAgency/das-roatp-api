9e4ad972f902682c2c5fb5c141fbe365a4368e93
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2022-12-09 13:56:48 +0000

    Csp 358 filter out onboarding providers from endpoints (#109)
    * CSP-358 EF changes to filter count on active providers only
    
    * CSP-358 count updated to use sql
    
    * CSP-358 clearing up code smells around Task
    
    * CSP-358 fixed issues raised at TR
    
    * CSP-358 added check around tryParse to remove code smell
    
    * CSP-358 removed code smell around extra semicolon

--- src/SFA.DAS.Roatp.Data/Repositories/ProviderCoursesReadRepository.cs
+++ src/SFA.DAS.Roatp.Data/Repositories/ProviderCoursesReadRepository.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SFA.DAS.Roatp.Data.Constants;
 using SFA.DAS.Roatp.Domain.Entities;
 using SFA.DAS.Roatp.Domain.Interfaces;
+using static System.Int32;
 
 namespace SFA.DAS.Roatp.Data.Repositories
 {
@@ -45,13 +48,24 @@ namespace SFA.DAS.Roatp.Data.Repositories
                 .ToListAsync();
         }
 
-        public async Task<int> GetProvidersCount(int larscode)
+        public async Task<int> GetProvidersCount(int larsCode)
         {
-            return await _roatpDataContext
-                .ProviderCourses
-                .AsNoTracking()
-                .Where(c => c.LarsCode == larscode)
-                .CountAsync();
+            await using var connection = _roatpDataContext.Database.GetDbConnection();
+            await connection.OpenAsync();
+
+            await using var command = connection.CreateCommand();
+            command.CommandText = $@"SELECT count(0)
+                    FROM providerCourse PC
+                    INNER JOIN Provider P on P.ID = PC.ProviderID
+                    LEFT OUTER JOIN ProviderRegistrationDetail PRD on P.ukprn = PRD.ukprn
+                    where PC.larsCode ={larsCode}
+                    AND PRD.StatusId IN ({OrganisationStatus.Active},{OrganisationStatus.ActiveNotTakingOnApprentices})"; 
+            var result = await command.ExecuteScalarAsync();
+            var success = TryParse(result?.ToString(), out var count);
+            if (success)
+                return count;
+
+            return 0;
         }
     }
 }
