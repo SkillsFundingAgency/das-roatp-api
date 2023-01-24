9872b74dd16e0a670fb7809a82e9784b74a5c742
Ravi Udari <ravi.udari@digital.education.gov.uk>
2023-01-24 11:54:05 +0000

    CSP-5 refactored healthcheck

---
+++ src/SFA.DAS.Roatp.Api/AppStart/HealthCheckStartup.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Http;
+using SFA.DAS.Roatp.Api.AppStart;
+using SFA.DAS.Roatp.Api.Infrastructure;
+
+namespace SFA.DAS.Roatp.Api.AppStart
+{
+    [ExcludeFromCodeCoverage]
+    public static class HealthCheckStartup
+    {
+        public static IApplicationBuilder UseHealthChecks(this IApplicationBuilder app)
+        {
+            app.UseHealthChecks("/health", new HealthCheckOptions
+            {
+                ResponseWriter = HealthCheckResponseWriter.WriteJsonResponse
+            });
+            app.UseHealthChecks("/ping", new HealthCheckOptions
+            {
+                Predicate = (_) => false,
+                ResponseWriter = (context, report) =>
+                {
+                    context.Response.ContentType = "application/json";
+                    return context.Response.WriteAsync("");
+                }
+            });
+            return app;
+        }
+    }
+}
