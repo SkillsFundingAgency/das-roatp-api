dae9d523882d9e2bf3627a7716954307d9410e28
MarkFCain <mark.cain@digital.education.gov.uk>
2023-02-03 10:10:12 +0000

    CSP-243 removing commented out code

--- src/SFA.DAS.Roatp.Application/Mediatr/Behaviors/ValidationBehavior.cs
+++ src/SFA.DAS.Roatp.Application/Mediatr/Behaviors/ValidationBehavior.cs
@@ -63,35 +63,4 @@ namespace SFA.DAS.Roatp.Application.Mediatr.Behaviors
             return response;
         }
     }
-
-    //
-    // [ExcludeFromCodeCoverage]
-    // public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
-    //     where TRequest : IRequest<TResponse>
-    // {
-    //     private readonly IEnumerable<IValidator<TRequest>> _validators;
-    //
-    //     public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
-    //     {
-    //         _validators = validators;
-    //     }
-    //
-    //     public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
-    //     {
-    //         var context = new ValidationContext<TRequest>(request);
-    //
-    //         var failures = _validators
-    //             .Select(x => x.ValidateAsync(context).GetAwaiter().GetResult())
-    //             .SelectMany(x => x.Errors)
-    //             .Where(x => x != null)
-    //             .ToList();
-    //
-    //         if (failures.Any())
-    //         {
-    //             throw new ValidationException(failures);
-    //         }
-    //
-    //         return next();
-    //     }
-    // }
 }
