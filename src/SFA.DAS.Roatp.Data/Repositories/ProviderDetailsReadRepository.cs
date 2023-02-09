29910909c271e763b9295e35f65a3e824fa5bada
Ravi Udari <ravi.udari@digital.education.gov.uk>
2023-02-09 14:26:28 +0000

    Merge conflict resolved

--- src/SFA.DAS.Roatp.Data/Repositories/ProviderDetailsReadRepository.cs
+++ src/SFA.DAS.Roatp.Data/Repositories/ProviderDetailsReadRepository.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Logging;
 using SFA.DAS.Roatp.Data.Constants;
 using SFA.DAS.Roatp.Domain.Interfaces;
 using SFA.DAS.Roatp.Domain.Models;
-using ProviderType = SFA.DAS.Roatp.Domain.Models.ProviderType;
+using ProviderType = SFA.DAS.Roatp.Data.Constants.ProviderType;
 
 namespace SFA.DAS.Roatp.Data.Repositories
 {
