c66f1235ad9c541434433542e4e46e4c11b7dfc6
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2023-05-22 13:36:49 +0100

    CSP-778 fixes put in to stop null lat long or latitude of 0 being use… (#145)
    * CSP-778 fixes put in to stop null lat long or latitude of 0 being used in calculations
    
    * CSP-778 added protective check for latitude=0 and made indentation uniform
    
    * CSP-778 updated distance endpoint to return -1 when no lat/long available

--- src/SFA.DAS.Roatp.Data/Repositories/ProviderDetailsReadRepository.cs
+++ src/SFA.DAS.Roatp.Data/Repositories/ProviderDetailsReadRepository.cs
@@ -1,70 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using SFA.DAS.Roatp.Domain.Constants;
 using SFA.DAS.Roatp.Domain.Interfaces;
 using SFA.DAS.Roatp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
 using ProviderType = SFA.DAS.Roatp.Domain.Constants.ProviderType;
 
-namespace SFA.DAS.Roatp.Data.Repositories
+namespace SFA.DAS.Roatp.Data.Repositories;
+
+[ExcludeFromCodeCoverage]
+public class ProviderDetailsReadRepository : IProviderDetailsReadRepository
 {
-    [ExcludeFromCodeCoverage]
-    public class ProviderDetailsReadRepository : IProviderDetailsReadRepository
+    private readonly RoatpDataContext _roatpDataContext;
+    private readonly ILogger<ProviderDetailsReadRepository> _logger;
+
+    public ProviderDetailsReadRepository(ILogger<ProviderDetailsReadRepository> logger, RoatpDataContext roatpDataContext)
     {
-        private readonly RoatpDataContext _roatpDataContext;
-        private readonly ILogger<ProviderDetailsReadRepository> _logger;
+        _logger = logger;
+        _roatpDataContext = roatpDataContext;
+    }
 
-        public ProviderDetailsReadRepository(ILogger<ProviderDetailsReadRepository> logger, RoatpDataContext roatpDataContext)
-        {
-            _logger = logger;
-            _roatpDataContext = roatpDataContext;
-        }
+    public async Task<ProviderCourseDetailsModel> GetProviderForUkprnAndLarsCodeWithDistance(int ukprn, int larsCode, decimal? latitude, decimal? longitude)
+    {
+        _logger.LogInformation("Gathering ProviderDetails with distance for ukprn {ukprn}, larscode {larscode}", ukprn, larsCode);
+        var provider = await _roatpDataContext.ProviderDetailsWithDistance
+            .FromSqlInterpolated(GetProviderForUkprnAndLarsCodeWithDistanceSql(ukprn, larsCode, latitude, longitude)).FirstOrDefaultAsync();
+        return provider;
+    }
 
-        public async Task<ProviderCourseDetailsModel> GetProviderForUkprnAndLarsCodeWithDistance(int ukprn, int larsCode, decimal? latitude, decimal? longitude)
-        {
-            _logger.LogInformation("Gathering ProviderDetails with distance for ukprn {ukprn}, larscode {larscode}", ukprn,larsCode); 
-            var provider = await _roatpDataContext.ProviderDetailsWithDistance
-                .FromSqlInterpolated(GetProviderForUkprnAndLarsCodeWithDistanceSql(ukprn, larsCode, latitude, longitude)).FirstOrDefaultAsync();
-            return provider;
-        }
+    public async Task<List<ProviderCourseLocationDetailsModel>> GetProviderLocationDetailsWithDistance(int ukprn, int larsCode, decimal? latitude, decimal? longitude)
+    {
+        _logger.LogInformation("Gathering ProviderLocationDetails with distance for ukprn {ukprn}, larscode {larscode}", ukprn, larsCode);
 
-        public async Task<List<ProviderCourseLocationDetailsModel>> GetProviderLocationDetailsWithDistance(int ukprn, int larsCode, decimal? latitude, decimal? longitude)
-        {
-            _logger.LogInformation("Gathering ProviderLocationDetails with distance for ukprn {ukprn}, larscode {larscode}", ukprn, larsCode);
+        var providerLocations = await _roatpDataContext.ProviderLocationDetailsWithDistance
+            .FromSqlInterpolated(GetProviderLocationDetailsWithDistanceSql(ukprn, larsCode, latitude, longitude)).ToListAsync();
+        return providerLocations;
+    }
 
-            var providerLocations = await _roatpDataContext.ProviderLocationDetailsWithDistance
-                .FromSqlInterpolated(GetProviderLocationDetailsWithDistanceSql(ukprn, larsCode, latitude, longitude)).ToListAsync();
-            return providerLocations;
-        }
+    public async Task<List<ProviderCourseSummaryModel>> GetProvidersForLarsCodeWithDistance(int larsCode, decimal? latitude, decimal? longitude)
+    {
+        _logger.LogInformation("Gathering all ProviderDetails with distance for larscode {larscode}", larsCode);
+        var providers = await _roatpDataContext.ProviderSummaryDetailsWithDistance
+            .FromSqlInterpolated(GetProvidersForLarsCodeWithDistanceSql(larsCode, latitude, longitude)).ToListAsync();
+        return providers;
+    }
 
-        public async Task<List<ProviderCourseSummaryModel>> GetProvidersForLarsCodeWithDistance(int larsCode, decimal? latitude, decimal? longitude)
-        {
-            _logger.LogInformation("Gathering all ProviderDetails with distance for larscode {larscode}",  larsCode);
-            var providers = await _roatpDataContext.ProviderSummaryDetailsWithDistance
-                .FromSqlInterpolated(GetProvidersForLarsCodeWithDistanceSql( larsCode, latitude, longitude)).ToListAsync();
-            return providers;
-        }
+    public async Task<List<ProviderCourseLocationDetailsModel>> GetAllProviderlocationDetailsWithDistance(int larsCode,
+        decimal? latitude, decimal? longitude)
+    {
+        _logger.LogInformation(
+            "Gathering all ProviderLocationDetails with distance for larscode {larscode}", larsCode);
 
-        public async Task<List<ProviderCourseLocationDetailsModel>> GetAllProviderlocationDetailsWithDistance(int larsCode,
-            decimal? latitude, decimal? longitude)
-        {
-            _logger.LogInformation(
-                "Gathering all ProviderLocationDetails with distance for larscode {larscode}", larsCode);
+        var providerLocations = await _roatpDataContext.ProviderLocationDetailsWithDistance
+            .FromSqlInterpolated(GetAllProviderLocationDetailsWithDistanceSql(larsCode, latitude, longitude))
+            .ToListAsync();
+        return providerLocations;
 
-            var providerLocations = await _roatpDataContext.ProviderLocationDetailsWithDistance
-                .FromSqlInterpolated(GetAllProviderLocationDetailsWithDistanceSql( larsCode, latitude, longitude))
-                .ToListAsync();
-            return providerLocations;
+    }
 
-        }
-
-        private static FormattableString GetProviderForUkprnAndLarsCodeWithDistanceSql(int ukprn, int larsCode, decimal? lat, decimal? lon )
-        {
-            return $@"
-                   select   p.ukprn,
+    private static FormattableString GetProviderForUkprnAndLarsCodeWithDistanceSql(int ukprn, int larsCode, decimal? lat, decimal? lon)
+    {
+        return $@"
+                    SELECT  p.ukprn,
                             pc.LarsCode,
                             p.LegalName,
                             p.TradingName,
@@ -81,12 +81,13 @@ namespace SFA.DAS.Roatp.Data.Repositories
                             pa.Town as Town,
                             PA.Postcode as Postcode,
                             p.Id as ProviderId,
-                            CASE  WHEN ({lat} is null) THEN null
-                                WHEN ({lon} is null) THEN null
-                                ELSE
-                                    geography::Point(isnull(pa.Latitude,0), isnull(pa.Longitude,0), 4326)
-                                            .STDistance(geography::Point({lat}, {lon}, 4326)) * 0.0006213712 END
-			                                as Distance
+                            CASE  WHEN ({lat} is null) THEN -1
+                                WHEN ({lon} is null) THEN -1
+                                WHEN (isnull(pa.Latitude,0) = 0) THEN -1
+                            ELSE
+                                geography::Point(pa.Latitude, pa.Longitude, 4326)
+                                        .STDistance(geography::Point({lat}, {lon}, 4326)) * 0.0006213712 END
+			                            as Distance
                             FROM provider P
 		                    INNER JOIN ProviderCourse pc on p.Id = pc.ProviderId
 							LEFT OUTER JOIN [ProviderAddress] PA on P.Id = PA.ProviderId
@@ -94,12 +95,12 @@ namespace SFA.DAS.Roatp.Data.Repositories
 		                    Where P.ukprn={ukprn}
 		                    AND pc.LarsCode={larsCode}
                             AND PRD.StatusId in ({OrganisationStatus.Active}, {OrganisationStatus.ActiveNotTakingOnApprentices})";
-        }
+    }
 
-        private static FormattableString GetProvidersForLarsCodeWithDistanceSql(int larsCode, decimal? lat, decimal? lon)
-        {
-            return $@"
-                   select p.Id as ProviderId,
+    private static FormattableString GetProvidersForLarsCodeWithDistanceSql(int larsCode, decimal? lat, decimal? lon)
+    {
+        return $@"
+                    SELECT  p.Id as ProviderId,
                             p.ukprn,
                             pc.LarsCode,
                             p.LegalName,
@@ -108,80 +109,82 @@ namespace SFA.DAS.Roatp.Data.Repositories
                                 ELSE pc.IsApprovedByRegulator End IsApprovedByRegulator,
                             CASE  WHEN ({lat} is null) THEN null
                                 WHEN ({lon} is null) THEN null
+                                WHEN (isnull(pa.Latitude,0) = 0) THEN null
                                 ELSE
-                                    geography::Point(isnull(pa.Latitude,0), isnull(pa.Longitude,0), 4326)
+                                    geography::Point(pa.Latitude, pa.Longitude, 4326)
                                             .STDistance(geography::Point({lat}, {lon}, 4326)) * 0.0006213712 END
 			                                as Distance
                             FROM provider P
 		                    INNER JOIN ProviderCourse pc on p.Id = pc.ProviderId
-							LEFT OUTER JOIN [ProviderAddress] PA on P.Id = PA.ProviderId
+				            LEFT OUTER JOIN [ProviderAddress] PA on P.Id = PA.ProviderId
                             LEFT OUTER JOIN ProviderRegistrationDetail PRD on P.Ukprn = PRD.Ukprn 
                             LEFT OUTER JOIN Standard S on PC.LarsCode = S.LarsCode
 		                    WHERE pc.LarsCode={larsCode}
                             AND PRD.StatusId in ({OrganisationStatus.Active}, {OrganisationStatus.ActiveNotTakingOnApprentices})
                             AND PRD.ProviderTypeId={ProviderType.Main}";
-        }
+    }
 
-        private static FormattableString GetProviderLocationDetailsWithDistanceSql(int ukprn, int larsCode, decimal? lat, decimal? lon)
-        {
-            return $@"
+    private static FormattableString GetProviderLocationDetailsWithDistanceSql(int ukprn, int larsCode, decimal? lat, decimal? lon)
+    {
+        return $@"
                     SELECT  P.Id as providerId,
-                    LocationType,
-                    PL.AddressLine1,
-                    PL.AddressLine2,
-                    PL.Town,
-                    PL.County,
-                    PL.Postcode,
-	                PCL.HasDayReleaseDeliveryOption,
-	                PCL.HasBlockReleaseDeliveryOption,
-	                CASE	WHEN ({lat} is null) THEN null
-			                WHEN ({lon} is null) THEN null
-	                ELSE
-	                  geography::Point(isnull(PL.Latitude,0), isnull(PL.Longitude,0), 4326)
-				                .STDistance(geography::Point({lat}, {lon}, 4326)) * 0.0006213712 END
-				                as Distance
-                      FROM Provider P
-                      INNER JOIN ProviderCourse PC
-                      ON p.Id = PC.ProviderID
-                      INNER JOIN ProviderCourseLocation PCL on PC.Id = PCL.ProviderCourseId
-                      INNER JOIN ProviderLocation PL On PCL.ProviderLocationId = PL.Id
-                      LEFT OUTER JOIN ProviderRegistrationDetail PRD on P.Ukprn = PRD.Ukprn 
-                      WHERE P.Ukprn={ukprn}
-                      AND LarsCode={larsCode}
-                      AND PRD.StatusId in ({OrganisationStatus.Active}, {OrganisationStatus.ActiveNotTakingOnApprentices})";
+                            LocationType,
+                            PL.AddressLine1,
+                            PL.AddressLine2,
+                            PL.Town,
+                            PL.County,
+                            PL.Postcode,
+	                        PCL.HasDayReleaseDeliveryOption,
+	                        PCL.HasBlockReleaseDeliveryOption,
+	                        CASE	WHEN ({lat} is null) THEN null
+			                        WHEN ({lon} is null) THEN null
+			                        WHEN (PL.Latitude = 0) THEN null
+	                        ELSE
+                                geography::Point(isnull(PL.Latitude,0), isnull(PL.Longitude,0), 4326)
+		                                .STDistance(geography::Point({lat}, {lon}, 4326)) * 0.0006213712 END
+		                                as Distance
+                            FROM Provider P
+                            INNER JOIN ProviderCourse PC
+                            ON p.Id = PC.ProviderID
+                            INNER JOIN ProviderCourseLocation PCL on PC.Id = PCL.ProviderCourseId
+                            INNER JOIN ProviderLocation PL On PCL.ProviderLocationId = PL.Id
+                            LEFT OUTER JOIN ProviderRegistrationDetail PRD on P.Ukprn = PRD.Ukprn 
+                            WHERE P.Ukprn={ukprn}
+                            AND LarsCode={larsCode}
+                            AND PRD.StatusId in ({OrganisationStatus.Active}, {OrganisationStatus.ActiveNotTakingOnApprentices})";
 
-        }
+    }
 
-        private static FormattableString GetAllProviderLocationDetailsWithDistanceSql(int larsCode, decimal? lat, decimal? lon)
-        {
-            return $@"
-                    SELECT P.Ukprn,
-                    P.Id as providerId,
-                    PC.LarsCode,
-                    LocationType,
-                    PL.AddressLine1,
-                    PL.AddressLine2,
-                    PL.Town,
-                    PL.County,
-                    PL.Postcode,
-	                PCL.HasDayReleaseDeliveryOption,
-	                PCL.HasBlockReleaseDeliveryOption,
-	                CASE	WHEN ({lat} is null) THEN null
-			                WHEN ({lon} is null) THEN null
-	                ELSE
-	                  geography::Point(isnull(PL.Latitude,0), isnull(PL.Longitude,0), 4326)
-				                .STDistance(geography::Point({lat}, {lon}, 4326)) * 0.0006213712 END
-				                as Distance
-                FROM Provider P
-                INNER JOIN ProviderCourse PC
-                ON p.Id = PC.ProviderID
-                INNER JOIN ProviderCourseLocation PCL on PC.Id = PCL.ProviderCourseId
-                INNER JOIN ProviderLocation PL On PCL.ProviderLocationId = PL.Id
-                LEFT OUTER JOIN Region R on R.Id =PL.RegionId
-                LEFT OUTER JOIN ProviderRegistrationDetail PRD on P.Ukprn = PRD.Ukprn 
-                WHERE  LarsCode={larsCode}
-                AND PRD.StatusId in ({OrganisationStatus.Active}, {OrganisationStatus.ActiveNotTakingOnApprentices})
-                AND PRD.ProviderTypeId={ProviderType.Main}";
-        }
+    private static FormattableString GetAllProviderLocationDetailsWithDistanceSql(int larsCode, decimal? lat, decimal? lon)
+    {
+        return $@"
+                    SELECT  P.Ukprn,
+                            P.Id as providerId,
+                            PC.LarsCode,
+                            LocationType,
+                            PL.AddressLine1,
+                            PL.AddressLine2,
+                            PL.Town,
+                            PL.County,
+                            PL.Postcode,
+	                        PCL.HasDayReleaseDeliveryOption,
+	                        PCL.HasBlockReleaseDeliveryOption,
+	                        CASE	WHEN ({lat} is null) THEN null
+			                        WHEN ({lon} is null) THEN null
+			                        WHEN (PL.Latitude = 0) THEN null
+	                        ELSE
+	                          geography::Point(isnull(PL.Latitude,0), isnull(PL.Longitude,0), 4326)
+				                        .STDistance(geography::Point({lat}, {lon}, 4326)) * 0.0006213712 END
+				                        as Distance
+                            FROM Provider P
+                            INNER JOIN ProviderCourse PC
+                            ON p.Id = PC.ProviderID
+                            INNER JOIN ProviderCourseLocation PCL on PC.Id = PCL.ProviderCourseId
+                            INNER JOIN ProviderLocation PL On PCL.ProviderLocationId = PL.Id
+                            LEFT OUTER JOIN Region R on R.Id =PL.RegionId
+                            LEFT OUTER JOIN ProviderRegistrationDetail PRD on P.Ukprn = PRD.Ukprn 
+                            WHERE  LarsCode={larsCode}
+                            AND PRD.StatusId in ({OrganisationStatus.Active}, {OrganisationStatus.ActiveNotTakingOnApprentices})
+                            AND PRD.ProviderTypeId={ProviderType.Main}";
     }
 }
