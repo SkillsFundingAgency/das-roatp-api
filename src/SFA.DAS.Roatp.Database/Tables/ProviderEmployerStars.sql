2c5ce4eb00e179973b978bfa7efecd9087ca6c48
Alan Burns <adburns57@gmail.com>
2024-12-16 10:39:47 +0000

    .. add missing table definition

--- src/SFA.DAS.Roatp.Database/Tables/ProviderEmployerStars.sql
+++ src/SFA.DAS.Roatp.Database/Tables/ProviderEmployerStars.sql
@@ -1,4 +1,11 @@
-﻿CREATE TABLE [dbo].[ProviderEmployerStars]
-(
-	[Id] INT NOT NULL PRIMARY KEY
-)
+﻿CREATE TABLE [dbo].[ProviderEmployerStars] (
+    [Ukprn] [bigint] NOT NULL,
+    [ReviewCount] [int] NOT NULL,
+    [Stars] [int] NOT NULL,
+    [TimePeriod] NVARCHAR(50) NOT NULL
+);
+GO
+
+CREATE INDEX IXU_ProviderEmployerStars ON [dbo].[ProviderEmployerStars] ([Ukprn],[TimePeriod]) 
+INCLUDE ([ReviewCount],[Stars]);
+GO
