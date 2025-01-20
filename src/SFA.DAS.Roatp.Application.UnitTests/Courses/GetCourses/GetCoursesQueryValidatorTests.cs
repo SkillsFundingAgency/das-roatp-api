def153c2744aa1336eeafff44b0b7d13c66dce09
shaun-downey-education <shaun.downey@education.gov.uk>
2025-01-20 14:09:52 +0000

    CSP-1758: Sonarcloud code suggestions and additional validator code coverage.

--- src/SFA.DAS.Roatp.Application.UnitTests/Courses/GetCourses/GetCoursesQueryValidatorTests.cs
+++ src/SFA.DAS.Roatp.Application.UnitTests/Courses/GetCourses/GetCoursesQueryValidatorTests.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation.TestHelper;
 using NUnit.Framework;
 using SFA.DAS.Roatp.Application.Courses.Queries.GetCourses;
+using System;
 using System.Threading.Tasks;
 
 namespace SFA.DAS.Roatp.Application.UnitTests.Courses.GetCourses;
@@ -26,13 +27,31 @@ public sealed class GetCoursesQueryValidatorTests
     }
 
     [Test]
+    public async Task Validate_WhenLongitudeAndLatitudeDoNotHaveValues_DistanceCanBeNull()
+    {
+        var query = new GetCoursesQuery()
+        {
+            Longitude = null,
+            Latitude = null,
+            LarsCodes = new int[] { 1 },
+            Distance = null
+        };
+
+        var sut = new GetCoursesQueryValidator();
+
+        var result = await sut.TestValidateAsync(query);
+
+        result.ShouldNotHaveValidationErrorFor(c => c.Distance);
+    }
+
+    [Test]
     public async Task Validate_WhenLarsCodesIsEmpty_QueryIsInvalid()
     {
         var query = new GetCoursesQuery()
         {
             Longitude = 90,
             Latitude = -90,
-            LarsCodes = new int[] {  },
+            LarsCodes = Array.Empty<int>(),
             Distance = null
         };
 
