719f8d79788258b55a7d0c1d7507af1e6d61b833
shaun-downey-education <shaun.downey@education.gov.uk>
2025-01-29 15:57:17 +0000

    CSP-1758: Update store procedure with additional constraints.

--- src/SFA.DAS.Roatp.Database/StoredProcedures/GetTrainingProvidersCount.sql
+++ src/SFA.DAS.Roatp.Database/StoredProcedures/GetTrainingProvidersCount.sql
@@ -1,4 +1,6 @@
-CREATE PROCEDURE [dbo].[GetCoursesByDistance]
+-- this calculates the distance from Training Provider training locations / regions
+
+CREATE PROCEDURE dbo.GetTrainingProvidersCount
     @Latitude FLOAT NULL,
     @Longitude FLOAT NULL,
     @Distance INT NULL,
@@ -10,23 +12,24 @@ BEGIN
     DECLARE @NearestRegionId INT, 
             @AlternativeRegionId INT,
             @NearestRegion VARCHAR(200),
-			@Provider INT = 0,
 			@National INT = 1,
+			@Provider INT = 0,
 			@Regional INT = 2,
 			@EPSG_COORDINATE_SYSTEM_ID INT = 4326,
 			@MetersToMilesMetric FLOAT = 0.0006213712;
 
-    IF @Latitude IS NOT NULL 
-		-- Match to nearest region (which may have an alternative with same co-ordinates)
+	IF @Latitude IS NOT NULL 
+		-- match to nearest region (which may have an alternative with same co-ordinates)
 		SELECT TOP 1 @NearestRegionId = reg1.[Id] , @NearestRegion = reg1.SubregionName + ' (' + reg1.RegionName + ')' , @AlternativeRegionid = reg2.[id]
 		FROM [dbo].[Region] reg1
 		LEFT JOIN [dbo].[Region] reg2 ON reg1.[Latitude] = reg2.[Latitude] AND reg1.[Longitude]= reg2.[Longitude] AND reg1.[Id] != reg2.[id]
-			ORDER BY geography::Point(reg1.Latitude, reg1.Longitude, @EPSG_COORDINATE_SYSTEM_ID).STDistance(geography::Point(@Latitude, @Longitude, @EPSG_COORDINATE_SYSTEM_ID)), reg1.[id];
+			ORDER BY geography::Point(reg1.Latitude, reg1.Longitude, @EPSG_COORDINATE_SYSTEM_ID)
+					.STDistance(geography::Point(@Latitude, @Longitude, @EPSG_COORDINATE_SYSTEM_ID)), reg1.[id];
 	ELSE
 		SET @Distance = NULL;
 
-	-- Get the Standards and national QAR by Standard
-	WITH LarsCodes AS (SELECT CONVERT(int,[key])+1 Ordering, [value] LarsCode FROM OPENJSON('[' + @larscodes + ']','$')),
+	-- get the Standards and national QAR by Standard
+	WITH LarsCodes AS (SELECT CONVERT(int,[key]) +1 Ordering, [value] LarsCode FROM OPENJSON('[' + @larscodes + ']','$')),
 
 	StandardsList
 	AS
@@ -43,14 +46,17 @@ BEGIN
 		FROM [dbo].[ProviderCourse] pc1 
 		JOIN [dbo].[Provider] pr1 on pr1.Id = pc1.ProviderId
 		JOIN [dbo].[ProviderRegistrationDetail] tp on tp.[Ukprn] = pr1.[Ukprn] AND tp.[Statusid] = 1 AND tp.[ProviderTypeId] = 1 -- Active, Main only
+		JOIN [dbo].[ProviderCourseLocation] pcl1 on pcl1.ProviderCourseId = pc1.[Id]
+		JOIN [dbo].[ProviderLocation] pl1 on pl1.Id = pcl1.ProviderLocationId
 		GROUP BY Larscode
 	)
 
 	-- Main Query
-	SELECT
-		 st1.[LarsCode],
-		 COUNT(DISTINCT UKPRN) AS 'ProvidersCount',
-		 ISNULL(AllProviders,0) AS 'AllProvidersCount'
+	SELECT 
+		-- order by given order
+		 COUNT(DISTINCT UKPRN) AS 'ProvidersCount'
+		,ISNULL(AllProviders,0) AS 'AllProvidersCount'
+		,st1.[LarsCode]
 	FROM StandardsList st1
 	LEFT JOIN ActiveProviders act on act.[LarsCode] = st1.[LarsCode]
 	LEFT JOIN (
@@ -64,8 +70,8 @@ BEGIN
 			-- LocationType: Provider = 0, National = 1, Regional = 2
 			,CASE WHEN LocationType = 0 THEN Distance
 				  WHEN LocationOrdering = 3 THEN 99999
-				  ELSE 0 END AS 'Course_Distance' -- Not used?
-			-- Nearest distance for any provider
+				  ELSE 0 END Course_Distance
+			-- nearest distance for any provider
 			,ROW_NUMBER() OVER (PARTITION BY [Ukprn], [LarsCode] ORDER BY Distance) TP_Std_Dist_Seq
 		FROM
 			(
@@ -74,27 +80,28 @@ BEGIN
 					,pc1.[LarsCode]
 					,[LocationType]
 					,CASE
-						WHEN [LocationType] = 0 THEN @Provider
-						WHEN [LocationType] = 1 THEN @National
-						WHEN pl1.[RegionId] IS NOT NULL THEN 
-							(CASE WHEN pl1.[RegionId] = @NearestRegionId THEN 0 -- Same Region
-								  WHEN @AlternativeRegionId IS NOT NULL AND pl1.[RegionId] = @AlternativeRegionId THEN 0 -- alternative Region
-								  ELSE 3 -- Other Regions
-								  END)
-						ELSE 3 -- other
-						END AS 'LocationOrdering',
-
+					 WHEN @Latitude  IS NULL THEN 0  -- No Location check
+					 WHEN [LocationType] = 0 THEN 2  -- Provider
+					 WHEN [LocationType] = 1 THEN 1  -- National
+					 WHEN pl1.[RegionId] IS NOT NULL THEN 
+						(CASE WHEN pl1.[RegionId] = @NearestRegionId THEN 0 -- same Region
+							  WHEN @AlternativeRegionid IS NOT NULL AND pl1.[RegionId] = @AlternativeRegionid THEN 0 -- alternative Region
+							  ELSE 3 -- other Regions
+							  END)
+					 ELSE 3 -- other
+					 END LocationOrdering
 					-- calculate distance 
-					CASE 
-						WHEN @Latitude IS NOT NULL AND [LocationType] = @Provider
-							THEN ROUND(geography::Point(pl1.Latitude, pl1.Longitude, @EPSG_COORDINATE_SYSTEM_ID).STDistance(geography::Point(convert(float,@Latitude), convert(float,@Longitude), @EPSG_COORDINATE_SYSTEM_ID)) * @MetersToMilesMetric, 1) 
-						WHEN [LocationType] = @National
-							THEN 0
-						WHEN @Latitude IS NOT NULL AND [LocationType] = @Regional
-							THEN ROUND(geography::Point(rg1.Latitude, rg1.Longitude, @EPSG_COORDINATE_SYSTEM_ID).STDistance(geography::Point(convert(float,@Latitude), convert(float,@Longitude), @EPSG_COORDINATE_SYSTEM_ID)) * @MetersToMilesMetric, 1) 
-						ELSE 0 
-					END AS 'Distance'
-			FROM [dbo].[ProviderCourse] pc1
+					,CASE 
+					WHEN @Latitude IS NOT NULL AND [LocationType] = @Provider
+						THEN ROUND(geography::Point(pl1.Latitude, pl1.Longitude, @EPSG_COORDINATE_SYSTEM_ID)
+									.STDistance(geography::Point(convert(float,@Latitude), convert(float,@Longitude), @EPSG_COORDINATE_SYSTEM_ID)) * @MetersToMilesMetric,1) 
+					WHEN [LocationType] = 1  -- National
+						THEN 0
+					WHEN @Latitude IS NOT NULL AND [LocationType] = @Regional
+						THEN ROUND(geography::Point(rg1.Latitude, rg1.Longitude, @EPSG_COORDINATE_SYSTEM_ID)
+									.STDistance(geography::Point(convert(float,@Latitude), convert(float,@Longitude), @EPSG_COORDINATE_SYSTEM_ID)) * @MetersToMilesMetric,1) 
+					ELSE 0 END AS Distance
+			FROM [dbo].[ProviderCourse] pc1 
 				JOIN Larscodes lc1 on lc1.LarsCode = pc1.LarsCode
 				JOIN [dbo].[Provider] pr1 on pr1.Id = pc1.ProviderId
 				JOIN [dbo].[ProviderRegistrationDetail] tp on tp.[Ukprn] = pr1.[Ukprn] AND tp.[Statusid] = 1 AND tp.[ProviderTypeId] = 1 -- Active, Main only
@@ -103,8 +110,8 @@ BEGIN
 				LEFT JOIN [dbo].[Region] rg1 on rg1.[Id] = pl1.[RegionId]
 			) ab1 
 			WHERE 
-			LocationOrdering != 3 AND -- Exclude outside Regions
-			(@Distance IS NULL OR Distance <= @Distance)
+				LocationOrdering != 3 AND -- exclude outside Regions
+				(@Distance IS NULL OR Distance <= @Distance)
 		) ab2
 	ON st1.LarsCode = ab2.LarsCode AND TP_Std_Dist_Seq = 1
 	GROUP BY st1.[LarsCode], st1.[Title], st1.[Level], st1.[Ordering], AllProviders
