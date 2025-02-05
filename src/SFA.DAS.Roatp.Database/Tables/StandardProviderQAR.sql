9b6042f43f3b704a29afb09a24e7bb81e5543784
Alan Burns <adburns57@gmail.com>
2025-02-05 16:46:47 +0000

    CSP-1899_compute the qar and review ratings

--- src/SFA.DAS.Roatp.Database/Tables/StandardProviderQAR.sql
+++ src/SFA.DAS.Roatp.Database/Tables/StandardProviderQAR.sql
@@ -6,6 +6,13 @@
   [Leavers] VARCHAR(10), 
   [AchievementRate] VARCHAR(10),
   [CreatedDate] DATETIME2 DEFAULT GETUTCDATE(),
+  [AchievementRank] AS (CONVERT(VARCHAR(20)
+                       ,CASE WHEN ISNULL([AchievementRate],'x') = 'x' 
+                             THEN 'None'
+                             WHEN [AchievementRate] < '50' THEN 'VeryPoor'
+                             WHEN [AchievementRate] < '60' THEN 'Poor'
+                             WHEN [AchievementRate] < '70' THEN 'Good'
+                             ELSE 'Excellent' END)),    
   CONSTRAINT PK_StandardProviderQAR PRIMARY KEY ([TimePeriod], [Ukprn], [IfateReferenceNumber])
 );
 GO
