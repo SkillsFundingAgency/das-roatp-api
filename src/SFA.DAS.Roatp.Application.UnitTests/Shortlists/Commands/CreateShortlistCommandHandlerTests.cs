3fa1d3a17be15b8a32b0f33ca6aaf323b13b0a16
Deven Shah <devenshah1974@gmail.com>
2025-02-18 15:57:01 +0000

    CSP-1881 Implemented Shortlist create command and repository

---
+++ src/SFA.DAS.Roatp.Application.UnitTests/Shortlists/Commands/CreateShortlistCommandHandlerTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Roatp.Application.Shortlists.Commands.CreateShortlist;
+using SFA.DAS.Roatp.Domain.Entities;
+using SFA.DAS.Roatp.Domain.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.Roatp.Application.UnitTests.Shortlists.Commands;
+
+public class CreateShortlistCommandHandlerTests
+{
+    [Test, MoqAutoData]
+    public async Task Handle_ShortlistExists_ReturnsExistingShortlist(
+        CreateShortlistCommand command,
+        [Frozen] Mock<IShortlistWriteRepository> shortlistWriteRepositoryMock,
+        CreateShortlistCommandHandler sut,
+        Shortlist shortlist,
+        CancellationToken cancellationToken)
+    {
+        shortlistWriteRepositoryMock.Setup(s => s.Get(command.UserId, command.Ukprn, command.LarsCode, command.LocationDescription, cancellationToken)).ReturnsAsync(shortlist);
+
+        var actual = await sut.Handle(command, cancellationToken);
+
+        actual.Shortlist.Should().Be(shortlist);
+        shortlistWriteRepositoryMock.Verify(s => s.Create(It.IsAny<Shortlist>(), cancellationToken), Times.Never);
+    }
+
+    [Test, MoqAutoData]
+    public async Task Handle_ShortlistDoesNotExist_CreatesNewShortlist(
+        CreateShortlistCommand command,
+        [Frozen] Mock<IShortlistWriteRepository> shortlistWriteRepositoryMock,
+        CreateShortlistCommandHandler sut,
+        CancellationToken cancellationToken)
+    {
+        shortlistWriteRepositoryMock.Setup(s => s.Get(command.UserId, command.Ukprn, command.LarsCode, command.LocationDescription, cancellationToken)).ReturnsAsync((Shortlist)null);
+
+        var actual = await sut.Handle(command, cancellationToken);
+
+        actual.Shortlist.Should().BeEquivalentTo(command, config => config.ExcludingMissingMembers());
+        shortlistWriteRepositoryMock.Verify(s => s.Create(It.Is<Shortlist>(s => s.Id != Guid.Empty && s.UserId == command.UserId && s.Ukprn == command.Ukprn && s.LarsCode == command.LarsCode && s.LocationDescription == command.LocationDescription), cancellationToken), Times.Once);
+    }
+}
---
+++ src/SFA.DAS.Roatp.Application/Shortlists/Commands/CreateShortlist/CreateShortlistCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.Roatp.Domain.Entities;
+using SFA.DAS.Roatp.Domain.Interfaces;
+
+namespace SFA.DAS.Roatp.Application.Shortlists.Commands.CreateShortlist;
+
+public class CreateShortlistCommandHandler(IShortlistWriteRepository _shortlistWriteRepository) : IRequestHandler<CreateShortlistCommand, CreateShortlistCommandResult>
+{
+    public async Task<CreateShortlistCommandResult> Handle(CreateShortlistCommand request, CancellationToken cancellationToken)
+    {
+        Shortlist shortlist = await _shortlistWriteRepository.Get(request.UserId, request.Ukprn, request.LarsCode, request.LocationDescription, cancellationToken);
+
+        if (shortlist == null)
+        {
+            shortlist = ConvertToShortlist(request);
+            await _shortlistWriteRepository.Create(shortlist, cancellationToken);
+        }
+
+        return new(shortlist);
+    }
+
+    private static Shortlist ConvertToShortlist(CreateShortlistCommand command)
+        => new()
+        {
+            Id = Guid.NewGuid(),
+            UserId = command.UserId,
+            Ukprn = command.Ukprn,
+            LarsCode = command.LarsCode,
+            LocationDescription = command.LocationDescription,
+            Latitude = command.Latitude,
+            Longitude = command.Longitude
+        };
+}
