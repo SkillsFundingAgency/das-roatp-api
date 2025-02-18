cbc3debef8927e5aa0c9100ad14e905e0cf35819
shaun-downey-education <148352239+shaun-downey-education@users.noreply.github.com>
2025-02-18 13:02:36 +0000

    CSP-1758: FAT - 7. Managing Standards Inner API for Training Courses (#168)
    * CSP-1758_Managing_standards_training_courses
    
    * CSP-1758: Tidy up usings.
    
    * CSP-1758: Sonarcloud code suggestions and additional validator code coverage.
    
    * CSP-1758: Reregister training courses repo.
    
    * CSP-1758: Combine get provider count endpoints.
    
    * CSP-1758: Update store procedure with additional constraints.
    
    * CSP-1758: Updated store procedure to include new constraints. Reordered selected properties to match internal roatp api script.
    
    * CSP-1758: Explicity declare both reader and dbcommand types.
    
    * CSP-1758: Added .http file to web test project for GetTrainingProvidesCount endpoint.
    
    * CSP-1758: Correct store procedure property order when selecting results.
    
    * CSP-1758: Fixed store procedure format.

---
+++ src/SFA.DAS.Roatp.Application/Courses/Queries/GetCourseTrainingProvidersCount/CourseTrainingProviderCountModel.cs
@@ -0,0 +1,17 @@
+﻿using SFA.DAS.Roatp.Domain.Entities;
+
+namespace SFA.DAS.Roatp.Application.Courses.Queries.GetCourseTrainingProvidersCount;
+
+public sealed class CourseTrainingProviderCountModel
+{
+    public int LarsCode { get; set; }
+    public int ProvidersCount { get; set; }
+    public int TotalProvidersCount { get; set; }
+
+    public static implicit operator CourseTrainingProviderCountModel(CourseInformation source) => new()
+    {
+        LarsCode = source.LarsCode,
+        ProvidersCount = source.ProvidersCount,
+        TotalProvidersCount = source.TotalProvidersCount
+    };
+}
--- src/SFA.DAS.Roatp.Data/Repositories/ProviderCoursesReadRepository.cs
+++ src/SFA.DAS.Roatp.Data/Repositories/ProviderCoursesReadRepository.cs
@@ -1,13 +1,10 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SFA.DAS.Roatp.Domain.Entities;
+using SFA.DAS.Roatp.Domain.Interfaces;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using SFA.DAS.Roatp.Domain.Constants;
-using SFA.DAS.Roatp.Domain.Entities;
-using SFA.DAS.Roatp.Domain.Interfaces;
-using static System.Int32;
 
 namespace SFA.DAS.Roatp.Data.Repositories
 {
@@ -46,27 +43,6 @@ namespace SFA.DAS.Roatp.Data.Repositories
                 .AsNoTracking()
                 .Where(c => c.Provider.Ukprn == ukprn)
                 .ToListAsync();
-        }
-
-        public async Task<int> GetProvidersCount(int larsCode)
-        {
-            await using var connection = _roatpDataContext.Database.GetDbConnection();
-            await connection.OpenAsync();
-
-            await using var command = connection.CreateCommand();
-            command.CommandText = $@"SELECT count(0)
-                    FROM providerCourse PC
-                    INNER JOIN Provider P on P.ID = PC.ProviderID
-                    LEFT OUTER JOIN ProviderRegistrationDetail PRD on P.ukprn = PRD.ukprn
-                    where PC.larsCode ={larsCode}
-                    AND PRD.StatusId IN ({OrganisationStatus.Active},{OrganisationStatus.ActiveNotTakingOnApprentices}) 
-                    AND PRD.ProviderTypeId={ProviderType.Main}"; 
-            var result = await command.ExecuteScalarAsync();
-            var success = TryParse(result?.ToString(), out var count);
-            if (success)
-                return count;
-
-            return 0;
         }
     }
 }
