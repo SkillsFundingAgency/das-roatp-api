d120b3f284cad8bf6c4c806396fa2c0d0f3a2c77
MarkFCain <mark.cain@digital.education.gov.uk>
2025-02-19 15:03:37 +0000

    CSP-1916 updated for TR comments

--- src/SFA.DAS.Roatp.Application.UnitTests/AcademicYears/Queries/GetLatest/GetAcademicYearsLatestHandlerTests.cs
+++ src/SFA.DAS.Roatp.Application.UnitTests/AcademicYears/Queries/GetLatest/GetAcademicYearsLatestHandlerTests.cs
@@ -3,11 +3,10 @@ using FluentAssertions;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.Roatp.Application.AcademicYears.Queries.GetLatest;
+using SFA.DAS.Roatp.Domain.Entities;
 using SFA.DAS.Roatp.Domain.Interfaces;
 using SFA.DAS.Testing.AutoFixture;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -27,23 +26,42 @@ public class GetAcademicYearsLatestHandlerTests
         response.Should().NotBeNull();
     }
 
-    [Test, RecursiveMoqAutoData]
-    public async Task Handle_ReturnsResult_CheckingQarPeriod(
-        List<Domain.Entities.NationalQar> nationalQars,
+    [Test]
+    [InlineAutoData("2324", "2223", "2122", "2324")]
+    [InlineAutoData("2324", "2122", "2223", "2324")]
+    [InlineAutoData("2223", "2324", "2122", "2324")]
+    [InlineAutoData("2223", "2122", "2324", "2324")]
+    [InlineAutoData("2122", "2223", "2324", "2324")]
+    [InlineAutoData("2122", "2324", "2223", "2324")]
+    [InlineAutoData("0022", "2728", "2223", "2728")]
+    [InlineAutoData("2122", "3031", "2989", "3031")]
+    public async Task Handle_MultipleNationalQars_ReturnsLatestQarPeriod(
+        string timePeriod1, string timePeriod2, string timePeriod3, string expectedTimePeriod,
         [Frozen] Mock<INationalQarReadRepository> nationalQarReadRepositoryMock,
+        [Frozen] Mock<IProviderEmployerStarsReadRepository> providerEmployerStarsReadRepositoryMock,
         GetAcademicYearsLatestQuery query,
-        GetAcademicYearsLatestQueryHandler sut,
         CancellationToken cancellationToken)
     {
+        var nationalQars = new List<NationalQar>
+        {
+            new() { TimePeriod = timePeriod1 },
+            new() { TimePeriod = timePeriod2 },
+            new() { TimePeriod = timePeriod3 }
+        };
+
         nationalQarReadRepositoryMock.Setup(r => r.GetAll()).ReturnsAsync(nationalQars);
+
+        var sut = new GetAcademicYearsLatestQueryHandler(nationalQarReadRepositoryMock.Object, providerEmployerStarsReadRepositoryMock.Object);
+
         var response = await sut.Handle(query, cancellationToken);
 
         response.Should().NotBeNull();
-        response.QarPeriod.Should().Be(nationalQars.Max(x => x.TimePeriod));
+        response.QarPeriod.Should().Be(expectedTimePeriod);
     }
 
+
     [Test, RecursiveMoqAutoData]
-    public async Task Handle_ReturnsResult_CheckingReviewPeriod_NoFeedbackPeriods(
+    public async Task Handle_CheckingReviewPeriodWithNoFeedbackPeriods_ReturnsCalculatedReviewPeriod(
         List<Domain.Entities.NationalQar> nationalQars,
         [Frozen] Mock<IProviderEmployerStarsReadRepository> providerEmployerStarsReadRepository,
         GetAcademicYearsLatestQuery query,
@@ -53,18 +71,17 @@ public class GetAcademicYearsLatestHandlerTests
         var noTimePeriods = new List<string>();
         providerEmployerStarsReadRepository.Setup(r => r.GetTimePeriods()).ReturnsAsync(noTimePeriods);
 
-        string reviewPeriodCalculated = string.Concat(DateTime.UtcNow.AddMonths(-19).Year.ToString().AsSpan(2, 2),
-                                                    DateTime.UtcNow.AddMonths(-7).Year.ToString().AsSpan(2, 2));
-
         var response = await sut.Handle(query, cancellationToken);
 
         response.Should().NotBeNull();
-        response.ReviewPeriod.Should().Be(reviewPeriodCalculated);
+        response.ReviewPeriod.Should().NotBeNull();
+        response.ReviewPeriod.Length.Should().Be(4);
+
     }
 
 
     [Test, RecursiveMoqAutoData]
-    public async Task Handle_ReturnsResult_CheckingReviewPeriod_WithFeedbackPeriods(
+    public async Task Handle_MultipleReviewPeriods_ReturnsMaximumTimePeriod(
         List<Domain.Entities.NationalQar> nationalQars,
         [Frozen] Mock<IProviderEmployerStarsReadRepository> providerEmployerStarsReadRepository,
         GetAcademicYearsLatestQuery query,
