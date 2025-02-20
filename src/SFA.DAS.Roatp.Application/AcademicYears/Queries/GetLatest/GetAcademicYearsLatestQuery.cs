75ed5af493d214fe8410bd2b5bc55701f044ee06
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2025-02-20 10:50:54 +0000

    CSP-1916 Get latest acedemic years (#173)
    * CSP-1916 Get latest acedemic years
    
    * CSP-1916 updated for TR comments

---
+++ src/SFA.DAS.Roatp.Application/AcademicYears/Queries/GetLatest/GetAcademicYearsLatestQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace SFA.DAS.Roatp.Application.AcademicYears.Queries.GetLatest;
+public class GetAcademicYearsLatestQuery : IRequest<GetAcademicYearsLatestQueryResult>
+{
+}
---
+++ src/SFA.DAS.Roatp.Data/Configuration/ProviderEmployerStarsConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SFA.DAS.Roatp.Domain.Entities;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SFA.DAS.Roatp.Data.Configuration;
+
+[ExcludeFromCodeCoverage]
+public class ProviderEmployerStarsConfiguration : IEntityTypeConfiguration<ProviderEmployerStars>
+{
+    public void Configure(EntityTypeBuilder<ProviderEmployerStars> builder)
+    {
+        builder.ToTable(nameof(ProviderEmployerStars));
+        builder.HasKey(table => new
+        {
+            table.TimePeriod,
+            table.Ukprn
+        });
+        builder.Property(p => p.ReviewCount).IsRequired();
+        builder.Property(p => p.Stars).IsRequired();
+        builder.Property(p => p.Rating).IsRequired();
+    }
+}
