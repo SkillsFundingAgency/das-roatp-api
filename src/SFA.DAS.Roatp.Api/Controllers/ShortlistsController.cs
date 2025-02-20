8d875df0123300e4e870acd3b7c7e90f5be1975d
Deven Shah <devenshah1974@gmail.com>
2025-02-20 14:04:41 +0000

    CSP-1889 Implements get shortlist count
    CSP-1889 Implements get shortlist count
    
    CSP-1889 Refactored shortlist repository name
    
    CSP-1889 Fixed minor naming issue

--- src/SFA.DAS.Roatp.Api/Controllers/ShortlistsController.cs
+++ src/SFA.DAS.Roatp.Api/Controllers/ShortlistsController.cs
@@ -1,11 +1,15 @@
-﻿using System.Threading;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
+using FluentValidation.Results;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.Roatp.Api.Infrastructure;
 using SFA.DAS.Roatp.Application.Mediatr.Responses;
 using SFA.DAS.Roatp.Application.Shortlists.Commands.CreateShortlist;
+using SFA.DAS.Roatp.Application.Shortlists.Queries.GetShortlistCountForUser;
 
 namespace SFA.DAS.Roatp.Api.Controllers;
 
@@ -16,7 +20,7 @@ public class ShortlistsController(IMediator _mediator) : ActionResponseControlle
     [HttpPost]
     [ProducesResponseType(typeof(CreateShortlistCommandResult), StatusCodes.Status201Created)]
     [ProducesResponseType(typeof(CreateShortlistCommandResult), StatusCodes.Status204NoContent)]
-    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(IEnumerable<ValidationFailure>), StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> CreateShortlist([FromBody] CreateShortlistCommand command, CancellationToken cancellationToken)
     {
         ValidatedResponse<CreateShortlistCommandResult> validatedResponse = await _mediator.Send(command, cancellationToken);
@@ -31,5 +35,15 @@ public class ShortlistsController(IMediator _mediator) : ActionResponseControlle
     public IActionResult GetShortlist([FromRoute] string id, CancellationToken cancellationToken)
     {
         return Ok("not implemented");
+    }
+
+    [HttpGet]
+    [Route("users/{userId}/count")]
+    [ProducesResponseType(typeof(IEnumerable<ValidationFailure>), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(GetShortlistsCountForUserQueryResult), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetShortlistsCountForUser([FromRoute] Guid userId, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(new GetShortlistsCountForUserQuery(userId), cancellationToken);
+        return GetResponse(result);
     }
 }
