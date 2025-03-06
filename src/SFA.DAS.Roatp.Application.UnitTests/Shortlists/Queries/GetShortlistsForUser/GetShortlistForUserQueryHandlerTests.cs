38eb58bbe3cd88233998f6812e01ff0f2b497a2d
Deven Shah <devenshah1974@gmail.com>
2025-03-06 15:00:10 +0000

    CSP-1891 Implementes get shortlists for user query handler

---
+++ src/SFA.DAS.Roatp.Application.UnitTests/Shortlists/Queries/GetShortlistsForUser/GetShortlistForUserQueryHandlerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Roatp.Application.Mediatr.Responses;
+using SFA.DAS.Roatp.Application.Shortlists.Queries.GetShortlistsForUser;
+using SFA.DAS.Roatp.Domain.Interfaces;
+
+namespace SFA.DAS.Roatp.Application.UnitTests.Shortlists.Queries.GetShortlistsForUser;
+
+public class GetShortlistForUserQueryHandlerTests
+{
+    [Test, AutoData]
+    public async Task Handle_ReturnsResult(GetShortlistsForUserQueryResult expected, Guid userId, CancellationToken cancellationToken)
+    {
+        Mock<IShortlistsRepository> shortlistsRepoMock = new();
+        shortlistsRepoMock.Setup(x => x.GetShortlistsForUser(userId, cancellationToken)).ReturnsAsync(JsonSerializer.Serialize(expected, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase }));
+        GetShortlistForUserQueryHandler sut = new(shortlistsRepoMock.Object);
+
+        ValidatedResponse<GetShortlistsForUserQueryResult> validatedResponse = await sut.Handle(new GetShortlistsForUserQuery(userId), cancellationToken);
+
+        shortlistsRepoMock.Verify(x => x.GetShortlistsForUser(userId, cancellationToken), Times.Once);
+        validatedResponse.Result.Should().BeEquivalentTo(expected);
+    }
+}
--- src/SFA.DAS.Roatp.Application/Shortlists/Queries/GetShortlistsForUser/GetShortlistForUserQueryHandler.cs
+++ src/SFA.DAS.Roatp.Application/Shortlists/Queries/GetShortlistsForUser/GetShortlistForUserQueryHandler.cs
@@ -1,14 +1,20 @@
-﻿using System.Threading;
+﻿using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using SFA.DAS.Roatp.Application.Mediatr.Responses;
+using SFA.DAS.Roatp.Domain.Interfaces;
 
 namespace SFA.DAS.Roatp.Application.Shortlists.Queries.GetShortlistsForUser;
 
-public class GetShortlistForUserQueryHandler : IRequestHandler<GetShortlistsForUserQuery, ValidatedResponse<GetShortlistsForUserQueryResult>>
+public class GetShortlistForUserQueryHandler(IShortlistsRepository _shortlistsRepository) : IRequestHandler<GetShortlistsForUserQuery, ValidatedResponse<GetShortlistsForUserQueryResult>>
 {
     public async Task<ValidatedResponse<GetShortlistsForUserQueryResult>> Handle(GetShortlistsForUserQuery request, CancellationToken cancellationToken)
     {
-        return await Task.FromResult(new ValidatedResponse<GetShortlistsForUserQueryResult>(new GetShortlistsForUserQueryResult() { UserId = request.UserId }));
+        string jsonResult = await _shortlistsRepository.GetShortlistsForUser(request.UserId, cancellationToken);
+
+        var result = JsonSerializer.Deserialize<GetShortlistsForUserQueryResult>(jsonResult, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+
+        return await Task.FromResult(new ValidatedResponse<GetShortlistsForUserQueryResult>(result));
     }
 }
