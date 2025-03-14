a677d2f03cd5445d9bd323eb66325f9bf7f791fb
Deven Shah <devenshah1974@gmail.com>
2025-03-14 14:40:20 +0000

    CSP-1882 Enable info logs

--- src/SFA.DAS.Roatp.Jobs/Program.cs
+++ src/SFA.DAS.Roatp.Jobs/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.Roatp.Data.Extensions;
 using SFA.DAS.Roatp.Jobs.Extensions;
 
@@ -14,6 +15,19 @@ var host = new HostBuilder()
             .ConfigureFunctionsApplicationInsights()
             .AddRoatpDataContext(context.Configuration)
             .AddServiceRegistrations(context.Configuration);
+    })
+    .ConfigureLogging(logging =>
+    {
+        // This rule filters logs to capture only warnings and errors, removing this rule will allow Information logs to be captured
+        logging.Services.Configure<LoggerFilterOptions>(options =>
+        {
+            LoggerFilterRule defaultRule = options.Rules.FirstOrDefault(rule => rule.ProviderName
+                == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+            if (defaultRule is not null)
+            {
+                options.Rules.Remove(defaultRule);
+            }
+        });
     })
     .Build();
 
