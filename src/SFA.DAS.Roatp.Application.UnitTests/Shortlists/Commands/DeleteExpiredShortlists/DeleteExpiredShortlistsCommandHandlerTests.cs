dac7de6fde32799cbf67bffb42b411762b360753
Deven Shah <devenshah1974@gmail.com>
2025-03-18 15:38:17 +0000

    CSP-1882 CSP-1926 Implements delete expired shortlists function and api (#184)
    * CSP-1882 Adds ShortlistsExpiryDate on get shortlist for user response
    
    * CSP-1882 Adds delete expired shortlist stored procedure
    
    * CSP-1882 Implements Delete Expired Shortlists endpoint
    
    * CSP-1882 Enable info logs
    
    * CSP-1882 Fix date conversion in stored procedure
    
    * CSP-1926 Implements new function to delete expired shortlists
    
    * CSP-1926 Fixed package warnings

---
+++ src/SFA.DAS.Roatp.Application.UnitTests/Shortlists/Commands/DeleteExpiredShortlists/DeleteExpiredShortlistsCommandHandlerTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Roatp.Application.Shortlists.Commands.DeleteExpiredShortlists;
+using SFA.DAS.Roatp.Domain.Interfaces;
+
+namespace SFA.DAS.Roatp.Application.UnitTests.Shortlists.Commands.DeleteExpiredShortlists;
+
+public class DeleteExpiredShortlistsCommandHandlerTests
+{
+    [Test]
+    public async Task Handle_WhenCalled_ThenShouldDeleteExpiredShortlists()
+    {
+        Mock<IShortlistsRepository> repoMock = new();
+        // Arrange
+        var handler = new DeleteExpiredShortlistsCommandHandler(repoMock.Object);
+        var command = new DeleteExpiredShortlistsCommand();
+
+        // Act
+        await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        repoMock.Verify(x => x.DeleteExpiredShortlistItems(Application.Constants.ShortlistExpiryDays, CancellationToken.None), Times.Once);
+    }
+}
---
+++ src/SFA.DAS.Roatp.Jobs.UnitTests/Functions/DeleteExpiredShortlistsFunctionTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Roatp.Jobs.ApiClients;
+using SFA.DAS.Roatp.Jobs.Functions;
+
+namespace SFA.DAS.Roatp.Jobs.UnitTests.Functions;
+
+public class DeleteExpiredShortlistsFunctionTests
+{
+    [Test, AutoData]
+    public async Task DeleteExpiredShortlistsFunction_InvokesOuterApi(CancellationToken cancellationToken)
+    {
+        Mock<ICourseManagementOuterApiClient> mockClient = new();
+        DeleteExpiredShortlistsFunction sut = new(Mock.Of<ILogger<DeleteExpiredShortlistsFunction>>(), mockClient.Object);
+
+        await sut.Run(null, cancellationToken);
+
+        mockClient.Verify(c => c.Delete(DeleteExpiredShortlistsFunction.DeleteExpiredShortlistsUri, cancellationToken), Times.Once);
+    }
+}
