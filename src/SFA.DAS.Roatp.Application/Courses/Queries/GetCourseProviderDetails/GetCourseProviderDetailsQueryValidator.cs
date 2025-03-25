443701d6397b53bff26a27937e5278bcea0fced6
shaun-downey-education <shaun.downey@education.gov.uk>
2025-03-25 10:11:10 +0000

    CSP-1910: Replaced old provider course details endpoint with new course provider details endpoint.

---
+++ src/SFA.DAS.Roatp.Application/Courses/Queries/GetCourseProviderDetails/GetCourseProviderDetailsQueryValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace SFA.DAS.Roatp.Application.Courses.Queries.GetCourseProviderDetails;
+
+public sealed class GetCourseProviderDetailsQueryValidator : AbstractValidator<GetCourseProviderDetailsQuery>
+{
+    public GetCourseProviderDetailsQueryValidator()
+    {
+
+    }
+}
---
+++ src/SFA.DAS.Roatp.Domain/Models/GetCourseProviderDetailsParameters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SFA.DAS.Roatp.Domain.Models;
+
+public sealed class GetCourseProviderDetailsParameters
+{
+    public int LarsCode { get; set; }
+    public long Ukprn { get; set; }
+    public decimal? Lon { get; set; }
+    public decimal? Lat { get; set; }
+    public string Location { get; set; }
+    public Guid UserId { get; set; }
+}
