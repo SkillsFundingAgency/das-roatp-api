4a51a84202eb14a0639afba390439cd6828f9dd9
shaun-downey-education <shaun.downey@education.gov.uk>
2025-03-27 21:49:35 +0000

    CSP-1910: Update GetCourseProviderDetails_InvokesQueryHandler test to match new endpoint definition.

--- src/SFA.DAS.Roatp.Api.UnitTests/Controllers/ExternalReadControllers/CoursesControllerTests/GetCourseProviderDetailsTests.cs
+++ src/SFA.DAS.Roatp.Api.UnitTests/Controllers/ExternalReadControllers/CoursesControllerTests/GetCourseProviderDetailsTests.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.Roatp.Api.Controllers.ExternalReadControllers;
+using SFA.DAS.Roatp.Application.Common;
 using SFA.DAS.Roatp.Application.Courses.Queries.GetCourseProviderDetails;
 using SFA.DAS.Roatp.Application.Courses.Queries.GetProvidersFromLarsCode;
 using SFA.DAS.Roatp.Application.Mediatr.Responses;
@@ -20,7 +21,7 @@ public sealed class GetCourseProviderDetailsTests
     [Test]
     [MoqAutoData]
     public async Task GetCourseProviderDetails_InvokesQueryHandler(
-        GetCourseProviderDetailsQuery query,
+        GetCourseProviderDetailsRequest request,
         GetCourseProviderDetailsQueryResult queryResult,
         [Frozen] Mock<IMediator> mediatorMock,
         [Greedy] CoursesController sut
@@ -33,28 +34,24 @@ public sealed class GetCourseProviderDetailsTests
             It.Is<GetCourseProviderDetailsQuery>(q => 
                 q.LarsCode == larsCode &&
                 q.Ukprn == ukprn && 
-                q.Location == query.Location &&
-                q.Longitude == query.Longitude &&
-                q.ShortlistUserId == query.ShortlistUserId
+                q.Location == request.Location &&
+                q.Longitude == request.Longitude &&
+                q.ShortlistUserId == request.ShortlistUserId
             ), 
             It.IsAny<CancellationToken>())
         ).ReturnsAsync(new ValidatedResponse<GetCourseProviderDetailsQueryResult>(queryResult));
 
-        await sut.GetCourseProviderDetails(larsCode, ukprn, query);
+        await sut.GetCourseProviderDetails(larsCode, ukprn, request);
 
-        Assert.Multiple(() =>
-        {
-            Assert.That(query.LarsCode, Is.EqualTo(larsCode));
-            Assert.That(query.Ukprn, Is.EqualTo(ukprn));
-        });
-
-        mediatorMock.Verify(m => m.Send(It.Is<GetCourseProviderDetailsQuery>(
-            q =>
-                q.LarsCode == larsCode
-                && q.Latitude == query.Latitude
-                && q.Longitude == query.Longitude
-                && q.Location == query.Location
-                && q.ShortlistUserId == query.ShortlistUserId
-        ), It.IsAny<CancellationToken>()));
+        mediatorMock.Verify(m => 
+            m.Send(It.Is<GetCourseProviderDetailsQuery>(
+                q =>
+                    q.Ukprn == ukprn &&
+                    q.LarsCode == larsCode
+                    && q.Latitude == request.Latitude
+                    && q.Longitude == request.Longitude
+                    && q.Location == request.Location
+                    && q.ShortlistUserId == request.ShortlistUserId
+        ), It.IsAny<CancellationToken>()), Times.Once);
     }
 }
