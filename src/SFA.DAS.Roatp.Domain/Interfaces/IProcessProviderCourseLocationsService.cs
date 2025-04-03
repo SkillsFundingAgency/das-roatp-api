b8315915ad3a1719da94f290ead861ba716c5e51
shaun-downey-education <148352239+shaun-downey-education@users.noreply.github.com>
2025-04-03 14:14:50 +0100

    CSP-1910: Roatp API: New endpoint GET /api/courses/{larscode}/providers/{ukprn}/details
    * CSP-1910: Replaced old provider course details endpoint with new course provider details endpoint.
    
    * CSP-1910: Code refactor and additional unit test coverage.
    
    * CSP-1910: Update GetCourseProviderDetails_InvokesQueryHandler test to match new endpoint definition.
    
    * CSP-1910: Remove duplicated namespaces as per sonarcloud.
    
    * CSP-1910: Added GetCourseProviderDetails store procedure to branch.
    
    * CSP-1910: Ensure connection is closed ad removed from pool after querying data.
    
    * CSP-1910: Fixed url route on GetCourseProviderDetails

--- src/SFA.DAS.Roatp.Domain/Interfaces/IProcessProviderCourseLocationsService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using SFA.DAS.Roatp.Domain.Models;
-
-namespace SFA.DAS.Roatp.Domain.Interfaces;
-
-public interface IProcessProviderCourseLocationsService
-{
-    List<DeliveryModel> ConvertProviderLocationsToDeliveryModels(List<ProviderCourseLocationDetailsModel> providerCourseLocations);
-}
