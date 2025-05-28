f9f2a33ef3837347223b3915f6cd0e6084b89aec
PeterDavidWilliams <30286445+PeterDavidWilliams@users.noreply.github.com>
2025-05-28 14:37:50 +0100

    CSP-1978 - Configured azure logging (#208)

--- src/SFA.DAS.Roatp.Api/Startup.cs
+++ src/SFA.DAS.Roatp.Api/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.ApplicationInsights;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
@@ -48,9 +50,14 @@ public class Startup
     public void ConfigureServices(IServiceCollection services)
     {
         services
-            .AddLogging()
             .AddApplicationInsightsTelemetry()
             .AddTelemetryNotFoundAsSuccessfulResponse();
+
+        services.AddLogging(builder =>
+        {
+            builder.AddFilter<ApplicationInsightsLoggerProvider>(string.Empty, LogLevel.Information);
+            builder.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft", LogLevel.Information);
+        });
 
         if (!IsEnvironmentLocalOrDev)
         {
