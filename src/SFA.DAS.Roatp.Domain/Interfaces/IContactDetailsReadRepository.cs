5d805d337829d07a831e2e7533dacd3f300458eb
MarkFCain <mark.cain@digital.education.gov.uk>
2025-08-05 09:18:22 +0100

    CSP-2148 updated references from contactDetails to ProviderContacts

--- src/SFA.DAS.Roatp.Domain/Interfaces/IContactDetailsReadRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-using SFA.DAS.Roatp.Domain.Entities;
-
-namespace SFA.DAS.Roatp.Domain.Interfaces;
-
-public interface IContactDetailsReadRepository
-{
-    Task<ProviderContact> GetLatestProviderContact(int ukprn);
-}
