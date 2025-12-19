7498beb3946625215c761ec3da985da48b8acb5f
Ravi UDARI <Ravi.UDARI@EDUCATION.GOV.UK>
2025-12-19 13:13:05 +0000

    CSP-2354 Add CourseType filtering to standards API endpoint

--- src/SFA.DAS.Roatp.Application/Standards/Queries/GetAllStandardsQuery.cs
+++ src/SFA.DAS.Roatp.Application/Standards/Queries/GetAllStandardsQuery.cs
@@ -1,6 +1,15 @@
 ﻿using MediatR;
+using SFA.DAS.Roatp.Domain.Models;
 
 namespace SFA.DAS.Roatp.Application.Standards.Queries
 {
-    public class GetAllStandardsQuery : IRequest<GetAllStandardsQueryResult> { }
+
+    public class GetAllStandardsQuery : IRequest<GetAllStandardsQueryResult>
+    {
+        public CourseType? CourseType { get; }
+        public GetAllStandardsQuery(CourseType? courseType)
+        {
+            CourseType = courseType;
+        }
+    }
 }
