f6cf650da82c077fdb864b1fb3e0b936bf6e04da
Ravi UDARI <Ravi.UDARI@EDUCATION.GOV.UK>
2025-12-23 15:31:06 +0000

    CSP-2358 Added tests for patch command edge cases and validation

--- src/SFA.DAS.Roatp.Application.UnitTests/ProviderCourse/Commands/PatchProviderCourse/PatchProviderCourseCommandTests.cs
+++ src/SFA.DAS.Roatp.Application.UnitTests/ProviderCourse/Commands/PatchProviderCourse/PatchProviderCourseCommandTests.cs
@@ -140,5 +140,84 @@ namespace SFA.DAS.Roatp.Application.UnitTests.ProviderCourse.Commands.PatchProvi
             command.StandardInfoUrl.Should().BeNull();
             command.IsPresentStandardInfoUrl.Should().BeFalse();
         }
+
+        [Test]
+        public void Command_PatchContainsStandardInfoUrlWithUppercaseReplace_StandardInfoUrlIsSet()
+        {
+            var ukprn = 10000002;
+            var larsCode = "2";
+            var testValue = "upper-case-replace";
+            var patchCommand = new JsonPatchDocument<Domain.Models.PatchProviderCourse>();
+            patchCommand.Operations.Add(new Operation<Domain.Models.PatchProviderCourse> { op = "REPLACE", path = StandardInfoUrl, value = testValue });
+
+            var command = new PatchProviderCourseCommand
+            {
+                Ukprn = ukprn,
+                LarsCode = larsCode,
+                Patch = patchCommand
+            };
+
+            command.StandardInfoUrl.Should().Be(testValue);
+            command.IsPresentStandardInfoUrl.Should().BeTrue();
+        }
+
+        [Test]
+        public void Command_PatchContainsStandardInfoUrlWithNonReplaceOp_Ignored()
+        {
+            var ukprn = 10000003;
+            var larsCode = "3";
+            var patchCommand = new JsonPatchDocument<Domain.Models.PatchProviderCourse>();
+            patchCommand.Operations.Add(new Operation<Domain.Models.PatchProviderCourse> { op = "add", path = StandardInfoUrl, value = "ignored" });
+
+            var command = new PatchProviderCourseCommand
+            {
+                Ukprn = ukprn,
+                LarsCode = larsCode,
+                Patch = patchCommand
+            };
+
+            command.StandardInfoUrl.Should().BeNull();
+            command.IsPresentStandardInfoUrl.Should().BeFalse();
+        }
+
+        [Test]
+        public void Command_PatchContainsIsApprovedByRegulatorWithInvalidValue_IsApprovedByRegulatorIsNullButPresent()
+        {
+            var ukprn = 10000004;
+            var larsCode = "4";
+            var invalidValue = "not-a-bool";
+            var patchCommand = new JsonPatchDocument<Domain.Models.PatchProviderCourse>();
+            patchCommand.Operations.Add(new Operation<Domain.Models.PatchProviderCourse> { op = Replace, path = IsApprovedByRegulator, value = invalidValue });
+
+            var command = new PatchProviderCourseCommand
+            {
+                Ukprn = ukprn,
+                LarsCode = larsCode,
+                Patch = patchCommand
+            };
+
+            command.IsApprovedByRegulator.Should().BeNull();
+            command.IsPresentIsApprovedByRegulator.Should().BeTrue();
+        }
+
+        [Test]
+        public void Command_PatchContainsHasOnlineDeliveryOptionWithInvalidValue_HasOnlineDeliveryOptionIsNullButPresent()
+        {
+            var ukprn = 10000005;
+            var larsCode = "5";
+            var invalidValue = "123";
+            var patchCommand = new JsonPatchDocument<Domain.Models.PatchProviderCourse>();
+            patchCommand.Operations.Add(new Operation<Domain.Models.PatchProviderCourse> { op = Replace, path = HasOnlineDeliveryOption, value = invalidValue });
+
+            var command = new PatchProviderCourseCommand
+            {
+                Ukprn = ukprn,
+                LarsCode = larsCode,
+                Patch = patchCommand
+            };
+
+            command.HasOnlineDeliveryOption.Should().BeNull();
+            command.IsPresentHasOnlineDeliveryOption.Should().BeTrue();
+        }
     }
 }
