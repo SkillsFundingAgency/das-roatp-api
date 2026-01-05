e65a1e11955f3a514dd39158f63bb9570d19c2d3
Ravi UDARI <Ravi.UDARI@EDUCATION.GOV.UK>
2026-01-05 14:12:49 +0000

    CSP-2351 Renamed tests as per review comments and separated logger checks test

--- src/SFA.DAS.Roatp.Application.UnitTests/Standards/Queries/GetStandardForLarsCode/GetStandardForLarsCodeQueryHandlerTests.cs
+++ src/SFA.DAS.Roatp.Application.UnitTests/Standards/Queries/GetStandardForLarsCode/GetStandardForLarsCodeQueryHandlerTests.cs
@@ -11,75 +11,76 @@ using SFA.DAS.Roatp.Domain.Entities;
 using SFA.DAS.Roatp.Domain.Interfaces;
 using SFA.DAS.Testing.AutoFixture;
 
-namespace SFA.DAS.Roatp.Application.UnitTests.Standards.Queries.GetStandardForLarsCode
+namespace SFA.DAS.Roatp.Application.UnitTests.Standards.Queries.GetStandardForLarsCode;
+
+[TestFixture]
+public class GetStandardForLarsCodeQueryHandlerTests
 {
-    [TestFixture]
-    public class GetStandardForLarsCodeQueryHandlerTests
+    [Test, RecursiveMoqAutoData()]
+    public async Task Handle_WhenStandardExistsForLarsCode_ReturnsValidatedResponse(
+        string larsCode,
+        [Frozen] Mock<IStandardsReadRepository> repositoryMock,
+        [Frozen] Mock<ILogger<GetStandardForLarsCodeQueryHandler>> loggerMock,
+        GetStandardForLarsCodeQueryHandler sut)
     {
-        [Test, RecursiveMoqAutoData()]
-        public async Task Handle_WhenStandardExistsForLarsCode_ReturnsValidatedResponseWithStandardAndLogsInformation(
-            string larsCode,
-            [Frozen] Mock<IStandardsReadRepository> repositoryMock,
-            [Frozen] Mock<ILogger<GetStandardForLarsCodeQueryHandler>> loggerMock,
-            GetStandardForLarsCodeQueryHandler sut)
-        {
-            // Arrange
-            var expectedStandard = new Standard { LarsCode = larsCode };
-            repositoryMock.Setup(r => r.GetStandard(It.Is<string>(s => s == larsCode)))
-                          .ReturnsAsync(expectedStandard);
+        var expectedStandard = new Standard { LarsCode = larsCode };
+        repositoryMock.Setup(r => r.GetStandard(It.Is<string>(s => s == larsCode)))
+            .ReturnsAsync(expectedStandard);
 
-            var query = new GetStandardForLarsCodeQuery(larsCode);
+        var query = new GetStandardForLarsCodeQuery(larsCode);
 
-            // Act
-            var response = await sut.Handle(query, CancellationToken.None);
+        var response = await sut.Handle(query, CancellationToken.None);
 
-            // Assert
-            response.Should().NotBeNull();
-            response.Result.Should().NotBeNull();
-            response.Result.Should().BeEquivalentTo((GetStandardForLarsCodeQueryResult)expectedStandard);
+        response.Should().NotBeNull();
+        response.Result.Should().NotBeNull();
+        response.Result.Should().BeEquivalentTo((GetStandardForLarsCodeQueryResult)expectedStandard);
 
-            repositoryMock.Verify(r => r.GetStandard(larsCode), Times.Once);
+        repositoryMock.Verify(r => r.GetStandard(larsCode), Times.Once);
 
-            loggerMock.Verify(
-                x => x.Log(
-                    LogLevel.Information,
-                    It.IsAny<EventId>(),
-                    It.Is<It.IsAnyType>((v, t) => v.ToString().Contains("Returning standard for larsCode") && v.ToString().Contains(larsCode)),
-                    null,
-                    It.IsAny<Func<It.IsAnyType, Exception, string>>()),
-                Times.Once);
-        }
+    }
 
-        [Test, RecursiveMoqAutoData()]
-        public async Task Handle_WhenStandardDoesNotExistForLarsCode_ReturnsValidatedResponseWithNullStandardAndLogsInformation(
-            string larsCode,
-            [Frozen] Mock<IStandardsReadRepository> repositoryMock,
-            [Frozen] Mock<ILogger<GetStandardForLarsCodeQueryHandler>> loggerMock,
-            GetStandardForLarsCodeQueryHandler sut)
-        {
-            // Arrange
-            repositoryMock.Setup(r => r.GetStandard(It.Is<string>(s => s == larsCode)))
-                          .ReturnsAsync((Standard)null);
+    [Test, RecursiveMoqAutoData()]
+    public async Task Handle_WhenStandardDoesNotExistForLarsCode_ReturnsValidatedResponseWithNullStandard(
+        string larsCode,
+        [Frozen] Mock<IStandardsReadRepository> repositoryMock,
+        [Frozen] Mock<ILogger<GetStandardForLarsCodeQueryHandler>> loggerMock,
+        GetStandardForLarsCodeQueryHandler sut)
+    {
+        repositoryMock.Setup(r => r.GetStandard(It.Is<string>(s => s == larsCode)))
+            .ReturnsAsync((Standard)null);
 
-            var query = new GetStandardForLarsCodeQuery(larsCode);
+        var query = new GetStandardForLarsCodeQuery(larsCode);
 
-            // Act
-            var response = await sut.Handle(query, CancellationToken.None);
+        var response = await sut.Handle(query, CancellationToken.None);
 
-            // Assert
-            response.Should().NotBeNull();
-            response.Result.Should().BeNull();
+        response.Should().NotBeNull();
+        response.Result.Should().BeNull();
 
-            repositoryMock.Verify(r => r.GetStandard(larsCode), Times.Once);
+        repositoryMock.Verify(r => r.GetStandard(larsCode), Times.Once);
+    }
 
-            loggerMock.Verify(
-                x => x.Log(
-                    LogLevel.Information,
-                    It.IsAny<EventId>(),
-                    It.Is<It.IsAnyType>((v, t) => v.ToString().Contains("Returning standard for larsCode") && v.ToString().Contains(larsCode)),
-                    null,
-                    It.IsAny<Func<It.IsAnyType, Exception, string>>()),
-                Times.Once);
-        }
+    [Test, RecursiveMoqAutoData()]
+    public async Task Handle_WhenStandardExists_VerifyLogger(
+        string larsCode,
+        [Frozen] Mock<IStandardsReadRepository> repositoryMock,
+        [Frozen] Mock<ILogger<GetStandardForLarsCodeQueryHandler>> loggerMock,
+        GetStandardForLarsCodeQueryHandler sut)
+    {
+        var expectedStandard = new Standard { LarsCode = larsCode };
+        repositoryMock.Setup(r => r.GetStandard(It.Is<string>(s => s == larsCode)))
+            .ReturnsAsync(expectedStandard);
+
+        var query = new GetStandardForLarsCodeQuery(larsCode);
+
+        var response = await sut.Handle(query, CancellationToken.None);
+
+        loggerMock.Verify(
+            x => x.Log(
+                LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString().Contains("Returning standard for larsCode") && v.ToString().Contains(larsCode)),
+                null,
+                It.IsAny<Func<It.IsAnyType, Exception, string>>()),
+            Times.Once);
     }
 }
