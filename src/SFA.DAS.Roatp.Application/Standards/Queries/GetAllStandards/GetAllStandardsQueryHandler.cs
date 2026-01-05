6aa68853c3a0c2e2c5f169ee4a199bd8dca0bf00
Ravi UDARI <Ravi.UDARI@EDUCATION.GOV.UK>
2026-01-05 12:04:00 +0000

    CSP-2354 Refactor to use file-scoped namespaces and  introduced an implicit operator for mapping

--- src/SFA.DAS.Roatp.Application/Standards/Queries/GetAllStandards/GetAllStandardsQueryHandler.cs
+++ src/SFA.DAS.Roatp.Application/Standards/Queries/GetAllStandards/GetAllStandardsQueryHandler.cs
@@ -8,34 +8,33 @@ using SFA.DAS.Roatp.Domain.Entities;
 using SFA.DAS.Roatp.Domain.Interfaces;
 using SFA.DAS.Roatp.Domain.Models;
 
-namespace SFA.DAS.Roatp.Application.Standards.Queries.GetAllStandards
+namespace SFA.DAS.Roatp.Application.Standards.Queries.GetAllStandards;
+
+public class GetAllStandardsQueryHandler : IRequestHandler<GetAllStandardsQuery, GetAllStandardsQueryResult>
 {
-    public class GetAllStandardsQueryHandler : IRequestHandler<GetAllStandardsQuery, GetAllStandardsQueryResult>
+    private readonly IStandardsReadRepository _standardsReadRepository;
+    private readonly ILogger<GetAllStandardsQueryHandler> _logger;
+
+    public GetAllStandardsQueryHandler(IStandardsReadRepository standardsReadRepository, ILogger<GetAllStandardsQueryHandler> logger)
     {
-        private readonly IStandardsReadRepository _standardsReadRepository;
-        private readonly ILogger<GetAllStandardsQueryHandler> _logger;
+        _standardsReadRepository = standardsReadRepository;
+        _logger = logger;
+    }
 
-        public GetAllStandardsQueryHandler(IStandardsReadRepository standardsReadRepository, ILogger<GetAllStandardsQueryHandler> logger)
+    public async Task<GetAllStandardsQueryResult> Handle(GetAllStandardsQuery request, CancellationToken cancellationToken)
+    {
+        var allStandards = await _standardsReadRepository.GetAllStandards();
+        if (request.CourseType.HasValue)
         {
-            _standardsReadRepository = standardsReadRepository;
-            _logger = logger;
+            allStandards = FilterStandardsWithCourseType(allStandards, request.CourseType.Value);
         }
-
-        public async Task<GetAllStandardsQueryResult> Handle(GetAllStandardsQuery request, CancellationToken cancellationToken)
-        {
-            var allStandards = await _standardsReadRepository.GetAllStandards();
-            if (request.CourseType.HasValue)
-            {
-                allStandards = FilterStandardsWithCourseType(allStandards, request.CourseType.Value);
-            }
-            _logger.LogInformation("Returning {AllStandardsCount} standards", allStandards.Count);
-            return new GetAllStandardsQueryResult(allStandards);
-        }
-        private static List<Standard> FilterStandardsWithCourseType(List<Standard> allStandards, CourseType courseTypeFilter)
-        {
-            return allStandards
-                .Where(c => c.CourseType == courseTypeFilter)
-                .ToList();
-        }
+        _logger.LogInformation("Returning {AllStandardsCount} standards", allStandards.Count);
+        return new GetAllStandardsQueryResult(allStandards);
+    }
+    private static List<Standard> FilterStandardsWithCourseType(List<Standard> allStandards, CourseType courseTypeFilter)
+    {
+        return allStandards
+            .Where(c => c.CourseType == courseTypeFilter)
+            .ToList();
     }
 }
