c6023cb16d15c2021bf59c01b7f73e6ec2498826
Ravi UDARI <Ravi.UDARI@EDUCATION.GOV.UK>
2026-01-09 09:09:55 +0000

    CSP-2358 Refactored unit tests to target PatchProviderCommand and PatchProvider model.

--- src/SFA.DAS.Roatp.Application.UnitTests/Providers/Commands/PatchProvider/PatchProviderCommandTests.cs
+++ src/SFA.DAS.Roatp.Application.UnitTests/Providers/Commands/PatchProvider/PatchProviderCommandTests.cs
@@ -2,8 +2,7 @@
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.JsonPatch.Operations;
 using NUnit.Framework;
-using SFA.DAS.Roatp.Application.ProviderCourse.Commands.PatchProviderCourse;
-using SFA.DAS.Roatp.Domain.Models;
+using SFA.DAS.Roatp.Application.Providers.Commands.PatchProvider;
 
 namespace SFA.DAS.Roatp.Application.UnitTests.Providers.Commands.PatchProvider;
 
@@ -11,114 +10,39 @@ namespace SFA.DAS.Roatp.Application.UnitTests.Providers.Commands.PatchProvider;
 public class PatchProviderCommandTests
 {
     private const string Replace = "replace";
-    private const string ContactUsEmail = "ContactUsEmail";
-    private const string ContactUsPhoneNumber = "ContactUsPhoneNumber";
-    private const string StandardInfoUrl = "StandardInfoUrl";
-    private const string IsApprovedByRegulator = "IsApprovedByRegulator";
+    private const string MarketingInfo = "MarketingInfo";
 
     [Test]
-    public void Command_PatchContainsStandardInfoUrl_StandardInfoUrlIsSet()
+    public void Command_PatchContainsMarketingInfo_MarketingInfoIsSet()
     {
         var ukprn = 10000001;
-        var larsCode = "1";
         var testValue = "value";
-        var patchCommand = new JsonPatchDocument<PatchProviderCourse>();
-        patchCommand.Operations.Add(new Operation<PatchProviderCourse> { op = Replace, path = StandardInfoUrl, value = testValue });
+        var patchCommand = new JsonPatchDocument<Domain.Models.PatchProvider>();
+        patchCommand.Operations.Add(new Operation<Domain.Models.PatchProvider> { op = Replace, path = MarketingInfo, value = testValue });
 
-        var command = new PatchProviderCourseCommand
+        var command = new PatchProviderCommand
         {
             Ukprn = ukprn,
-            LarsCode = larsCode,
             Patch = patchCommand
         };
 
-        testValue.Should().Be(command.StandardInfoUrl);
-        command.IsPresentStandardInfoUrl.Should().BeTrue();
-    }
-
-    [Test]
-    public void Command_PatchContainsContactUsEmail_ContactUsEmailIsSet()
-    {
-        var ukprn = 10000001;
-        var larsCode = "1";
-        var testValue = "value";
-        var patchCommand = new JsonPatchDocument<PatchProviderCourse>();
-        patchCommand.Operations.Add(new Operation<PatchProviderCourse> { op = Replace, path = ContactUsEmail, value = testValue });
-
-        var command = new PatchProviderCourseCommand
-        {
-            Ukprn = ukprn,
-            LarsCode = larsCode,
-            Patch = patchCommand
-        };
-
-        testValue.Should().Be(command.ContactUsEmail);
-        command.IsPresentContactUsEmail.Should().BeTrue();
-    }
-
-    [Test]
-    public void Command_PatchContainsContactUsPhoneNumber_ContactUsPhoneNumberIsSet()
-    {
-        var ukprn = 10000001;
-        var larsCode = "1";
-        var testValue = "value";
-        var patchCommand = new JsonPatchDocument<PatchProviderCourse>();
-        patchCommand.Operations.Add(new Operation<PatchProviderCourse> { op = Replace, path = ContactUsPhoneNumber, value = testValue });
-
-        var command = new PatchProviderCourseCommand
-        {
-            Ukprn = ukprn,
-            LarsCode = larsCode,
-            Patch = patchCommand
-        };
-
-        testValue.Should().Be(command.ContactUsPhoneNumber);
-        command.IsPresentContactUsPhoneNumber.Should().BeTrue();
-    }
-
-
-    [Test]
-    public void Command_PatchContainsIsApprovedByRegulator_IsApprovedByRegulatorIsSet()
-    {
-        var ukprn = 10000001;
-        var larsCode = "1";
-        var testValue = "true";
-        var patchCommand = new JsonPatchDocument<PatchProviderCourse>();
-        patchCommand.Operations.Add(new Operation<PatchProviderCourse> { op = Replace, path = IsApprovedByRegulator, value = testValue });
-
-        var command = new PatchProviderCourseCommand
-        {
-            Ukprn = ukprn,
-            LarsCode = larsCode,
-            Patch = patchCommand
-        };
-
-        command.IsApprovedByRegulator.Should().BeTrue();
-        command.IsPresentIsApprovedByRegulator.Should().BeTrue();
+        testValue.Should().Be(command.MarketingInfo);
+        command.IsPresentMarketingInfo.Should().BeTrue();
     }
 
     [Test]
     public void Command_PatchContainsNoDetails_FieldsAreNotSet()
     {
         var ukprn = 10000001;
-        var larsCode = "1";
-        var patchCommand = new JsonPatchDocument<PatchProviderCourse>();
+        var patchCommand = new JsonPatchDocument<Domain.Models.PatchProvider>();
 
-        var command = new PatchProviderCourseCommand
+        var command = new PatchProviderCommand
         {
             Ukprn = ukprn,
-            LarsCode = larsCode,
             Patch = patchCommand
         };
 
-        command.IsApprovedByRegulator.Should().BeNull();
-        command.IsPresentIsApprovedByRegulator.Should().BeFalse();
-        command.ContactUsEmail.Should().BeNull();
-        command.IsPresentContactUsEmail.Should().BeFalse();
-        command.ContactUsPhoneNumber.Should().BeNull();
-        command.IsPresentContactUsPhoneNumber.Should().BeFalse();
-        command.StandardInfoUrl.Should().BeNull();
-        command.IsPresentHasOnlineDeliveryOption.Should().BeFalse();
-        command.IsPresentStandardInfoUrl.Should().BeFalse();
+        command.MarketingInfo.Should().BeNull();
+        command.IsPresentMarketingInfo.Should().BeFalse();
     }
 }
