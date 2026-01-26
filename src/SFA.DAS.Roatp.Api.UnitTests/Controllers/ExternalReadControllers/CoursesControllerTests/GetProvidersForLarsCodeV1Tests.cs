b1ac1c959307bbcd79f8f84f99df0d69d7e79ba0
Ravi UDARI <Ravi.UDARI@EDUCATION.GOV.UK>
2026-01-26 09:14:01 +0000

    CSP-2379 Added unit tests to verify CoursesController's GetProvidersForLarsCode endpoint returns NotFound for null/valid results and BadRequest for validation errors.

--- src/SFA.DAS.Roatp.Api.UnitTests/Controllers/ExternalReadControllers/CoursesControllerTests/GetProvidersForLarsCodeV1Tests.cs
+++ src/SFA.DAS.Roatp.Api.UnitTests/Controllers/ExternalReadControllers/CoursesControllerTests/GetProvidersForLarsCodeV1Tests.cs
@@ -1,8 +1,10 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using AutoFixture.NUnit3;
 using FluentAssertions;
 using FluentAssertions.Execution;
+using FluentValidation.Results;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
@@ -74,6 +76,87 @@ namespace SFA.DAS.Roatp.Api.UnitTests.Controllers.ExternalReadControllers.Course
                     && q.Location == request.Location
                     && q.UserId == request.UserId),
                 It.IsAny<CancellationToken>()));
+        }
+
+        [Test, MoqAutoData]
+        public async Task GetProvidersForLarsCodeV1_WhenResultNullAndValid_ReturnsNotFound(
+            GetProvidersFromLarsCodeRequest request,
+            int larsCodeInt,
+            GetProvidersForLarsCodeQueryResultV2 queryResultV2,
+            [Frozen] Mock<IMediator> mediatorMock,
+            [Greedy] CoursesController sut)
+        {
+            var response = new ValidatedResponse<GetProvidersForLarsCodeQueryResultV2>(new List<ValidationFailure>());
+            mediatorMock.Setup(m => m.Send(
+                  It.Is<GetProvidersForLarsCodeQueryV2>(q =>
+                      q.LarsCode == larsCodeInt.ToString()
+                      && q.Latitude == request.Latitude
+                      && q.Longitude == request.Longitude
+                      && q.OrderBy == request.OrderBy
+                      && q.Distance == request.Distance
+                      && q.Page == request.Page
+                      && q.PageSize == request.PageSize
+                      && q.Location == request.Location
+                      && q.UserId == request.UserId),
+                  It.IsAny<CancellationToken>()))
+              .ReturnsAsync(response);
+
+            var result = await sut.GetProvidersForLarsCode(larsCodeInt, request);
+
+            result.Should().BeOfType<NotFoundResult>();
+
+            mediatorMock.Verify(m => m.Send(
+                It.Is<GetProvidersForLarsCodeQueryV2>(q => q.LarsCode == larsCodeInt.ToString()),
+                It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Test, MoqAutoData]
+        public async Task GetProvidersForLarsCodeV1_WhenInvalid_ReturnsBadRequest(
+            GetProvidersFromLarsCodeRequest request,
+            int larsCodeInt,
+            GetProvidersForLarsCodeQueryResultV2 queryResultV2,
+            [Frozen] Mock<IMediator> mediatorMock,
+            [Greedy] CoursesController sut)
+        {
+            var validationErrors = new List<ValidationFailure>
+            {
+             new ValidationFailure("Latitude", "Latitude is required"),
+             new ValidationFailure("Longitude", "Longitude is required")
+            };
+
+            var response = new ValidatedResponse<GetProvidersForLarsCodeQueryResultV2>(validationErrors);
+
+            mediatorMock.Setup(m => m.Send(
+                  It.Is<GetProvidersForLarsCodeQueryV2>(q =>
+                      q.LarsCode == larsCodeInt.ToString()
+                      && q.Latitude == request.Latitude
+                      && q.Longitude == request.Longitude
+                      && q.OrderBy == request.OrderBy
+                      && q.Distance == request.Distance
+                      && q.Page == request.Page
+                      && q.PageSize == request.PageSize
+                      && q.Location == request.Location
+                      && q.UserId == request.UserId),
+                  It.IsAny<CancellationToken>()))
+              .ReturnsAsync(response);
+
+            var result = await sut.GetProvidersForLarsCode(larsCodeInt, request);
+
+            result.Should().BeOfType<BadRequestObjectResult>();
+            var badRequest = (BadRequestObjectResult)result;
+            badRequest.Value.Should().NotBeNull();
+
+            badRequest.Value.Should().BeAssignableTo<List<SFA.DAS.Roatp.Api.Infrastructure.ValidationError>>();
+            var errors = (List<SFA.DAS.Roatp.Api.Infrastructure.ValidationError>)badRequest.Value;
+            errors.Should().HaveCount(2);
+            errors[0].PropertyName.Should().Be("Latitude");
+            errors[0].ErrorMessage.Should().Be("Latitude is required");
+            errors[1].PropertyName.Should().Be("Longitude");
+            errors[1].ErrorMessage.Should().Be("Longitude is required");
+
+            mediatorMock.Verify(m => m.Send(
+                It.Is<GetProvidersForLarsCodeQueryV2>(q => q.LarsCode == larsCodeInt.ToString()),
+                It.IsAny<CancellationToken>()), Times.Once);
         }
     }
 }
