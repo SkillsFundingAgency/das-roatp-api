c7409de12c666cc002dcc134b154862ffc8ece13
Ravi UDARI <Ravi.UDARI@EDUCATION.GOV.UK>
2026-01-29 18:37:09 +0000

    CSP-2381 CSP-2382 Refactored ProviderCourseModelExternalModelV1 to inherit from ProviderCourseModelBase

--- src/SFA.DAS.Roatp.Api/Models/ProviderCourseModelExternalModelV1.cs
+++ src/SFA.DAS.Roatp.Api/Models/ProviderCourseModelExternalModelV1.cs
@@ -1,29 +1,11 @@
-﻿using SFA.DAS.Roatp.Application.ProviderCourse.Queries.ExternalRead.GetProviderCourse;
-using SFA.DAS.Roatp.Domain.Models;
+﻿using SFA.DAS.Roatp.Application.ProviderCourse.Queries;
+using SFA.DAS.Roatp.Application.ProviderCourse.Queries.ExternalRead.GetProviderCourse;
 
 namespace SFA.DAS.Roatp.Api.Models;
 
-public class ProviderCourseModelExternalModelV1
+public class ProviderCourseModelExternalModelV1 : ProviderCourseModelBase
 {
     public int LarsCode { get; set; }
-    public int ProviderCourseId { get; set; }
-    public string CourseName { get; set; }
-    public CourseType? CourseType { get; set; }
-    public int Level { get; set; }
-    public string IfateReferenceNumber { get; set; }
-    public string StandardInfoUrl { get; set; }
-    public string ContactUsPhoneNumber { get; set; }
-    public string ContactUsEmail { get; set; }
-    public bool? IsApprovedByRegulator { get; set; }
-    public bool? HasNationalDeliveryOption { get; set; }
-    public bool? HasHundredPercentEmployerDeliveryOption { get; set; }
-    public bool HasPortableFlexiJobOption { get; set; }
-    public bool HasOnlineDeliveryOption { get; set; }
-    public string ApprovalBody { get; set; }
-    public bool IsRegulatedForProvider { get; set; }
-    public bool HasLocations { get; set; }
-
-
     public static implicit operator ProviderCourseModelExternalModelV1(ProviderCourseModelExternal providerCourseModel)
     {
         if (providerCourseModel == null) return null;
