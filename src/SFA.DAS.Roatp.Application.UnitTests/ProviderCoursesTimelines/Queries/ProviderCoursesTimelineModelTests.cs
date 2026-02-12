9f6ba6cfad916801cc416844100f539c65f961ba
Deven Shah <devenshah1974@gmail.com>
2026-02-12 15:28:52 +0000

    CSP-2449 CSP-2383 Fixed employer provider issue

--- src/SFA.DAS.Roatp.Application.UnitTests/ProviderCoursesTimelines/Queries/ProviderCoursesTimelineModelTests.cs
+++ src/SFA.DAS.Roatp.Application.UnitTests/ProviderCoursesTimelines/Queries/ProviderCoursesTimelineModelTests.cs
@@ -1,29 +1,62 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
+using FluentAssertions;
 using NUnit.Framework;
 using SFA.DAS.Roatp.Application.ProviderCoursesTimelines.Queries;
+using SFA.DAS.Roatp.Domain.Entities;
 using SFA.DAS.Roatp.Domain.Models;
 
 namespace SFA.DAS.Roatp.Application.UnitTests.ProviderCoursesTimelines.Queries;
 
 public class ProviderCoursesTimelineModelTests
 {
-    [Test]
-    public void Operator_ConvertsFromProviderRegistrationDetails()
+    ProviderRegistrationDetail _expected = null;
+    ProviderCoursesTimelineModel _actual = null;
+
+    [SetUp]
+    public void BeforeEachTest()
     {
-        var providerRegistrationDetails = TestDataHelper.GetProviderRegistrationDetails();
-        ProviderCoursesTimelineModel providerModel = providerRegistrationDetails;
-        Assert.IsNotNull(providerModel);
-        Assert.AreEqual(12345678, providerModel.Ukprn);
-        Assert.AreEqual(ProviderStatusType.Active, providerModel.Status);
-        Assert.AreEqual(ProviderType.Main, providerModel.ProviderType);
-        Assert.AreEqual(1, providerModel.CourseTypes.Count());
-        var courseTypeModel = providerModel.CourseTypes.First();
-        Assert.AreEqual(CourseType.Apprenticeship, courseTypeModel.CourseType);
-        Assert.AreEqual(1, courseTypeModel.Courses.Count());
-        var courseTimelineModel = courseTypeModel.Courses.First();
-        Assert.AreEqual("LARS123", courseTimelineModel.LarsCode);
-        Assert.AreEqual(new DateTime(2023, 1, 1, 0, 0, 0, DateTimeKind.Unspecified), courseTimelineModel.EffectiveFrom);
-        Assert.IsNull(courseTimelineModel.EffectiveTo);
+        _expected = TestDataHelper.GetProviderRegistrationDetails();
+        _actual = _expected;
+    }
+
+    [Test]
+    public void Operator_ReturnsNull()
+    {
+        ProviderRegistrationDetail expected = null;
+
+        ProviderCoursesTimelineModel actual = expected;
+
+        actual.Should().BeNull();
+    }
+
+    [Test]
+    public void Operator_ConvertsFromProviderRegistrationDetails() => _actual.Should().NotBeNull();
+
+    [Test]
+    public void Operator_SetsUkprn() => _actual.Ukprn.Should().Be(_expected.Ukprn);
+
+    [Test]
+    public void Operator_SetsProviderStatusType() => _actual.Status.Should().Be(ProviderStatusType.Active);
+
+    [Test]
+    public void Operator_SetsProviderType() => _actual.ProviderType.Should().Be(ProviderType.Main);
+
+    [Test]
+    public void Operator_SetsCourseTypes() => _actual.CourseTypes.Should().HaveCount(2);
+
+    [Test]
+    public void Operator_HasNoCourseForCourseType_SetsEmptyCourse() => _actual.CourseTypes.First(c => c.CourseType == CourseType.ShortCourse).Courses.Should().HaveCount(0);
+
+    [Test]
+    public void Operator_HasCourseForCourseType_SetsCourse() => _actual.CourseTypes.First(c => c.CourseType == CourseType.Apprenticeship).Courses.Should().HaveCount(1);
+
+    [Test]
+    public void Operator_SetsLarsCodeInCourses()
+    {
+        var expected = _expected.Provider.ProviderCoursesTimelines[0];
+        var actual = _actual.CourseTypes.First(c => c.CourseType == CourseType.Apprenticeship).Courses.First();
+        actual.LarsCode.Should().Be(expected.LarsCode);
+        actual.EffectiveFrom.Should().Be(expected.EffectiveFrom);
+        actual.EffectiveTo.Should().Be(expected.EffectiveTo);
     }
 }
