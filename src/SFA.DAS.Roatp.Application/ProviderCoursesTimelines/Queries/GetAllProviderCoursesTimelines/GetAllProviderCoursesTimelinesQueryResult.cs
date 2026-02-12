dd8da15a48099298a241ef6a3fb26e2fe413c11b
Deven Shah <devenshah1974@gmail.com>
2026-02-12 12:49:49 +0000

    CSP-2383 Implements get specific proivder timeline

--- src/SFA.DAS.Roatp.Application/ProviderCoursesTimelines/Queries/GetAllProviderCoursesTimelines/GetAllProviderCoursesTimelinesQueryResult.cs
+++ src/SFA.DAS.Roatp.Application/ProviderCoursesTimelines/Queries/GetAllProviderCoursesTimelines/GetAllProviderCoursesTimelinesQueryResult.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using SFA.DAS.Roatp.Domain.Entities;
-using SFA.DAS.Roatp.Domain.Models;
 
 namespace SFA.DAS.Roatp.Application.ProviderCoursesTimelines.Queries.GetAllProviderCoursesTimelines;
 
@@ -14,32 +12,10 @@ public class GetAllProviderCoursesTimelinesQueryResult
     {
         GetAllProviderCoursesTimelinesQueryResult result = new()
         {
-            Providers = providers.Select(p => new ProviderCoursesTimelineModel
-            {
-                Ukprn = p.Ukprn,
-                Status = (ProviderStatusType)p.StatusId,
-                ProviderType = (ProviderType)p.ProviderTypeId,
-                CourseTypes = p.Provider?.ProviderCourseTypes
-                    .Select(ct => new CourseTypeModel(
-                        ct.CourseType,
-                        p.Provider.ProviderCoursesTimelines
-                            .Where(t => t.Standard.CourseType == ct.CourseType)
-                            .Select(t => new CoursesTimelineModel(t.LarsCode, t.EffectiveFrom, t.EffectiveTo))))
-                    ?? []
-            })
+            Providers = providers.Select(p => (ProviderCoursesTimelineModel)p)
         };
         return result;
     }
 }
 
-public class ProviderCoursesTimelineModel
-{
-    public int Ukprn { get; set; }
-    public ProviderStatusType Status { get; set; }
-    public ProviderType ProviderType { get; set; }
-    public IEnumerable<CourseTypeModel> CourseTypes { get; set; } = [];
-}
 
-public record CourseTypeModel(CourseType CourseType, IEnumerable<CoursesTimelineModel> Courses);
-
-public record CoursesTimelineModel(string LarsCode, DateTime EffectiveFrom, DateTime? EffectiveTo);
