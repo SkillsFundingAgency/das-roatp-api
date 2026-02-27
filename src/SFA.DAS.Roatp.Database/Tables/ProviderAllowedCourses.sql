cc5818327f6ae15053613920729b01f5f4846bc5
Deven Shah <devenshah1974@gmail.com>
2026-02-27 16:12:27 +0000

    CSP-2472 Added ProviderAllowedCourses table

---
+++ src/SFA.DAS.Roatp.Database/Tables/ProviderAllowedCourses.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[ProviderAllowedCourses]
+(
+  [Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+  [Ukprn] INT NOT NULL,
+  [LarsCode] VARCHAR(20) NOT NULL
+);
+
+GO;
+
+CREATE UNIQUE INDEX IX_ProviderAllowedCourses_Ukprn_LarsCode
+ON [dbo].[ProviderAllowedCourses] ([Ukprn],[LarsCode]);
+
+GO;
+
+
