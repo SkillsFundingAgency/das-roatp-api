e5c3fa4bb002f059aaef732e3ca2db6ff9d2bd41
Deven Shah <devenshah1974@gmail.com>
2026-03-03 09:54:23 +0000

    CSP-2510 Added table and view for recording course forecast

---
+++ src/SFA.DAS.Roatp.Database/Views/ForecastQuartersView.sql
@@ -0,0 +1,21 @@
+﻿CREATE VIEW [dbo].[ForecastQuartersView]
+AS
+-- this calculates the next 4 quarters and AYs
+WITH Quarters AS (
+    SELECT 0 AS [QuarterPeriod]
+    UNION ALL
+    SELECT [QuarterPeriod]+3 AS [Quarter]
+    FROM Quarters 
+    WHERE [QuarterPeriod] < 9
+)
+SELECT 
+    'AY'+RIGHT(YEAR(DATEADD(month,-7,QuarterDate)),2)+RIGHT(YEAR(DATEADD(month,5,QuarterDate)),2) [TimePeriod]
+    ,(((15-((19-month(QuarterDate))%12))-1)/3) [Quarter]
+    ,DATEADD(day,1,EOMONTH(QuarterDate,-((MONTH(QuarterDate)+10)%3)-1)) [StartDate]
+    ,EOMONTH(QuarterDate,-((MONTH(QuarterDate)+10)%3)+2) [EndDate]
+FROM (
+    SELECT [QuarterPeriod], dateadd(month,[QuarterPeriod],GETUTCDATE()) QuarterDate
+    FROM Quarters
+) ab1;
+
+GO
