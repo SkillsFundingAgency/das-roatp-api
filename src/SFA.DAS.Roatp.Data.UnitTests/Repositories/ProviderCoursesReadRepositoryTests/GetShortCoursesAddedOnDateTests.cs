6fd1b577bed65024f406345601f5c69356e32c88
Deven Shah <devenshah1974@gmail.com>
2026-03-19 14:54:27 +0000

    CSP-2517 Implemented repository to retrive short courses add on a date

---
+++ src/SFA.DAS.Roatp.Data.UnitTests/Repositories/ProviderCoursesReadRepositoryTests/GetShortCoursesAddedOnDateTests.cs
@@ -0,0 +1,71 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Roatp.Data.Repositories;
+using SFA.DAS.Roatp.Data.UnitTests.Setup;
+using SFA.DAS.Roatp.Domain.Entities;
+using SFA.DAS.Roatp.Domain.Models;
+
+namespace SFA.DAS.Roatp.Data.UnitTests.Repositories.ProviderCoursesReadRepositoryTests;
+
+public class GetShortCoursesAddedOnDateTests
+{
+    [Test]
+    public async Task GetShortCoursesAddedAfterDate_ReturnsExpectedResult()
+    {
+        var forDate = DateTime.Today.AddDays(-1);
+        var dataContext = RoatpDataContextFactory.CreateInMemoryContext();
+        AddProviderCourses(dataContext, forDate);
+
+        ProviderCoursesReadRepository sut = new(dataContext);
+
+        var actual = await sut.GetShortCoursesAddedOnDate(forDate, default);
+
+        actual.Should().HaveCount(1);
+        actual[0].CreatedDate.Date.Should().Be(DateTime.Today.AddDays(-1).Date);
+    }
+
+    [Test]
+    public async Task GetShortCoursesAddedAfterDate_ReturnsShortCoursesOnly()
+    {
+        var forDate = DateTime.Today.AddDays(-1);
+        var dataContext = RoatpDataContextFactory.CreateInMemoryContext();
+        AddProviderCourses(dataContext, forDate);
+
+        ProviderCoursesReadRepository sut = new(dataContext);
+
+        var actual = await sut.GetShortCoursesAddedOnDate(forDate, default);
+
+        actual.Should().Contain(c => c.Standard.CourseType == CourseType.ShortCourse);
+    }
+
+    private static void AddProviderCourses(RoatpDataContext dataContext, DateTime forDate)
+    {
+        Fixture fixture = new();
+        fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
+        fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+        List<ProviderCourse> providerCourses = fixture.Build<ProviderCourse>()
+            .With(c => c.CreatedDate, DateTime.Today)
+            .With(c => c.Standard, fixture.Build<Standard>().With(s => s.CourseType, CourseType.ShortCourse).Create())
+            .CreateMany()
+            .ToList();
+
+        providerCourses.Add(
+            fixture
+                .Build<ProviderCourse>()
+                .With(c => c.CreatedDate, forDate)
+                .With(c => c.Standard, fixture.Build<Standard>().With(s => s.CourseType, CourseType.ShortCourse).Create())
+                .Create());
+
+        providerCourses.Add(
+            fixture
+                .Build<ProviderCourse>()
+                .With(c => c.CreatedDate, forDate)
+                .With(c => c.Standard, fixture.Build<Standard>().With(s => s.CourseType, CourseType.Apprenticeship).Create())
+                .Create());
+
+        dataContext.AddRange(providerCourses);
+        dataContext.SaveChanges();
+    }
+}
--- src/SFA.DAS.Roatp.Data/Repositories/ProviderCoursesReadRepository.cs
+++ src/SFA.DAS.Roatp.Data/Repositories/ProviderCoursesReadRepository.cs
@@ -1,52 +1,63 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SFA.DAS.Roatp.Domain.Entities;
 using SFA.DAS.Roatp.Domain.Interfaces;
 
-namespace SFA.DAS.Roatp.Data.Repositories
+namespace SFA.DAS.Roatp.Data.Repositories;
+
+[ExcludeFromCodeCoverage]
+internal class ProviderCoursesReadRepository : IProviderCoursesReadRepository
 {
-    [ExcludeFromCodeCoverage]
-    internal class ProviderCoursesReadRepository : IProviderCoursesReadRepository
+    private readonly RoatpDataContext _roatpDataContext;
+
+    public ProviderCoursesReadRepository(RoatpDataContext roatpDataContext)
     {
-        private readonly RoatpDataContext _roatpDataContext;
+        _roatpDataContext = roatpDataContext;
+    }
 
-        public ProviderCoursesReadRepository(RoatpDataContext roatpDataContext)
-        {
-            _roatpDataContext = roatpDataContext;
-        }
+    public async Task<ProviderCourse> GetProviderCourse(int providerId, string larsCode)
+    {
+        return await _roatpDataContext
+            .ProviderCourses
+            .AsNoTracking()
+            .Where(c => c.ProviderId == providerId && c.LarsCode == larsCode)
+            .SingleOrDefaultAsync();
+    }
 
-        public async Task<ProviderCourse> GetProviderCourse(int providerId, string larsCode)
-        {
-            return await _roatpDataContext
-                .ProviderCourses
-                .AsNoTracking()
-                .Where(c => c.ProviderId == providerId && c.LarsCode == larsCode)
-                .SingleOrDefaultAsync();
-        }
+    public async Task<ProviderCourse> GetProviderCourseByUkprn(int ukprn, string larsCode)
+    {
+        return await _roatpDataContext
+            .ProviderCourses
+            .Include(c => c.Standard)
+            .Include(c => c.Locations)
+            .AsNoTracking()
+            .Where(c => c.Provider.Ukprn == ukprn && c.LarsCode == larsCode)
+            .SingleOrDefaultAsync();
+    }
 
-        public async Task<ProviderCourse> GetProviderCourseByUkprn(int ukprn, string larsCode)
-        {
-            return await _roatpDataContext
-                .ProviderCourses
-                .Include(c => c.Standard)
-                .Include(c => c.Locations)
-                .AsNoTracking()
-                .Where(c => c.Provider.Ukprn == ukprn && c.LarsCode == larsCode)
-                .SingleOrDefaultAsync();
-        }
+    public async Task<List<ProviderCourse>> GetAllProviderCourses(int ukprn)
+    {
+        return await _roatpDataContext
+             .ProviderCourses
+             .Include(pc => pc.Standard)
+             .Include(pc => pc.Locations)
+             .Include(pc => pc.Provider)
+             .Where(pc => pc.Provider.Ukprn == ukprn && pc.Provider.ProviderCourseTypes.Any(pct => pct.CourseType == pc.Standard.CourseType))
+             .AsNoTracking().ToListAsync();
+    }
 
-        public async Task<List<ProviderCourse>> GetAllProviderCourses(int ukprn)
-        {
-            return await _roatpDataContext
-                 .ProviderCourses
-                 .Include(pc => pc.Standard)
-                 .Include(pc => pc.Locations)
-                 .Include(pc => pc.Provider)
-                 .Where(pc => pc.Provider.Ukprn == ukprn && pc.Provider.ProviderCourseTypes.Any(pct => pct.CourseType == pc.Standard.CourseType))
-                 .AsNoTracking().ToListAsync();
-        }
+    public async Task<List<ProviderCourse>> GetShortCoursesAddedOnDate(DateTime dateTime, CancellationToken cancellationToken)
+    {
+        return await _roatpDataContext
+            .ProviderCourses
+            .Include(pc => pc.Standard)
+            .Where(pc => pc.CreatedDate.Date == dateTime.Date && pc.Standard.CourseType == Domain.Models.CourseType.ShortCourse)
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
     }
 }
