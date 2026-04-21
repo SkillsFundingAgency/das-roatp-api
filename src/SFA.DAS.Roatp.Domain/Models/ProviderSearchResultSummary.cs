6f254e0513dcc72a0c4236ac53cebfb142d0d91e
Ravi UDARI <Ravi.UDARI@EDUCATION.GOV.UK>
2026-04-21 15:59:28 +0100

    CSP-2283 Refactored GetProvidersForLarsCodeQueryResult and ProviderSearchModel to inherit from this base class, removing duplicated properties.

---
+++ src/SFA.DAS.Roatp.Domain/Models/ProviderSearchResultSummary.cs
@@ -0,0 +1,16 @@
+namespace SFA.DAS.Roatp.Domain.Models;
+
+public class ProviderSearchResultSummary
+{
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int TotalPages { get; set; }
+    public int TotalCount { get; set; }
+    public string LarsCode { get; set; }
+    public string StandardName { get; set; }
+    public CourseType CourseType { get; set; }
+    public ApprenticeshipType ApprenticeshipType { get; set; }
+    public bool IsActiveAvailable { get; set; }
+    public string QarPeriod { get; set; }
+    public string ReviewPeriod { get; set; }
+}
