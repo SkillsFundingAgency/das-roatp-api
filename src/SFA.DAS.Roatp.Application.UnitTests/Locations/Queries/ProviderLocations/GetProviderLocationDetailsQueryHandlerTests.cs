4fcca55ec9d533def93284d36a20b1533f577bf4
Afzal Hussain <afi259@hotmail.co.uk>
2026-05-22 11:47:12 +0100

    CSP-2603 Added learning type to provider locations response

--- src/SFA.DAS.Roatp.Application.UnitTests/Locations/Queries/ProviderLocations/GetProviderLocationDetailsQueryHandlerTests.cs
+++ src/SFA.DAS.Roatp.Application.UnitTests/Locations/Queries/ProviderLocations/GetProviderLocationDetailsQueryHandlerTests.cs
@@ -1,4 +1,7 @@
-﻿using AutoFixture.NUnit4;
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit4;
 using FluentAssertions;
 using Moq;
 using NUnit.Framework;
@@ -7,9 +10,6 @@ using SFA.DAS.Roatp.Application.Locations.Queries.GetProviderLocations;
 using SFA.DAS.Roatp.Domain.Entities;
 using SFA.DAS.Roatp.Domain.Interfaces;
 using SFA.DAS.Testing.AutoFixture;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace SFA.DAS.Roatp.Application.UnitTests.Locations.Queries.ProviderLocations
 {
@@ -44,6 +44,7 @@ namespace SFA.DAS.Roatp.Application.UnitTests.Locations.Queries.ProviderLocation
             result.Standards[0].Title.Should().Be(location.ProviderCourseLocations[0].ProviderCourse.Standard.Title);
             result.Standards[0].Level.Should().Be(location.ProviderCourseLocations[0].ProviderCourse.Standard.Level);
             result.Standards[0].LarsCode.Should().Be(location.ProviderCourseLocations[0].ProviderCourse.Standard.LarsCode);
+            result.Standards[0].LearningType.Should().Be(location.ProviderCourseLocations[0].ProviderCourse.Standard.ApprenticeshipType);
             result.Standards[0].HasOtherVenues.Should().Be(false);
         }
 
