6e7dd734984ff1dfe698044b1e1a686a1cd4c0fd
Ravi UDARI <Ravi.UDARI@EDUCATION.GOV.UK>
2026-05-26 12:12:58 +0100

    CSP-2590 To avoid data loss deployment, added both ApprenticeshipType, LearningType columns

--- src/SFA.DAS.Roatp.Database/Tables/Standard.sql
+++ src/SFA.DAS.Roatp.Database/Tables/Standard.sql
@@ -10,7 +10,8 @@
     [Duration] INT NOT NULL,
     [DurationUnits] VARCHAR(6) NOT NULL,
     [Route] VARCHAR(500) NOT NULL,
-    [LearningType] Varchar(50) NOT NULL DEFAULT 'Apprenticeship',
+    [ApprenticeshipType] Varchar(50) NOT NULL,
+    [LearningType] Varchar(50) NOT NULL,
     [CourseType] NVarchar(50) NOT NULL,
     [IsActiveAvailable] BIT NOT NULL DEFAULT 0,
     CONSTRAINT PK_Standard PRIMARY KEY (StandardUId)
