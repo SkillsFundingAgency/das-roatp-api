170f41fce719b76f2ed5df554c069e97269a08a6
Afzal Hussain <afi259@hotmail.co.uk>
2026-07-10 15:20:54 +0100

    CSP-2630 Created handler

---
+++ src/SFA.DAS.Roatp.Application/ProviderAllowedCourses/Commands/AddProviderToAllowedCourse/AddProviderToAllowedCourseCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MediatR;
+using SFA.DAS.Roatp.Application.Common;
+using SFA.DAS.Roatp.Application.Mediatr.Responses;
+
+namespace SFA.DAS.Roatp.Application.ProviderAllowedCourses.Commands.AddProviderToAllowedCourse;
+
+public class AddProviderToAllowedCourseCommand : IRequest<ValidatedResponse<Unit>>, IUkprn, ILarsCode, IUserInfo
+{
+    public string UserId { get; set; }
+    public string UserDisplayName { get; set; }
+    public DateTime? DateLastStarts { get; set; }
+    public int Ukprn { get; set; }
+    public string LarsCode { get; set; }
+}
---
+++ src/SFA.DAS.Roatp.Application/ProviderAllowedCourses/Commands/AddProviderToAllowedCourse/AddProviderToAllowedCourseCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.Roatp.Application.Mediatr.Responses;
+using SFA.DAS.Roatp.Domain.Interfaces;
+
+namespace SFA.DAS.Roatp.Application.ProviderAllowedCourses.Commands.AddProviderToAllowedCourse;
+
+public class AddProviderToAllowedCourseCommandHandler(IStandardsReadRepository _standardsReadRepository, IProviderCourseTypesReadRepository _providerCourseTypesReadRepository, IProviderAllowedCoursesRepository _providerAllowedCoursesRepository) : IRequestHandler<AddProviderToAllowedCourseCommand, ValidatedResponse<Unit>>
+{
+    public async Task<ValidatedResponse<Unit>> Handle(AddProviderToAllowedCourseCommand command, CancellationToken cancellationToken)
+    {
+        var standard = await _standardsReadRepository.GetStandard(command.LarsCode);
+
+        var providerCourseTypes = await _providerCourseTypesReadRepository.GetProviderCourseTypesByUkprn(command.Ukprn);
+
+        var providerAllowedCourses = await _providerAllowedCoursesRepository.GetProviderAllowedCoursesByLarsCode(command.LarsCode, cancellationToken);
+
+        bool hasProviderCourseType = providerCourseTypes.Any(x => x.CourseType == standard.CourseType);
+
+        if (providerAllowedCourses.Any(x => x.Ukprn == command.Ukprn))
+    }
+}
