1c04c7a6c1d0e6bd4dcc2950aeb5aa7ca25c7f51
afzal-hussain259 <afi259@hotmail.co.uk>
2026-09-01 16:38:03 +0100

    CSP-2748 Implement changes to get course types endpoint (#317)
    * CSP-2748 Implement changes to get course types endpoint
    
    * CSP-2748 amened allowed count

--- src/SFA.DAS.Roatp.Application.UnitTests/ProviderCourseTypes/Queries/ProviderCourseTypesQueryHandlerTests.cs
+++ src/SFA.DAS.Roatp.Application.UnitTests/ProviderCourseTypes/Queries/ProviderCourseTypesQueryHandlerTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoFixture.NUnit4;
@@ -8,47 +9,388 @@ using NUnit.Framework;
 using SFA.DAS.Roatp.Application.ProviderCourseTypes.Queries.GetProviderCourseTypes;
 using SFA.DAS.Roatp.Domain.Entities;
 using SFA.DAS.Roatp.Domain.Interfaces;
+using SFA.DAS.Roatp.Domain.Models;
 using SFA.DAS.Testing.AutoFixture;
 
-namespace SFA.DAS.Roatp.Application.UnitTests.ProviderCourseTypes.Queries
+namespace SFA.DAS.Roatp.Application.UnitTests.ProviderCourseTypes.Queries;
+
+[TestFixture]
+public class ProviderCourseTypesQueryHandlerTests
 {
-    [TestFixture]
-    public class ProviderCourseTypesQueryHandlerTests
+    [Test, MoqAutoData]
+    public async Task WhenApprenticeshipProviderIsRestricted_ThenRestrictedCountIsNull(
+        [Frozen] Mock<IProviderCourseTypesRepository> providerCourseTypesRepository,
+        [Frozen] Mock<IProviderAllowedCoursesRepository> providerAllowedCoursesRepository,
+        [Frozen] Mock<IRestrictedCourseViewRepository> restrictedCourseViewRepository,
+        [Greedy] GetProviderCourseTypesQueryHandler sut,
+        GetProviderCourseTypesQuery request)
     {
-        [Test, RecursiveMoqAutoData()]
-        public async Task Handle_ReturnsResult(
-            ProviderCourseType providerCourseType,
-            List<ProviderCourseType> providerCourseTypes,
-            [Frozen] Mock<IProviderCourseTypesRepository> repo,
-            GetProviderCourseTypesQuery query,
-            GetProviderCourseTypesQueryHandler sut,
-            CancellationToken cancellationToken)
+        // Arrange
+        var providerCourseType = new ProviderCourseType
         {
-            repo.Setup(r => r.GetProviderCourseTypesByUkprn(It.IsAny<int>(), It.IsAny<CancellationToken>())).ReturnsAsync(providerCourseTypes);
+            CourseType = CourseType.Apprenticeship,
+            IsRestrictedProvider = true
+        };
 
-            var response = await sut.Handle(query, cancellationToken);
+        var providerAllowedCourses = new List<ProviderAllowedCourse>();
 
-            response.Should().NotBeNull();
-            response.Result.Count.Should().Be(providerCourseTypes.Count);
+        var restrictedCourses = new List<RestrictedCourseView>();
 
-            response.Result[0].CourseType.Should().Be(providerCourseTypes[0].CourseType);
-        }
+        providerCourseTypesRepository
+            .Setup(x => x.GetProviderCourseTypesByUkprn(request.Ukprn, It.IsAny<CancellationToken>()))
+            .ReturnsAsync([providerCourseType]);
 
-        [Test, RecursiveMoqAutoData()]
-        public async Task Handle_NoData_ReturnsEmptyList(
-            ProviderCourseType providerCourseType,
-            [Frozen] Mock<IProviderCourseTypesRepository> repo,
-            GetProviderCourseTypesQuery query,
-            GetProviderCourseTypesQueryHandler sut,
-            CancellationToken cancellationToken)
+        providerAllowedCoursesRepository
+            .Setup(x => x.GetProviderAllowedCourses(request.Ukprn, CourseType.Apprenticeship, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(providerAllowedCourses);
+
+        restrictedCourseViewRepository
+            .Setup(x => x.GetRestrictedCourses(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(restrictedCourses);
+
+        // Act
+        var result = await sut.Handle(request, CancellationToken.None);
+
+        // Assert
+        result.Result[0].RestrictedCount.Should().BeNull();
+    }
+
+    [Test, MoqAutoData]
+    public async Task WhenRestrictedApprenticeshipProvider_ThenAllowedCountIsProviderAllowedCourseCount(
+        [Frozen] Mock<IProviderCourseTypesRepository> providerCourseTypesRepository,
+        [Frozen] Mock<IProviderAllowedCoursesRepository> providerAllowedCoursesRepository,
+        [Frozen] Mock<IRestrictedCourseViewRepository> restrictedCourseViewRepository,
+        [Greedy] GetProviderCourseTypesQueryHandler sut,
+        GetProviderCourseTypesQuery request)
+    {
+        // Arrange
+        var providerCourseType = new ProviderCourseType
         {
-            List<ProviderCourseType> providerCourseTypes = new List<ProviderCourseType>();
-            repo.Setup(r => r.GetProviderCourseTypesByUkprn(It.IsAny<int>(), It.IsAny<CancellationToken>())).ReturnsAsync(providerCourseTypes);
+            CourseType = CourseType.Apprenticeship,
+            IsRestrictedProvider = true
+        };
 
-            var response = await sut.Handle(query, cancellationToken);
+        var providerAllowedCourses = new List<ProviderAllowedCourse>
+        {
+            new()
+            {
+                LarsCode = "100"
+            },
+            new()
+            {
+                LarsCode = "200"
+            }
+        };
 
-            response.Should().NotBeNull();
-            response.Result.Count.Should().Be(0);
-        }
+        var restrictedCourses = new List<RestrictedCourseView>();
+
+        providerCourseTypesRepository
+            .Setup(x => x.GetProviderCourseTypesByUkprn(request.Ukprn, It.IsAny<CancellationToken>()))
+            .ReturnsAsync([providerCourseType]);
+
+        providerAllowedCoursesRepository
+            .Setup(x => x.GetProviderAllowedCourses(request.Ukprn, CourseType.Apprenticeship,
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(providerAllowedCourses);
+
+        restrictedCourseViewRepository
+            .Setup(x => x.GetRestrictedCourses(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(restrictedCourses);
+
+        // Act
+        var result = await sut.Handle(request, CancellationToken.None);
+
+        // Assert
+        result.Result[0].AllowedCount.Should().Be(2);
+    }
+
+    [Test, MoqAutoData]
+    public async Task WhenApprenticeshipProviderIsUnrestricted_ThenAllowedCountIsNull(
+        [Frozen] Mock<IProviderCourseTypesRepository> providerCourseTypesRepository,
+        [Frozen] Mock<IProviderAllowedCoursesRepository> providerAllowedCoursesRepository,
+        [Frozen] Mock<IRestrictedCourseViewRepository> restrictedCourseViewRepository,
+        [Greedy] GetProviderCourseTypesQueryHandler sut,
+        GetProviderCourseTypesQuery request)
+    {
+        // Arrange
+        var providerCourseType = new ProviderCourseType
+        {
+            CourseType = CourseType.Apprenticeship,
+            IsRestrictedProvider = false
+        };
+
+        var providerAllowedCourses = new List<ProviderAllowedCourse>();
+
+        var restrictedCourses = new List<RestrictedCourseView>();
+
+        providerCourseTypesRepository
+            .Setup(x => x.GetProviderCourseTypesByUkprn(request.Ukprn, It.IsAny<CancellationToken>()))
+            .ReturnsAsync([providerCourseType]);
+
+        providerAllowedCoursesRepository
+            .Setup(x => x.GetProviderAllowedCourses(request.Ukprn, CourseType.Apprenticeship, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(providerAllowedCourses);
+
+        restrictedCourseViewRepository
+            .Setup(x => x.GetRestrictedCourses(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(restrictedCourses);
+
+        // Act
+        var result = await sut.Handle(request, CancellationToken.None);
+
+        // Assert
+        result.Result[0].AllowedCount.Should().BeNull();
+    }
+
+    [Test, MoqAutoData]
+    public async Task WhenApprenticeshipProviderIsUnrestricted_ThenRestrictedCountIncludesAllApprenticeshipRestrictedCourses(
+        [Frozen] Mock<IProviderCourseTypesRepository> providerCourseTypesRepository,
+        [Frozen] Mock<IProviderAllowedCoursesRepository> providerAllowedCoursesRepository,
+        [Frozen] Mock<IRestrictedCourseViewRepository> restrictedCourseViewRepository,
+        [Greedy] GetProviderCourseTypesQueryHandler sut,
+        GetProviderCourseTypesQuery request)
+    {
+        // Arrange
+        var providerCourseType = new ProviderCourseType
+        {
+            CourseType = CourseType.Apprenticeship,
+            IsRestrictedProvider = false
+        };
+
+        var providerAllowedCourses = new List<ProviderAllowedCourse>();
+
+        var restrictedCourses = new List<RestrictedCourseView>
+        {
+            new()
+            {
+                LarsCode = "100",
+                Standard = new()
+                {
+                    CourseType = CourseType.Apprenticeship
+                }
+            },
+            new()
+            {
+                LarsCode = "200",
+                Standard = new()
+                {
+                    CourseType = CourseType.Apprenticeship
+                }
+            }
+        };
+
+        providerCourseTypesRepository
+            .Setup(x => x.GetProviderCourseTypesByUkprn(request.Ukprn, It.IsAny<CancellationToken>()))
+            .ReturnsAsync([providerCourseType]);
+
+        providerAllowedCoursesRepository
+            .Setup(x => x.GetProviderAllowedCourses(request.Ukprn, CourseType.Apprenticeship, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(providerAllowedCourses);
+
+        restrictedCourseViewRepository
+            .Setup(x => x.GetRestrictedCourses(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(restrictedCourses);
+
+        // Act
+        var result = await sut.Handle(request, CancellationToken.None);
+
+        // Assert
+        result.Result[0].RestrictedCount.Should().Be(2);
+    }
+
+    [Test, MoqAutoData]
+    public async Task WhenUnrestrictedApprenticeshipProviderHasNonRestrictedCourseWithLastDateStarts_ThenCourseIsAddedToRestrictedCount(
+        [Frozen] Mock<IProviderCourseTypesRepository> providerCourseTypesRepository,
+        [Frozen] Mock<IProviderAllowedCoursesRepository> providerAllowedCoursesRepository,
+        [Frozen] Mock<IRestrictedCourseViewRepository> restrictedCourseViewRepository,
+        [Greedy] GetProviderCourseTypesQueryHandler sut,
+        GetProviderCourseTypesQuery request)
+    {
+        // Arrange
+        var providerCourseType = new ProviderCourseType
+        {
+            CourseType = CourseType.Apprenticeship,
+            IsRestrictedProvider = false
+        };
+
+        var providerAllowedCourses = new List<ProviderAllowedCourse>
+        {
+            new()
+            {
+                LarsCode = "100",
+                LastDateStarts = DateTime.Today.AddDays(-1)
+            }
+        };
+
+        var restrictedCourses = new List<RestrictedCourseView>
+        {
+            new()
+            {
+                LarsCode = "200",
+                Standard = new()
+                {
+                    CourseType = CourseType.Apprenticeship
+                }
+            }
+        };
+
+        providerCourseTypesRepository
+            .Setup(x => x.GetProviderCourseTypesByUkprn(request.Ukprn, It.IsAny<CancellationToken>()))
+            .ReturnsAsync([providerCourseType]);
+
+        providerAllowedCoursesRepository
+            .Setup(x => x.GetProviderAllowedCourses(request.Ukprn, CourseType.Apprenticeship, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(providerAllowedCourses);
+
+        restrictedCourseViewRepository
+            .Setup(x => x.GetRestrictedCourses(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(restrictedCourses);
+
+        // Act
+        var result = await sut.Handle(request, CancellationToken.None);
+
+        // Assert
+        result.Result[0].RestrictedCount.Should().Be(2);
+    }
+
+    [Test, MoqAutoData]
+    public async Task WhenUnrestrictedApprenticeshipProviderHasRestrictedCourseWithNullLastDateStarts_ThenCourseIsRemovedFromRestrictedCount(
+        [Frozen] Mock<IProviderCourseTypesRepository> providerCourseTypesRepository,
+        [Frozen] Mock<IProviderAllowedCoursesRepository> providerAllowedCoursesRepository,
+        [Frozen] Mock<IRestrictedCourseViewRepository> restrictedCourseViewRepository,
+        [Greedy] GetProviderCourseTypesQueryHandler sut,
+        GetProviderCourseTypesQuery request)
+    {
+        // Arrange
+        const string larsCode = "100";
+
+        var providerCourseType = new ProviderCourseType
+        {
+            CourseType = CourseType.Apprenticeship,
+            IsRestrictedProvider = false
+        };
+
+        var providerAllowedCourses = new List<ProviderAllowedCourse>
+        {
+            new()
+            {
+                LarsCode = larsCode,
+                LastDateStarts = null
+            }
+        };
+
+        var restrictedCourses = new List<RestrictedCourseView>
+        {
+            new()
+            {
+                LarsCode = larsCode,
+                Standard = new()
+                {
+                    CourseType = CourseType.Apprenticeship
+                }
+            }
+        };
+
+        providerCourseTypesRepository
+            .Setup(x => x.GetProviderCourseTypesByUkprn(request.Ukprn, It.IsAny<CancellationToken>()))
+            .ReturnsAsync([providerCourseType]);
+
+        providerAllowedCoursesRepository
+            .Setup(x => x.GetProviderAllowedCourses(request.Ukprn, CourseType.Apprenticeship, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(providerAllowedCourses);
+
+        restrictedCourseViewRepository
+            .Setup(x => x.GetRestrictedCourses(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(restrictedCourses);
+
+        // Act
+        var result = await sut.Handle(request, CancellationToken.None);
+
+        // Assert
+        result.Result[0].RestrictedCount.Should().Be(0);
+    }
+
+    [Test, MoqAutoData]
+    public async Task WhenCourseTypeIsShortCourse_ThenAllowedCountIsProviderAllowedCourseCount(
+        [Frozen] Mock<IProviderCourseTypesRepository> providerCourseTypesRepository,
+        [Frozen] Mock<IProviderAllowedCoursesRepository> providerAllowedCoursesRepository,
+        [Frozen] Mock<IRestrictedCourseViewRepository> restrictedCourseViewRepository,
+        [Greedy] GetProviderCourseTypesQueryHandler sut,
+        GetProviderCourseTypesQuery request)
+    {
+        // Arrange
+        var providerCourseType = new ProviderCourseType
+        {
+            CourseType = CourseType.ShortCourse
+        };
+
+        var providerAllowedCourses = new List<ProviderAllowedCourse>
+        {
+            new()
+            {
+                LarsCode = "100"
+            },
+            new()
+            {
+                LarsCode = "200"
+            }
+        };
+
+        var restrictedCourses = new List<RestrictedCourseView>();
+
+        providerCourseTypesRepository
+            .Setup(x => x.GetProviderCourseTypesByUkprn(request.Ukprn, It.IsAny<CancellationToken>()))
+            .ReturnsAsync([providerCourseType]);
+
+        providerAllowedCoursesRepository
+            .Setup(x => x.GetProviderAllowedCourses(request.Ukprn, CourseType.ShortCourse, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(providerAllowedCourses);
+
+        restrictedCourseViewRepository
+            .Setup(x => x.GetRestrictedCourses(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(restrictedCourses);
+
+        // Act
+        var result = await sut.Handle(request, CancellationToken.None);
+
+        // Assert
+        result.Result[0].AllowedCount.Should().Be(2);
+    }
+
+    [Test, MoqAutoData]
+    public async Task WhenCourseTypeIsShortCourse_ThenRestrictedCountIsNull(
+        [Frozen] Mock<IProviderCourseTypesRepository> providerCourseTypesRepository,
+        [Frozen] Mock<IProviderAllowedCoursesRepository> providerAllowedCoursesRepository,
+        [Frozen] Mock<IRestrictedCourseViewRepository> restrictedCourseViewRepository,
+        [Greedy] GetProviderCourseTypesQueryHandler sut,
+        GetProviderCourseTypesQuery request)
+    {
+        // Arrange
+        var providerCourseType = new ProviderCourseType
+        {
+            CourseType = CourseType.ShortCourse
+        };
+
+        var providerAllowedCourses = new List<ProviderAllowedCourse>();
+
+        var restrictedCourses = new List<RestrictedCourseView>();
+
+        providerCourseTypesRepository
+            .Setup(x => x.GetProviderCourseTypesByUkprn(request.Ukprn, It.IsAny<CancellationToken>()))
+            .ReturnsAsync([providerCourseType]);
+
+        providerAllowedCoursesRepository
+            .Setup(x => x.GetProviderAllowedCourses(request.Ukprn, CourseType.ShortCourse, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(providerAllowedCourses);
+
+        restrictedCourseViewRepository
+            .Setup(x => x.GetRestrictedCourses(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(restrictedCourses);
+
+        // Act
+        var result = await sut.Handle(request, CancellationToken.None);
+
+        // Assert
+        result.Result[0].RestrictedCount.Should().BeNull();
     }
 }
--- src/SFA.DAS.Roatp.Application/ProviderCourseTypes/Queries/GetProviderCourseTypes/GetProviderCourseTypesQueryHandler.cs
+++ src/SFA.DAS.Roatp.Application/ProviderCourseTypes/Queries/GetProviderCourseTypes/GetProviderCourseTypesQueryHandler.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using SFA.DAS.Roatp.Application.Mediatr.Responses;
+using SFA.DAS.Roatp.Domain.Entities;
 using SFA.DAS.Roatp.Domain.Interfaces;
+using SFA.DAS.Roatp.Domain.Models;
 
 namespace SFA.DAS.Roatp.Application.ProviderCourseTypes.Queries.GetProviderCourseTypes;
 
@@ -12,16 +15,86 @@ namespace SFA.DAS.Roatp.Application.ProviderCourseTypes.Queries.GetProviderCours
 public class GetProviderCourseTypesQueryHandler : IRequestHandler<GetProviderCourseTypesQuery, ValidatedResponse<List<ProviderCourseTypeModel>>>
 {
     private readonly IProviderCourseTypesRepository _providerCourseTypesReadRepository;
+    private readonly IProviderAllowedCoursesRepository _providerAllowedCoursesRepository;
+    private readonly IRestrictedCourseViewRepository _restrictedCourseViewRepository;
 
-    public GetProviderCourseTypesQueryHandler(IProviderCourseTypesRepository providerCourseTypesReadRepository)
+    public GetProviderCourseTypesQueryHandler(IProviderCourseTypesRepository providerCourseTypesReadRepository, IProviderAllowedCoursesRepository providerAllowedCoursesRepository, IRestrictedCourseViewRepository restrictedCourseViewRepository)
     {
         _providerCourseTypesReadRepository = providerCourseTypesReadRepository;
+        _providerAllowedCoursesRepository = providerAllowedCoursesRepository;
+        _restrictedCourseViewRepository = restrictedCourseViewRepository;
     }
 
     public async Task<ValidatedResponse<List<ProviderCourseTypeModel>>> Handle(GetProviderCourseTypesQuery request, CancellationToken cancellationToken)
     {
-        var providerCourseTypes = await _providerCourseTypesReadRepository.GetProviderCourseTypesByUkprn(request.Ukprn);
-        var result = providerCourseTypes.Select(x => (ProviderCourseTypeModel)x).ToList();
+        var providerCourseTypes = await _providerCourseTypesReadRepository.GetProviderCourseTypesByUkprn(request.Ukprn, cancellationToken);
+
+        var result = new List<ProviderCourseTypeModel>();
+
+        foreach (var providerCourseType in providerCourseTypes)
+        {
+            var courseType = new ProviderCourseTypeModel();
+
+            if (providerCourseType.CourseType == CourseType.Apprenticeship)
+            {
+                courseType = await GetApprenticeshipProviderCourseType(providerCourseType, request.Ukprn, providerCourseType.CourseType, cancellationToken);
+            }
+            if (providerCourseType.CourseType == CourseType.ShortCourse)
+            {
+                courseType = await GetShortCourseProviderCourseType(providerCourseType, request.Ukprn, providerCourseType.CourseType, cancellationToken);
+            }
+
+            result.Add(courseType);
+        }
+
         return new ValidatedResponse<List<ProviderCourseTypeModel>>(result);
+    }
+
+    private async Task<ProviderCourseTypeModel> GetApprenticeshipProviderCourseType(ProviderCourseType providerCourseType, int ukprn, CourseType courseType, CancellationToken cancellationToken)
+    {
+        var providerAllowedCourses = await _providerAllowedCoursesRepository.GetProviderAllowedCourses(ukprn, courseType, cancellationToken);
+
+        var model = (ProviderCourseTypeModel)providerCourseType;
+
+        if (providerCourseType.IsRestrictedProvider)
+        {
+            model.AllowedCount = providerAllowedCourses.Count;
+
+            model.RestrictedCount = null;
+        }
+        else
+        {
+            var restrictedCourses = await _restrictedCourseViewRepository.GetRestrictedCourses(cancellationToken);
+
+            var restrictedCoursesCount = restrictedCourses.Count(rc => rc.Standard.CourseType == courseType);
+
+            restrictedCoursesCount += providerAllowedCourses.Count(pac =>
+                pac.LastDateStarts != null &&
+                !restrictedCourses.Any(rc =>
+                    rc.LarsCode == pac.LarsCode));
+
+            restrictedCoursesCount -= providerAllowedCourses.Count(pac =>
+                pac.LastDateStarts == null &&
+                restrictedCourses.Any(rc =>
+                    rc.LarsCode == pac.LarsCode));
+
+            model.AllowedCount = null;
+            model.RestrictedCount = restrictedCoursesCount;
+        }
+
+        return model;
+    }
+
+    private async Task<ProviderCourseTypeModel> GetShortCourseProviderCourseType(ProviderCourseType providerCourseType, int ukprn, CourseType courseType, CancellationToken cancellationToken)
+    {
+        var providerAllowedCourses = await _providerAllowedCoursesRepository.GetProviderAllowedCourses(ukprn, courseType, cancellationToken);
+
+        var model = (ProviderCourseTypeModel)providerCourseType;
+
+        model.AllowedCount = providerAllowedCourses.Count;
+
+        model.RestrictedCount = null;
+
+        return model;
     }
 }
