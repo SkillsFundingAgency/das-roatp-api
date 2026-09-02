5e176b3018ff26b8f13d8701091f2fc0c0cca77c
Afzal Hussain <afi259@hotmail.co.uk>
2026-09-02 13:17:43 +0100

    CSP-2745 Added provider allowed course validation

--- src/SFA.DAS.Roatp.Application/ProviderAllowedCourses/Commands/UpsertProviderAllowedCourse/UpsertProviderAllowedCourseCommandValidator.cs
+++ src/SFA.DAS.Roatp.Application/ProviderAllowedCourses/Commands/UpsertProviderAllowedCourse/UpsertProviderAllowedCourseCommandValidator.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation;
+﻿using System.Linq;
+using FluentValidation;
 using SFA.DAS.Roatp.Application.Common;
 using SFA.DAS.Roatp.Domain.Interfaces;
 
@@ -7,7 +8,8 @@ namespace SFA.DAS.Roatp.Application.ProviderAllowedCourses.Commands.UpsertProvid
 public class UpsertProviderAllowedCourseCommandValidator : AbstractValidator<UpsertProviderAllowedCourseCommand>
 {
     public const string InvalidLastDateStarts = "LastDateStarts cannot be after LastDateStarts of the course";
-    public UpsertProviderAllowedCourseCommandValidator(IStandardsReadRepository standardsReadRepository)
+    public const string ExistsInProviderAllowedCourse = "Course already exists in provider allowed course for provider";
+    public UpsertProviderAllowedCourseCommandValidator(IStandardsReadRepository standardsReadRepository, IProviderAllowedCoursesRepository providerAllowedCoursesRepository)
     {
         Include(new UserInfoValidator());
         RuleFor(x => x)
@@ -25,5 +27,18 @@ public class UpsertProviderAllowedCourseCommandValidator : AbstractValidator<Ups
                        || command.LastDateStarts.Value <= standard.LastDateStarts.Value;
             })
             .WithMessage(InvalidLastDateStarts);
+        RuleFor(x => x)
+            .MustAsync(async (command, cancellation) =>
+            {
+                if (!command.IsStartRestricted)
+                {
+                    return true;
+                }
+
+                var providerAllowedCourse = await providerAllowedCoursesRepository.GetProviderAllowedCoursesByLarsCode(command.LarsCode, cancellation);
+
+                return !providerAllowedCourse.Any(x => x.Ukprn == command.Ukprn);
+            })
+            .WithMessage(ExistsInProviderAllowedCourse);
     }
 }
