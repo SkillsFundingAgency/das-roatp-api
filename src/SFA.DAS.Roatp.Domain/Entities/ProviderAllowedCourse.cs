a7c38b6fca7b4cd27724fd27e3c6e13c0b52e688
Afzal Hussain <afi259@hotmail.co.uk>
2026-09-02 11:05:43 +0100

    CSP-2745 Removed update date

--- src/SFA.DAS.Roatp.Domain/Entities/ProviderAllowedCourse.cs
+++ src/SFA.DAS.Roatp.Domain/Entities/ProviderAllowedCourse.cs
@@ -8,7 +8,6 @@ public class ProviderAllowedCourse
     public int Ukprn { get; set; }
     public string LarsCode { get; set; }
     public DateTime? LastDateStarts { get; set; }
-    public DateTime UpdatedDate { get; set; }
 
     public virtual Standard Standard { get; set; }
     public virtual Provider Provider { get; set; }
