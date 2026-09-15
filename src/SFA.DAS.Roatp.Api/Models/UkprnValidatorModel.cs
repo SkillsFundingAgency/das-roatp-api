18ccf50027240bf2e566ac3d1dfaddb841e06bae
afzal-hussain259 <afi259@hotmail.co.uk>
2026-09-15 17:00:31 +0100

    CSP-2755 Implements Post ProviderCourseType endpoint (#319)
    * CSP-2755 Implement changes
    
    * CSP-2755 added test coverage
    
    * CSP-2755 added code coverage
    
    * CSP-2755 Addressed review comments

---
+++ src/SFA.DAS.Roatp.Api/Models/UkprnValidatorModel.cs
@@ -0,0 +1,8 @@
+﻿using SFA.DAS.Roatp.Application.Common;
+
+namespace SFA.DAS.Roatp.Api.Models;
+
+public class UkprnValidatorModel : IUkprn
+{
+    public int Ukprn { get; set; }
+}
---
+++ src/SFA.DAS.Roatp.Application/ProviderCourseTypes/Commands/CreateProviderCourseType/CreateProviderCourseTypeCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.Roatp.Application.Mediatr.Responses;
+using SFA.DAS.Roatp.Domain.Constants;
+using SFA.DAS.Roatp.Domain.Entities;
+using SFA.DAS.Roatp.Domain.Interfaces;
+
+namespace SFA.DAS.Roatp.Application.ProviderCourseTypes.Commands.CreateProviderCourseType;
+
+public class CreateProviderCourseTypeCommandHandler(IProviderCourseTypesRepository _providerCourseTypesRepository) : IRequestHandler<CreateProviderCourseTypeCommand, ValidatedResponse<Unit>>
+{
+    public async Task<ValidatedResponse<Unit>> Handle(CreateProviderCourseTypeCommand command, CancellationToken cancellationToken)
+    {
+        var providerCourseTypes = new List<ProviderCourseType>();
+
+        foreach (var courseType in command.CourseTypes)
+        {
+            var providerCourseType = new ProviderCourseType()
+            {
+                Ukprn = command.Ukprn,
+                CourseType = courseType
+            };
+
+            providerCourseTypes.Add(providerCourseType);
+        }
+
+        await _providerCourseTypesRepository.CreateProviderCourseType(providerCourseTypes, command.UserId, command.UserDisplayName, command.Ukprn, AuditEventTypes.CreateProviderCourseType, cancellationToken);
+
+        return new ValidatedResponse<Unit>(Unit.Value);
+    }
+}
