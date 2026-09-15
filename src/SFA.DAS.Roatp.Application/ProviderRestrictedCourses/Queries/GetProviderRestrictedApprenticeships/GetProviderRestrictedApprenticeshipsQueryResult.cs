1ff696b57f391889144cb2454dd4db47c6cda749
afzal-hussain259 <afi259@hotmail.co.uk>
2026-09-15 17:03:48 +0100

    CSP-2768 Implements lookup restricted apprenticeships (#322)
    * CSP-2755 Implement changes
    
    * CSP-2755 added test coverage
    
    * CSP-2755 added code coverage
    
    * CSP-2755 Addressed review comments
    
    * CSP-2768 Implement get restricted apprenticeship endpoint
    
    * CSP-2768 Added validator and controller test coverage
    
    * CSP-2768 Added model test coverage
    
    * CSP-2768 added test coverage
    
    * CSP-2768 added further test coverage
    
    * CSP-2768 Amended validation to throw validation error if appreticeship is restricted

---
+++ src/SFA.DAS.Roatp.Application/ProviderRestrictedCourses/Queries/GetProviderRestrictedApprenticeships/GetProviderRestrictedApprenticeshipsQueryResult.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.Roatp.Application.ProviderRestrictedCourses.Queries.GetProviderRestrictedApprenticeships;
+
+public class GetProviderRestrictedApprenticeshipsQueryResult
+{
+    public List<RestrictedApprenticeshipModel> Courses { get; set; }
+}
---
+++ src/SFA.DAS.Roatp.Application/ProviderRestrictedCourses/Queries/GetProviderRestrictedApprenticeships/RestrictedApprenticeshipModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SFA.DAS.Roatp.Domain.Constants;
+using SFA.DAS.Roatp.Domain.Entities;
+
+namespace SFA.DAS.Roatp.Application.ProviderRestrictedCourses.Queries.GetProviderRestrictedApprenticeships;
+
+public class RestrictedApprenticeshipModel
+{
+    public string LarsCode { get; set; }
+    public string Title { get; set; }
+    public int Level { get; set; }
+    public DateTime? LastDateStarts { get; set; }
+    public bool IsClosedToNewStarts { get; set; }
+
+    public static implicit operator RestrictedApprenticeshipModel(Standard source)
+        => new()
+        {
+            LarsCode = source.LarsCode,
+            Title = source.Title,
+            Level = source.Level,
+            LastDateStarts = source.LastDateStarts == DateConstants.StartRestrictedDate ? null : source.LastDateStarts
+        };
+}
