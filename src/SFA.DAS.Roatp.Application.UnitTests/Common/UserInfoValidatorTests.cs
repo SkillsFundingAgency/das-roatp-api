9066f809492d52c7b53f6a03652c07208160ff0f
Deven Shah <devenshah1974@gmail.com>
2026-09-25 14:16:09 +0100

    CSP-2796 Added special character validation to UserInfo

--- src/SFA.DAS.Roatp.Application.UnitTests/Common/UserInfoValidatorTests.cs
+++ src/SFA.DAS.Roatp.Application.UnitTests/Common/UserInfoValidatorTests.cs
@@ -1,61 +1,108 @@
-﻿using FluentValidation.TestHelper;
+﻿using System.Threading.Tasks;
+using FluentValidation.TestHelper;
 using NUnit.Framework;
 using SFA.DAS.Roatp.Application.Common;
-using System.Threading.Tasks;
 
-namespace SFA.DAS.Roatp.Application.UnitTests.Common
+namespace SFA.DAS.Roatp.Application.UnitTests.Common;
+
+[TestFixture]
+public class UserInfoValidatorTests
 {
-    [TestFixture]
-    public class UserInfoValidatorTests
+    UserInfoValidator _sut;
+    const string ValidUserId = "TestUserId";
+    const string ValidUserDisplayName = "TestUserDisplayName";
+
+    [TestCase("", UserInfoValidator.UserIdEmptyErrorMessage, false)]
+    [TestCase("  ", UserInfoValidator.UserIdEmptyErrorMessage, false)]
+    [TestCase(null, UserInfoValidator.UserIdEmptyErrorMessage, false)]
+    [TestCase("test", "", true)]
+    public async Task UserInfoValidator_UserId_Validations(string userId, string expectedErrorMessage, bool isValid)
     {
-        UserInfoValidator _sut;
-        const string ValidUserId = "TestUserId";
-        const string ValidUserDisplayName = "TestUserDisplayName";
+        _sut = new UserInfoValidator();
+        var testObj = new UserInfoValidatorTestObject(userId, ValidUserDisplayName);
 
-        [TestCase("", UserInfoValidator.UserIdEmptyErrorMessage, false)]
-        [TestCase("  ", UserInfoValidator.UserIdEmptyErrorMessage, false)]
-        [TestCase(null, UserInfoValidator.UserIdEmptyErrorMessage, false)]
-        [TestCase("test", "", true)]
-        public async Task UserInfoValidator_UserId_Validations(string userId, string expectedErrorMessage, bool isValid)
+        var result = await _sut.TestValidateAsync(testObj);
+
+        if (isValid)
         {
-            _sut = new UserInfoValidator();
-            var testObj = new UserInfoValidatorTestObject(userId, ValidUserDisplayName);
-
-            var result = await _sut.TestValidateAsync(testObj);
-
-            if (isValid)
-                result.ShouldNotHaveValidationErrorFor(c => c.UserId);
-            else
-                result.ShouldHaveValidationErrorFor(c => c.UserId).WithErrorMessage(expectedErrorMessage);
+            result.ShouldNotHaveValidationErrorFor(c => c.UserId);
         }
-
-        [TestCase("", UserInfoValidator.UserDisplayNameEmptyErrorMessage, false)]
-        [TestCase("  ", UserInfoValidator.UserDisplayNameEmptyErrorMessage, false)]
-        [TestCase(null, UserInfoValidator.UserDisplayNameEmptyErrorMessage, false)]
-        [TestCase("test", "", true)]
-        public async Task UserInfoValidator_UserDisplayName_Validations(string userDisplayName, string expectedErrorMessage, bool isValid)
+        else
         {
-            _sut = new UserInfoValidator();
-            var testObj = new UserInfoValidatorTestObject(ValidUserId, userDisplayName);
-
-            var result = await _sut.TestValidateAsync(testObj);
-
-            if (isValid)
-                result.ShouldNotHaveValidationErrorFor(c => c.UserDisplayName);
-            else
-                result.ShouldHaveValidationErrorFor(c => c.UserDisplayName).WithErrorMessage(expectedErrorMessage);
+            result.ShouldHaveValidationErrorFor(c => c.UserId).WithErrorMessage(expectedErrorMessage);
         }
     }
 
-    public class UserInfoValidatorTestObject : IUserInfo
+    [TestCase("(_.#*$&%~'?`+_@ -", true)]
+    [TestCase("<", false)]
+    [TestCase(":", false)]
+    [TestCase("=", false)]
+    [TestCase("{", false)]
+    public void UserInfoValidator_CharacterValidations(string value, bool isValid)
     {
-        public string UserId { get; }
+        _sut = new UserInfoValidator();
+        var testObj = new UserInfoValidatorTestObject(value, value);
+        var result = _sut.TestValidate(testObj);
 
-        public string UserDisplayName { get; }
-        public UserInfoValidatorTestObject(string userId, string userDisplayName)
+        if (isValid)
         {
-            UserId = userId;
-            UserDisplayName = userDisplayName;
+            result.ShouldNotHaveValidationErrorFor(c => c.UserId);
+            result.ShouldNotHaveValidationErrorFor(c => c.UserDisplayName);
         }
+        else
+        {
+            result.ShouldHaveValidationErrorFor(c => c.UserId).WithErrorMessage(ValidationMessages.InvalidCharactersErrorMessage);
+            result.ShouldHaveValidationErrorFor(c => c.UserDisplayName).WithErrorMessage(ValidationMessages.InvalidCharactersErrorMessage);
+        }
+    }
+
+    [TestCase(256, true)]
+    [TestCase(257, false)]
+    public void UserInfoValidator_UserId_CharacterCountValidations(int characterCount, bool isValid)
+    {
+        _sut = new UserInfoValidator();
+        var value = new string('a', characterCount);
+        var testObj = new UserInfoValidatorTestObject(value, value);
+        var result = _sut.TestValidate(testObj);
+
+        if (isValid)
+        {
+            result.ShouldNotHaveValidationErrorFor(c => c.UserId);
+            result.ShouldNotHaveValidationErrorFor(c => c.UserDisplayName);
+        }
+        else
+        {
+            result.ShouldHaveValidationErrorFor(c => c.UserId).WithErrorMessage(UserInfoValidator.MaximumAllowedLengthErrorMessage);
+            result.ShouldHaveValidationErrorFor(c => c.UserDisplayName).WithErrorMessage(UserInfoValidator.MaximumAllowedLengthErrorMessage);
+        }
+    }
+
+    [TestCase("", UserInfoValidator.UserDisplayNameEmptyErrorMessage, false)]
+    [TestCase("  ", UserInfoValidator.UserDisplayNameEmptyErrorMessage, false)]
+    [TestCase(null, UserInfoValidator.UserDisplayNameEmptyErrorMessage, false)]
+    [TestCase("test", "", true)]
+    public async Task UserInfoValidator_UserDisplayName_Validations(string userDisplayName, string expectedErrorMessage, bool isValid)
+    {
+        _sut = new UserInfoValidator();
+        var testObj = new UserInfoValidatorTestObject(ValidUserId, userDisplayName);
+
+        var result = await _sut.TestValidateAsync(testObj);
+
+        if (isValid)
+            result.ShouldNotHaveValidationErrorFor(c => c.UserDisplayName);
+        else
+            result.ShouldHaveValidationErrorFor(c => c.UserDisplayName).WithErrorMessage(expectedErrorMessage);
+    }
+}
+
+public class UserInfoValidatorTestObject : IUserInfo
+{
+    public string UserId { get; }
+
+    public string UserDisplayName { get; }
+    public UserInfoValidatorTestObject(string userId, string userDisplayName)
+    {
+        UserId = userId;
+        UserDisplayName = userDisplayName;
     }
 }
--- src/SFA.DAS.Roatp.Application/Common/ValidationMessages.cs
+++ src/SFA.DAS.Roatp.Application/Common/ValidationMessages.cs
@@ -1,28 +1,29 @@
-﻿namespace SFA.DAS.Roatp.Application.Common
+﻿namespace SFA.DAS.Roatp.Application.Common;
+
+public static class ValidationMessages
 {
-    public static class ValidationMessages
+    public static string IsRequired(string fieldName) => $"{fieldName} is required";
+    public const string InvalidCharactersErrorMessage = "Must contain only valid characters";
+
+    public static class EmailValidationMessages
     {
-        public static string IsRequired (string fieldName) => $"{fieldName} is required";
-        public static class EmailValidationMessages
-        {
-            public const string EmailAddressTooLong = "Email address is too long, must be 256 characters or fewer";
-            public const string EmailAddressWrongFormat = "Email address must be in the correct format, like name@example.com";
-        }
+        public const string EmailAddressTooLong = "Email address is too long, must be 256 characters or fewer";
+        public const string EmailAddressWrongFormat = "Email address must be in the correct format, like name@example.com";
+    }
 
-        public static class PhoneNumberValidationMessages
-        {
-            public const string PhoneNumberWrongLength = "Telephone number must be between 10 and 50 characters";
-        }
+    public static class PhoneNumberValidationMessages
+    {
+        public const string PhoneNumberWrongLength = "Telephone number must be between 10 and 50 characters";
+    }
 
-        public static class UrlValidationMessages
-        {
-            public static string UrlTooLong (string fieldName) => $"{fieldName} address is too long, must be 500 characters or fewer";
-            public static string UrlWrongFormat(string fieldName) => $"{fieldName} address must be in the correct format, like www.example.com";
-        }
+    public static class UrlValidationMessages
+    {
+        public static string UrlTooLong(string fieldName) => $"{fieldName} address is too long, must be 500 characters or fewer";
+        public static string UrlWrongFormat(string fieldName) => $"{fieldName} address must be in the correct format, like www.example.com";
+    }
 
-        public static class MarketingInfoValidationMessages
-        {
-            public const string MarketingInfoTooLong = "The length of 'Marketing Info' must be 750 characters or fewer.";
-        }
+    public static class MarketingInfoValidationMessages
+    {
+        public const string MarketingInfoTooLong = "The length of 'Marketing Info' must be 750 characters or fewer.";
     }
 }
